8ab5af02879b4d7f20894ea26fb4671158888666
Francisco Neto <francisco.tln@outlook.com>
2023-11-05 20:03:29 +0100

    use real model without approximations

--- model-lambda.py
+++ model-lambda.py
@@ -10,8 +10,10 @@ C = 299792.458
 
 
 # Theoretical distance modulus for cosmological constant-dominated, flat universe:
-def model_distance_modulus(z, h0, p0):
-    luminosity_distance_model = (C * z / h0) *  (1 / p0) * (1 - np.power(1 - p0, 1 + z))
+def model_distance_modulus(z, h0, dte):
+    delta_t_emission = dte + 0.00001 #  Units: (km/s/Mpc)^-1
+    p = 1 - np.exp(-h0 * delta_t_emission)
+    luminosity_distance_model = (C * z / h0) * (1 / p) * (1 - np.power(1 - p, 1 + z))
     return 25 + 5 * np.log10(luminosity_distance_model)
 
 
@@ -22,15 +24,15 @@ def model_distance_modulus(z, h0, p0):
     ydata=distance_modulus_values,
     sigma=sigma_distance_moduli,
     absolute_sigma=True,
-    p0=[72, 0.52]
+    p0=[72, 0]
 )
 
-# Extract the optimal value for H0 and p0
-[h0, p0] = params_opt
-[h0_std, p0_std] = np.sqrt(np.diag(params_cov))
+# Extract the optimal value for H0 and n0
+[h0, dte] = params_opt
+[h0_std, dte_std] = np.sqrt(np.diag(params_cov))
 
 # Calculate residuals
-predicted_distance_modulus_values = model_distance_modulus(z=z_values, h0=h0, p0=p0)
+predicted_distance_modulus_values = model_distance_modulus(z=z_values, h0=h0, dte=dte)
 residuals = predicted_distance_modulus_values - distance_modulus_values
 
 # Calculate R-squared
@@ -50,7 +52,7 @@ print_color("Sample size", len(z_values))
 print_color("Estimated H0 (km/s/Mpc)", h0_label)
 print_color("R-squared (%)", f"{100 * r_squared:.5f}")
 print_color("RMSD (mag)", f"{rmsd:.5f}")
-print_color("p0", f"{p0:.5f} ± {p0_std:.5f}")
+print_color("dte", f"{dte:.5f} ± {dte_std:.5f}")
 
 # Plot the data and the fit
 plot_predictions(
