a0b4f31741710d3959a5fa0677c8d7a07ccfcf8a
Francisco Neto <francisco.tln@outlook.com>
2023-11-14 14:09:25 +0100

    for the pantheon2018 sample use the SH0es baseline value for absolute magnitude -19.253

--- y2018pantheon/data.py
+++ y2018pantheon/data.py
@@ -8,8 +8,8 @@ def get_data():
     df = pd.read_csv(path_to_data + 'data.txt', sep = ' ')
     selected_columns = df[['zcmb', 'mb', 'dmb']].sort_values(by = 'zcmb')
 
-    # Absolute Magnitude according to Union2.1 with systematics
-    M0 = -19.3081547178
+    # Absolute Magnitude according to https://arxiv.org/pdf/2112.04510.pdf
+    M0 = -19.253
 
     return (
         'Pantheon2018',
