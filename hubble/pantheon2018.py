76ff03239bb281f5e7dd6545aa47bcd0de64f280
Francisco Neto <francisco.tln@outlook.com>
2025-02-22 13:11:53 +0100

    pantheon2018

--- hubble/pantheon2018.py
+++ hubble/pantheon2018.py
@@ -7,7 +7,7 @@ from multiprocessing import Pool
 from .plotting import plot_predictions, print_color, plot_residuals
 from y2018pantheon.data import get_data
 
-legend, z_values, apparent_mag_values, _, cov_matrix = get_data()
+legend, z_values, apparent_mag_values, cov_matrix = get_data()
 
 # Speed of light (km/s)
 C = 299792.458
@@ -15,7 +15,7 @@ C = 299792.458
 # inverse covariance matrix
 inv_cov_matrix = np.linalg.inv(cov_matrix)
 
-# ΛCDM
+# Flat ΛCDM
 def integral_of_e_z(zs, omega_m):
     i = 0
     res = np.empty((len(zs),), dtype=np.float64)
@@ -26,106 +26,79 @@ def integral_of_e_z(zs, omega_m):
         i = i + 1
     return res
 
-def model_lcdm_apparent_mag(z, omega_m, M):
+def lcdm_apparent_mag(z, params):
+    [M, omega_m] = params
     a0_over_ae = 1 + z
-    comoving_distance = C * integral_of_e_z(zs = z, omega_m=omega_m)
-    return M + 25 + 5 * np.log10(a0_over_ae * comoving_distance)
+    luminosity_distance = a0_over_ae * C * integral_of_e_z(z, omega_m)
+    return M + 25 + 5 * np.log10(luminosity_distance)
 
 # Apparent magnitude for alternative matter-dominated, flat universe:
-def model_apparent_mag(z, p, M):
+def model_apparent_mag(z, params):
+    [M, p] = params
     a0_over_ae = (1 + z)**(1 / (1 - p))
-    comoving_distance = 2 * C * (1 - p) * (1 - 1 / np.sqrt(a0_over_ae))
-    return M + 25 + 5 * np.log10(a0_over_ae * comoving_distance)
+    luminosity_distance = 2 * C * (1 - p) * (a0_over_ae - np.sqrt(a0_over_ae))
+    return M + 25 + 5 * np.log10(luminosity_distance)
 
 
-def chi_squared(params, z, observed_mag):
-    [M0, p0] = params
-    delta = observed_mag - model_apparent_mag(z=z, p=p0, M=M0)
+def chi_squared(params):
+    delta = apparent_mag_values - model_apparent_mag(z_values, params)
     return delta.T @ inv_cov_matrix @ delta
 
 
-def log_likelihood(params, z, observed_mag):
-    return -0.5 * chi_squared(params, z, observed_mag)
+def log_likelihood(params):
+    return -0.5 * chi_squared(params)
+
+
+bounds = np.array([
+    (-30, -27), # M
+    (0.1, 0.6), # p
+])
 
 
 def log_prior(params):
-    [M0, p0] = params
-    if -29.5 < M0 < -27.5 and 0.1 < p0 < 0.6:
+    if np.all((bounds[:, 0] < params) & (params < bounds[:, 1])):
         return 0.0
     return -np.inf
 
 
-def log_probability(params, z, observed_mag):
+def log_probability(params):
     lp = log_prior(params)
     if np.isinf(lp):
         return -np.inf
-    return lp + log_likelihood(params, z, observed_mag)
+    return lp + log_likelihood(params)
 
 
 def main():
-    n_dim = 2
-    n_walkers = 20
-    n_steps = 1600
+    steps_to_discard = 100
+    n_dim = len(bounds)
+    n_walkers = 100
+    n_steps = steps_to_discard + 500
     initial_pos = np.zeros((n_walkers, n_dim))
-    initial_pos[:, 0] = np.random.uniform(-29.5, -27.5, n_walkers)
-    initial_pos[:, 1] = np.random.uniform(0.1, 0.6, n_walkers)
 
-    with Pool() as pool:
-        sampler = emcee.EnsembleSampler(
-            n_walkers,
-            n_dim,
-            log_probability,
-            args=(z_values, apparent_mag_values),
-            pool=pool
-        )
+    for dim, (lower, upper) in enumerate(bounds):
+      initial_pos[:, dim] = np.random.uniform(lower, upper, n_walkers)
+
+    with Pool(10) as pool:
+        sampler = emcee.EnsembleSampler(n_walkers, n_dim, log_probability, pool=pool)
         sampler.run_mcmc(initial_pos, n_steps, progress=True)
 
 
-    # Extract samples
     chains_samples = sampler.get_chain(discard=0, flat=False)
-    samples = sampler.get_chain(discard=100, flat=True)
+    samples = sampler.get_chain(discard=steps_to_discard, flat=True)
 
-    try:
-        tau = sampler.get_autocorr_time()
-        effective_samples = n_steps * n_walkers / np.max(tau)
-        print(f"Estimated autocorrelation time: {tau}")
-        print(f"Effective samples: {effective_samples:.2f}")
-    except Exception as e:
-        print("Could not calculate the autocorrelation time")
-
-    # Calculate the posterior means and uncertainties
     M0_samples = samples[:, 0]
     p_samples = samples[:, 1]
 
     [M0_16, M0_50, M0_84] = np.percentile(M0_samples, [16, 50, 84])
     [p_16, p_50, p_84] = np.percentile(p_samples, [16, 50, 84])
 
-    corner.corner(
-        samples,
-        labels=[r"$M_0$", r"$p$"],
-        truths=[M0_50, p_50],
-        show_titles=True,
-        title_fmt=".4f",
-        title_kwargs={"fontsize": 12},
-    )
-    plt.show()
-
-    # Plot results: chains for each parameter
-    fig, axes = plt.subplots(2, figsize=(10, 7))
-    axes[0].plot(chains_samples[:, :, 0], color='black', alpha=0.3)
-    axes[0].set_ylabel(r"$M_0$")
-    axes[1].plot(chains_samples[:, :, 1], color='black', alpha=0.3)
-    axes[1].set_ylabel(r"$p$")
-    plt.show()
+    best_fit_params = [M0_50, p_50]
 
     # Calculate residuals
-    predicted_apparent_mag = model_apparent_mag(z=z_values, p=p_50, M=M0_50)
+    predicted_apparent_mag = model_apparent_mag(z_values, best_fit_params)
     residuals = apparent_mag_values - predicted_apparent_mag
 
-    # Compute skewness
     skewness = stats.skew(residuals)
-
-    # Compute kurtosis
     kurtosis = stats.kurtosis(residuals)
 
     # Calculate R-squared
@@ -150,16 +123,36 @@ def main():
     print_color("RMSD (mag)", f"{rmsd:.3f}")
     print_color("Skewness of residuals", f"{skewness:.3f}")
     print_color("kurtosis of residuals", f"{kurtosis:.3f}")
-    print_color("Reduced chi squared", chi_squared([M0_50, p_50], z_values, apparent_mag_values)/ (len(z_values) - 2))
+    print_color("Reduced chi squared", chi_squared(best_fit_params)/ (len(z_values) - 2))
 
     # Plot the data and the fit
+    corner.corner(
+        samples,
+        labels=[r"$M_0$", r"$p$"],
+        quantiles=[0.16, 0.5, 0.84],
+        show_titles=True,
+        title_fmt=".4f",
+        title_kwargs={"fontsize": 12},
+        smooth=1.5,
+        smooth1d=1.5,
+    )
+    plt.show()
+
+    # Plot chains for each parameter
+    fig, axes = plt.subplots(2, figsize=(10, 7))
+    axes[0].plot(chains_samples[:, :, 0], color='black', alpha=0.3)
+    axes[0].set_ylabel(r"$M_0$")
+    axes[1].plot(chains_samples[:, :, 1], color='black', alpha=0.3)
+    axes[1].set_ylabel(r"$p$")
+    plt.show()
+
     plot_predictions(
         legend=legend,
         x=z_values,
         y=apparent_mag_values,
         y_err=np.sqrt(np.diag(cov_matrix)),
         y_model=predicted_apparent_mag,
-        label=f"Apparent mag: $p$={p_50:.4f} and $M_0$={M0_50:.4f}",
+        label=f"Best fit: $p$={p_50:.4f} and $M_0$={M0_50:.4f}",
         x_scale="log"
     )
 
@@ -183,22 +176,22 @@ M0 contains Hubble constant and absolute magnitude
 =============================
 
 Alternative
-M0: -28.549 +/-0.011
-p: 0.349 +0.010/-0.011
+M0: -28.5486 +0.0109/-0.0112
+p: 0.3491 +0.0102/-0.0107
 R-squared (%): 99.69
 RMSD (mag): 0.147
 Skewness of residuals: 0.082
-kurtosis of residuals: 0.786
+kurtosis of residuals: 0.797
 Reduced chi squared:  0.991
 
 =============================
 
 ΛCDM
-M0: -28.5749 +0.0116/-0.0096
-Ωm: 0.299 +0.022/-0.020
+M0: -28.5751 +0.0105/-0.0105
+Ωm: 0.2985 +0.0219/-0.0218
 R-squared (%): 99.71
 RMSD (mag): 0.143
-Skewness of residuals: 0.186
-kurtosis of residuals: 0.687
+Skewness of residuals: 0.191
+kurtosis of residuals: 0.697
 Reduce chi squared: 0.9815
 """
--- y2018pantheon/data.py
+++ y2018pantheon/data.py
@@ -1,28 +1,26 @@
-# Source: https://github.com/dscolnic/Pantheon/blob/master/lcparam_full_long_zhel.txt
+# Source: https://github.com/dscolnic/Pantheon/blob/master/lcparam_full_long.txt
 import os
 import pandas as pd
 import numpy as np
 
 path_to_data = os.path.dirname(os.path.abspath(__file__)) + '/raw-data/'
-df = pd.read_csv(path_to_data + 'mb.txt', sep = ' ')
+data_frame = pd.read_csv(path_to_data + 'mb.txt', sep = ' ')
 convariances_file = pd.read_csv(path_to_data + 'mb_covariance_sys.txt')
-n = int(np.sqrt(convariances_file.size))
-variances = np.array(convariances_file['cov_mu'].values, dtype=np.float64, copy=True).reshape((n, n))
 
-selected_columns = df[['zcmb', 'mb', 'dmb']]
-z_values = np.array(selected_columns['zcmb'], dtype = np.float64, copy = False)
-apparent_magnitude_vals = np.array(selected_columns['mb'], dtype = np.float64, copy = False)
-sigma_magnitudes = np.array(selected_columns['dmb'], dtype = np.float64, copy = False)
-covariance_matrix = variances + np.diag(selected_columns['dmb'].values ** 2)
+n = data_frame['zcmb'].values.size
+variances = convariances_file['cov_mu'].to_numpy().reshape((n, n))
+
+z_values = data_frame['zcmb'].to_numpy()
+apparent_magnitude_vals = data_frame['mb'].to_numpy()
+sigma_magnitudes = data_frame['dmb'].to_numpy()
+full_covariance_matrix = variances + np.diag(sigma_magnitudes ** 2)
 
 sort_indices = np.argsort(z_values)
-bin = sort_indices[0:None]
 
 def get_data():
     return (
         'Pantheon2018',
-        z_values[bin],
-        apparent_magnitude_vals[bin],
-        sigma_magnitudes[bin],
-        covariance_matrix[bin, :][:, bin],
+        z_values[sort_indices],
+        apparent_magnitude_vals[sort_indices],
+        full_covariance_matrix[sort_indices, :][:, sort_indices]
     )
