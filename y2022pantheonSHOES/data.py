3af4036b85e67d188240b0272758d5f6c1d9ccb2
Francisco Neto <francisco.tln@outlook.com>
2025-02-23 12:00:43 +0100

    pantheon+ data

--- y2022pantheonSHOES/data.py
+++ y2022pantheonSHOES/data.py
@@ -6,22 +6,21 @@ import numpy as np
 path_to_data = os.path.dirname(os.path.abspath(__file__)) + '/raw-data/'
 data_frame = pd.read_csv(path_to_data + 'distances.txt', sep = ' ')
 convariances_file = pd.read_csv(path_to_data + 'covariance_stat_sys.txt', sep = ' ')
-selected_columns = data_frame[['zHD', 'IS_CALIBRATOR', 'm_b_corr']]
+selected_columns = data_frame[['zHD', 'm_b_corr']]
 
-legend = 'Pantheon+'
+legend = 'Pantheon+ (2022)'
 z_values = selected_columns['zHD'].to_numpy()
 apparent_magnitudes = selected_columns['m_b_corr'].to_numpy()
 
 n = z_values.size
 covariance_matrix = convariances_file['cov_mu_shoes'].to_numpy().reshape((n, n))
 
-pantheon_range = np.where(z_values > 0.01)[0]
-sort_indices = np.argsort(z_values[pantheon_range])
+pantheon_plus_range = np.where(z_values > 0.01)[0]
 
 def get_data():
     return (
         legend,
-        z_values[sort_indices],
-        apparent_magnitudes[sort_indices],
-        covariance_matrix[np.ix_(sort_indices, sort_indices)],
+        z_values[pantheon_plus_range],
+        apparent_magnitudes[pantheon_plus_range],
+        covariance_matrix[np.ix_(pantheon_plus_range, pantheon_plus_range)],
     )
