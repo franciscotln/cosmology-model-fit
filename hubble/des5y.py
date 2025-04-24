5310359dfddb79ebdd9aeaaedb13906656c47c06
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-04-24 13:37:46 +0200

    add nuisanse parameter delta m in des5y alone

--- hubble/des5y.py
+++ hubble/des5y.py
@@ -16,43 +16,29 @@ C = 299792.458
 # inverse covariance matrix
 inv_cov_matrix = np.linalg.inv(cov_matrix)
 
-# Hubble constant km/s/Mpc as assumed in the dataset
-h0 = 70
+# Hubble constant km/s/Mpc
+H0 = 70
 
 
 # Flat
-def integral_e_z(zs, omega_m, w0, wa):
+def integral_e_z(zs, deltaM, omega_m, w0, wa):
     z = np.linspace(0, np.max(zs), num=2000)
     sum = 1 + z
     Ez = np.sqrt(omega_m * sum**3 + (1 - omega_m) * ((2 * sum**2) / (1 + sum**2))**(3 * (1 + w0)))
     integral_values = cumulative_trapezoid(1/Ez, z, initial=0)
     return np.interp(zs, z, integral_values)
 
-def wcdm_distance_modulus(z, params):
-    a0_over_ae = 1 + z
-    comoving_distance = (C / h0) * integral_e_z(z, *params)
-    return 25 + 5 * np.log10(a0_over_ae * comoving_distance)
 
-
-def lcdm_distance_modulus(z, params):
-    [omega_m] = params
+def model_distance_modulus(z, params):
+    deltaM = params[0]
     a0_over_ae = 1 + z
-    comoving_distance = (C / h0) * integral_e_z(z, omega_m, -1, 0)
-    return 25 + 5 * np.log10(a0_over_ae * comoving_distance)
+    comoving_distance = (C / H0) * integral_e_z(z, *params)
+    return deltaM + 25 + 5 * np.log10(a0_over_ae * comoving_distance)
 
 
 def chi_squared(params):
-    """ 
-    Computes modified likelihood to marginalize over M 
-    (Wood-Vasey et al. 2001, Appendix A9-A12)
-    """
-    delta = distance_modulus_values - wcdm_distance_modulus(z_values, params)
-    deltaT = np.transpose(delta)
-    chit2 = np.sum(delta @ inv_cov_matrix @ deltaT)      # First term: (Δ^T C^-1 Δ)
-    B = np.sum(delta @ inv_cov_matrix)                   # Second term: B
-    C = np.sum(inv_cov_matrix)                           # Third term: C
-    chi2 = chit2 - (B**2 / C) + np.log(C / (2 * np.pi))  # Full modified chi2
-    return chi2
+    delta = distance_modulus_values - model_distance_modulus(z_values, params)
+    return np.dot(delta, np.dot(inv_cov_matrix, delta))
 
 
 def log_likelihood(params):
@@ -60,6 +46,7 @@ def log_likelihood(params):
 
 
 bounds = np.array([
+    (-0.5, 0.5), # ΔM
     (0, 0.9), # Ωm
     (-3, 1), # w0
     (-18, 5) # wa
@@ -84,7 +71,7 @@ def main():
     steps_to_discard = 100
     ndim = len(bounds)
     nwalkers = 50
-    n_steps = steps_to_discard + 2000
+    n_steps = steps_to_discard + 4000
     initial_pos = np.random.uniform(bounds[:, 0], bounds[:, 1], size=(nwalkers, ndim))
 
     with Pool(10) as pool:
@@ -103,14 +90,15 @@ def main():
         print("Autocorrelation time could not be computed")
 
     [
+        [deltaM_16, deltaM_50, deltaM_84],
         [omega_16, omega_50, omega_84],
         [w0_16, w0_50, w0_84],
         [wa_16, wa_50, wa_84]
     ] = np.percentile(samples, [16, 50, 84], axis=0).T
 
-    best_fit_params = [omega_50, w0_50, wa_50]
+    best_fit_params = [deltaM_50, omega_50, w0_50, wa_50]
 
-    predicted_distance_modulus_values = wcdm_distance_modulus(z_values, best_fit_params)
+    predicted_distance_modulus_values = model_distance_modulus(z_values, best_fit_params)
     residuals = distance_modulus_values - predicted_distance_modulus_values
 
     skewness = stats.skew(residuals)
@@ -125,13 +113,15 @@ def main():
     rmsd = np.sqrt(np.mean(residuals ** 2))
 
     # Print the values in the console
-    omega_label = f"{omega_50:.4f} +{omega_84-omega_50:.4f}/-{omega_50-omega_16:.4f}"
-    w0_label = f"{w0_50:.4f} +{w0_84-w0_50:.4f}/-{w0_50-w0_16:.4f}"
-    wa_label = f"{wa_50:.4f} +{wa_84-wa_50:.4f}/-{wa_50-wa_16:.4f}"
+    deltaM_label = f"{deltaM_50:.4f} +{deltaM_84-deltaM_50:.4f} -{deltaM_50-deltaM_16:.4f}"
+    omega_label = f"{omega_50:.4f} +{omega_84-omega_50:.4f} -{omega_50-omega_16:.4f}"
+    w0_label = f"{w0_50:.4f} +{w0_84-w0_50:.4f} -{w0_50-w0_16:.4f}"
+    wa_label = f"{wa_50:.4f} +{wa_84-wa_50:.4f} -{wa_50-wa_16:.4f}"
 
     print_color("Dataset", legend)
     print_color("z range", f"{z_values[0]:.3f} - {z_values[-1]:.3f}")
     print_color("Sample size", len(z_values))
+    print_color("ΔM", deltaM_label)
     print_color("Ωm", omega_label)
     print_color("w0", w0_label)
     print_color("wa", wa_label)
@@ -141,7 +131,7 @@ def main():
     print_color("Chi squared", chi_squared(best_fit_params))
 
     # plot posterior distribution from samples
-    labels = [f"$\Omega_M$", f"$w_0$", f"$w_a$"]
+    labels = [f"$\Delta_M$", f"$\Omega_M$", f"$w_0$", f"$w_a$"]
     corner.corner(
         samples,
         labels=labels,
@@ -156,11 +146,11 @@ def main():
     plt.show()
 
     # Plot chains for each parameter
-    fig, axes = plt.subplots(ndim, figsize=(10, 7))
+    _, axes = plt.subplots(ndim, figsize=(10, 7))
     if ndim == 1:
         axes = [axes]
     for i in range(ndim):
-        axes[i].plot(chains_samples[:, :, i], color='black', alpha=0.3)
+        axes[i].plot(chains_samples[:, :, i], color='black', alpha=0.3, lw=0.4)
         axes[i].set_ylabel(labels[i])
         axes[i].set_xlabel("chain step")
         axes[i].axvline(x=steps_to_discard, color='red', linestyle='--', alpha=0.5)
@@ -193,76 +183,54 @@ if __name__ == '__main__':
 
 """
 ********************************
-Dataset: Union2.1
-z range: 0.015 - 1.414
-Sample size: 580
-********************************
-
-Flat ΛCDM
-Chi squared: 550.9463
-Ωm: 0.2993 +0.0427/-0.0401
-w0: -1
-wa: 0
-R-squared (%): 99.30
-RMSD (mag): 0.268
-Skewness of residuals: 1.406
-
-==============================
-
-Flat wCDM
-Chi squared: 550.9743
-Ωm: 0.3170 +0.0926/-0.1373
-w0: -1.0515 +0.3141/-0.4072 (0.14 sigma)
-wa: 0
-R-squared (%): 99.30
-RMSD (mag): 0.268
-Skewness of residuals: 1.407
-
-********************************
 Dataset: DES-SN5YR
 z range: 0.025 - 1.121
 Sample size: 1829
 ********************************
 
 Flat ΛCDM
-Chi squared: 1649.4584
-Ωm: 0.3505 +0.0169/-0.0164
+ΔM: 0.0213 +0.0109 -0.0107
+Ωm: 0.3502 +0.0170 -0.0163
 w0: -1
 wa: 0
-R-squared (%): 98.35
-RMSD (mag): 0.268
+R-squared (%): 98.41
+RMSD (mag): 0.264
 Skewness of residuals: 3.409
+Chi squared: 1640.31
 
 ==============================
 
 Flat wCDM
-Chi squared: 1648.1532
-Ωm: 0.2706 +0.0730/-0.0950
-w0: -0.8115 +0.1455/-0.1619 (1.23 sigma)
+ΔM: 0.0302 +0.0127 -0.0132
+Ωm: 0.2760 +0.0717 -0.0890
+w0: -0.8218 +0.1417 -0.1655 (1.16 sigma)
 wa: 0
-R-squared (%): 98.32
-RMSD (mag): 0.271
-Skewness of residuals: 3.417
+R-squared (%): 98.40
+RMSD (mag): 0.264
+Skewness of residuals: 3.416
+Chi squared: 1639.00
 
 ==============================
 
 Flat w0 - (1 + w0) * (((1 + z)**2 - 1) / ((1 + z)**2 + 1))
-Chi squared: 1647.8506
-Ωm: 0.2961 +0.0487/-0.0509
-w0: -0.8315 +0.1208/-0.1352 (1.32 sigma)
+ΔM: 0.0313 +0.0130 -0.0134
+Ωm: 0.2958 +0.0487 -0.0533
+w0: -0.8308 +0.1232 -0.1379 (1.30 sigma)
 wa: 0
-R-squared (%): 98.32
-RMSD (mag): 0.271
+R-squared (%): 98.40
+RMSD (mag): 0.264
 Skewness of residuals: 3.418
+Chi squared: 1638.71
 
 ==============================
 
 Flat w0waCDM
-Chi squared: 1641.8763
-Ωm: 0.4910 +0.0333/-0.0463
-w0: -0.4196 +0.3408/-0.2840 (1.86 sigma)
-wa: -8.3720 +3.8721/-4.3639 (2.00 sigma)
-R-squared (%): 98.21
-RMSD (mag): 0.280
+ΔM: 0.0580 +0.0173 -0.0170
+Ωm: 0.4933 +0.0313 -0.0446
+w0: -0.4021 +0.3529 -0.2997 (1.83 sigma)
+wa: -8.5853 +3.8321 -4.3735 (2.09 sigma)
+R-squared (%): 98.40
+RMSD (mag): 0.264
 Skewness of residuals: 3.453
+Chi squared: 1632.74
 """
