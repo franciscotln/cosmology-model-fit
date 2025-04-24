b02c96720c66963bb580fea814520486f3675167
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-04-24 12:33:01 +0200

    desi+des5y+cc nuisance parameter delta M

--- bao/desi_des5y_cc.py
+++ bao/desi_des5y_cc.py
@@ -1,7 +1,7 @@
 import numpy as np
 import emcee
 import corner
-from scipy.integrate import cumulative_trapezoid, quad
+from scipy.integrate import cumulative_trapezoid
 import matplotlib.pyplot as plt
 from multiprocessing import Pool
 from y2024DES.data import get_data
@@ -22,7 +22,7 @@ inv_cov_matrix = np.linalg.inv(cov_matrix)
 
 
 def h_over_h0_model(z, params):
-    _, _, O_m, w0, _ = params
+    _, _, _, O_m, w0, _ = params
     sum = 1 + z
     return np.sqrt(O_m * sum**3 + (1 - O_m) * ((2 * sum**2) / (1 + sum**2))**(3 * (1 + w0)))
 
@@ -34,9 +34,10 @@ def integral_e_z(zs, params):
 
 
 def model_distance_modulus(z, params):
-    h0 = params[0]
+    delta_M = params[0]
+    h0 = params[1]
     comoving_distance = (c/h0) * integral_e_z(z, params)
-    return 25 + 5 * np.log10((1 + z) * comoving_distance)
+    return delta_M + 25 + 5 * np.log10((1 + z) * comoving_distance)
 
 
 def plot_bao_predictions(params):
@@ -48,7 +49,7 @@ def plot_bao_predictions(params):
     unique_quantities = set(quantity_types)
     colors = { "DV_over_rs": "red", "DM_over_rs": "blue", "DH_over_rs": "green" }
 
-    h0, r_d, omega_m, w0, wa = params
+    _, h0, r_d, omega_m, w0, wa = params
     z_smooth = np.linspace(0, max(z_values), 100)
     plt.figure(figsize=(8, 6))
     for q in unique_quantities:
@@ -101,7 +102,7 @@ def plot_bao_predictions(params):
 
 
 def H_z(z, params):
-    h0 = params[0]
+    h0 = params[1]
     return h0 * h_over_h0_model(z, params)
 
 
@@ -117,7 +118,7 @@ def DV_z(z, params):
 
 
 def model_predictions(params):
-    r_d = params[1]
+    r_d = params[2]
     predictions = []
     for z, _, quantity in data:
         if quantity == "DV_over_rs":
@@ -130,6 +131,7 @@ def model_predictions(params):
 
 
 bounds = np.array([
+    (-0.5, 0.5), # ΔM
     (60, 80), # H0
     (115, 160), # r_d
     (0.2, 0.7), # omega_m
@@ -138,19 +140,9 @@ bounds = np.array([
 ])
 
 
-C = np.sum(inverse_cov_sn) # Third term: C
-log_term = np.log(C / (2 * np.pi))
-
 def chi_squared(params):
-    """ 
-    Computes modified likelihood to marginalize over M 
-    (Wood-Vasey et al. 2001, Appendix A9-A12)
-    """
     delta_sn = distance_moduli_values - model_distance_modulus(z_vals, params)
-    deltaT = np.transpose(delta_sn)
-    chit2 = np.sum(delta_sn @ inverse_cov_sn @ deltaT)     # First term: (Δ^T C^-1 Δ)
-    B = np.sum(delta_sn @ inverse_cov_sn)                  # Second term: B
-    chi_sn = chit2 - (B**2 / C) + log_term                 # Full modified chi2
+    chi_sn = delta_sn @ inverse_cov_sn @ delta_sn
 
     delta_bao = data['value'] - model_predictions(params)
     chi_bao = np.dot(delta_bao, np.dot(inv_cov_matrix, delta_bao))
@@ -177,19 +169,11 @@ def log_probability(params):
     return lp + log_likelihood(params)
 
 
-def compute_best_delta_M(mu_model):
-    delta = distance_moduli_values - mu_model
-    ones = np.ones_like(distance_moduli_values)
-    numerator = ones @ inverse_cov_sn @ delta
-    denominator = ones @ inverse_cov_sn @ ones
-    return numerator / denominator
-
-
 def main():
     ndim = len(bounds)
     nwalkers = 100
     burn_in = 500
-    nsteps = 4000 + burn_in
+    nsteps = 5000 + burn_in
     initial_pos = np.random.default_rng().uniform(bounds[:, 0], bounds[:, 1], size=(nwalkers, ndim))
 
     with Pool(10) as pool:
@@ -206,6 +190,7 @@ def main():
     samples = sampler.get_chain(discard=burn_in, flat=True)
 
     [
+        [delta_M_16, delta_M_50, delta_M_84],
         [h0_16, h0_50, h0_84],
         [rd_16, rd_50, rd_84],
         [omega_16, omega_50, omega_84],
@@ -213,11 +198,12 @@ def main():
         [wa_16, wa_50, wa_84],
     ] = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
 
-    best_fit = [h0_50, rd_50, omega_50, w0_50, wa_50]
+    best_fit = [delta_M_50, h0_50, rd_50, omega_50, w0_50, wa_50]
     DES5Y_EFF_SAMPLE = 1735
     deg_of_freedom = DES5Y_EFF_SAMPLE + data['value'].size + z_cc_vals.size - len(best_fit)
 
-    print(f"h0: {h0_50:.4f} +{(h0_84 - h0_50):.4f} -{(h0_50 - h0_16):.4f}")
+    print(f"ΔM: {delta_M_50:.4f} +{(delta_M_84 - delta_M_50):.4f} -{(delta_M_50 - delta_M_16):.4f}")
+    print(f"H0: {h0_50:.4f} +{(h0_84 - h0_50):.4f} -{(h0_50 - h0_16):.4f}")
     print(f"r_d: {rd_50:.4f} +{(rd_84 - rd_50):.4f} -{(rd_50 - rd_16):.4f}")
     print(f"Ωm: {omega_50:.4f} +{(omega_84 - omega_50):.4f} -{(omega_50 - omega_16):.4f}")
     print(f"w0: {w0_50:.4f} +{(w0_84 - w0_50):.4f} -{(w0_50 - w0_16):.4f}")
@@ -225,19 +211,18 @@ def main():
     print(f"Chi squared: {chi_squared(best_fit):.4f}")
     print(f"Degrees of freedom: {deg_of_freedom}")
 
-    predicted_distance_modulus = model_distance_modulus(z_vals, best_fit)
     plot_bao_predictions(best_fit)
     plot_sn_predictions(
         legend=legend,
         x=z_vals,
         y=distance_moduli_values,
         y_err=np.sqrt(np.diag(cov_matrix_sn)),
-        y_model=predicted_distance_modulus + compute_best_delta_M(predicted_distance_modulus),
+        y_model=model_distance_modulus(z_vals, best_fit),
         label=f"Best fit: $w_0$={w0_50:.4f}, $\Omega_m$={omega_50:.4f}",
         x_scale="log"
     )
 
-    labels = [r"$H_0$", r"$r_d$", f"$\Omega_m$", r"$w_0$", r"$w_a$"]
+    labels = [r"$\Delta_M$", r"$H_0$", r"$r_d$", f"$\Omega_m$", r"$w_0$", r"$w_a$"]
     corner.corner(
         samples,
         labels=labels,
@@ -254,7 +239,7 @@ def main():
     if ndim == 1:
         axes = [axes]
     for i in range(ndim):
-        axes[i].plot(chains_samples[:, :, i], color='black', alpha=0.3)
+        axes[i].plot(chains_samples[:, :, i], color='black', alpha=0.3, lw=0.4)
         axes[i].set_ylabel(labels[i])
         axes[i].set_xlabel("chain step")
         axes[i].axvline(x=burn_in, color='red', linestyle='--', alpha=0.5)
@@ -268,55 +253,48 @@ if __name__ == "__main__":
 
 """
 Flat ΛCDM: w(z) = -1
-h0: 68.6038 +1.5933 -1.6312
-r_d: 146.6324 +3.4678 -3.3189
-Ωm: 0.3102 +0.0079 -0.0078
+ΔM: -0.0492 +0.0503 -0.0516
+H0: 68.5349 +1.6377 -1.6360 km/s/Mpc
+r_d: 146.7310 +3.4909 -3.3338 Mpc
+Ωm: 0.3102 +0.0079 -0.0079
 w0: -1
 wa: 0
-Chi squared: 1682.2712
-Degrees of freedom: 1776
+Chi squared: 1673.0929
+Degrees of freedom: 1775
 
 ==============================
 
 Flat wCDM: w(z) = w0
-h0: 67.3200 +1.6616 -1.6298
-r_d: 146.9780 +3.5382 -3.3584
-Ωm: 0.2983 +0.0091 -0.0087
-w0: -0.8778 +0.0383 -0.0385 (3.18 sigma)
+ΔM: -0.0597 +0.0502 -0.0508
+H0: 67.3136 +1.6322 -1.6219 km/s/Mpc
+r_d: 146.9967 +3.4530 -3.3342 Mpc
+Ωm: 0.2984 +0.0089 -0.0088
+w0: -0.8770 +0.0383 -0.0386 (3.20 sigma)
 wa: 0
-Chi squared: 1672.3421
-Degrees of freedom: 1775
+Chi squared: 1663.1678
+Degrees of freedom: 1774
 
 ==============================
 
 Flat alternative: w(z) = w0 - (1 + w0) * (((1 + z)**2 - 1) / ((1 + z)**2 + 1))
-h0: 67.2068 +1.6433 -1.6295
-r_d: 147.0357 +3.4621 -3.3545
-Ωm: 0.3055 +0.0079 -0.0079
-w0: -0.8583 +0.0414 -0.0425
+ΔM: -0.0584 +0.0500 -0.0503
+H0: 67.2316 +1.6251 -1.6054 km/s/Mpc
+r_d: 146.9738 +3.4205 -3.3327 Mpc
+Ωm: 0.3054 +0.0080 -0.0077
+w0: -0.8576 +0.0417 -0.0429 (3.37 sigma)
 wa: 0
-Chi squared: 1671.3762
-Degrees of freedom: 1775
+Chi squared: 1662.2220
+Degrees of freedom: 1774
 
 ==============================
 
 Flat w0waCDM: w(z) = w0 + wa * z/(1 + z)
-h0: 67.0483 +1.6021 -1.6216
-r_d: 147.1580 +3.4869 -3.3037
-Ωm: 0.3194 +0.0132 -0.0156
-w0: -0.8001 +0.0730 -0.0672 (2.85 sigma)
-wa: -0.6404 +0.4499 -0.4601 (1.41 sigma)
-Chi squared: 1670.1887
-Degrees of freedom: 1774
-
-============================
-
-Flat linear: w(z) = w0 + wa * z
-h0: 67.1224 +1.6316 -1.6569
-r_d: 146.9962 +3.4718 -3.3038
-Ωm: 0.3263 +0.0119 -0.0135
-w0: -0.8278 +0.0540 -0.0494 (3.33 sigma)
-wa: -0.4225 +0.2303 -0.2463 (1.77 sigma)
-Chi squared: 1670.2464
-Degrees of freedom: 1774
+ΔM: -0.0552 +0.0499 -0.0516
+H0: 67.1046 +1.6403 -1.6549 km/s/Mpc
+r_d: 147.0160 +3.4922 -3.3143 Mpc
+Ωm: 0.3194 +0.0132 -0.0163
+w0: -0.8004 +0.0718 -0.0666 (2.88 sigma)
+wa: -0.6371 +0.4632 -0.4641 (1.37 sigma)
+Chi squared: 1661.0327
+Degrees of freedom: 1773
 """
