c28b09978ff7b8201f2a5c3225fa5213a932060f
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-05-14 11:41:16 +0200

    gp with DotProduct kernel

--- cosmic_chronometers/cc_gp.py
+++ cosmic_chronometers/cc_gp.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel, DotProduct, WhiteKernel
 from y2005cc.data import get_data
 
 legend, z_values, H_values, cov_matrix = get_data()
@@ -11,8 +11,7 @@ z_values = z_values.reshape(-1, 1)
 z_pred = np.linspace(0, z_values.max(), 500).reshape(-1, 1)
 
 # Define RBF kernel (Squared Exponential)
-kernel = ConstantKernel(constant_value=147**2) * RBF(length_scale=2.5)
-
+kernel = ConstantKernel(constant_value=16**2) * DotProduct(sigma_0=2.0)**2
 # Gaussian Process Regressor
 gp = GaussianProcessRegressor(
     kernel=kernel,
@@ -23,8 +22,9 @@ gp = GaussianProcessRegressor(
 gp.fit(z_values, H_values)
 
 H_pred, sigma = gp.predict(z_pred, return_std=True)
-print("H0", f"{H_pred[0]:.2f} ± {sigma[0]:.2f}") # H0 64.73 ± 5.20 km/s/Mpc
-print("GP score:", gp.score(z_values, H_values)) # 0.897
+print("H0", f"{H_pred[0]:.2f} ± {sigma[0]:.2f}") # H0 65.78 ± 4.96 km/s/Mpc
+print("GP score:", gp.score(z_values, H_values)) # 0.899
+print("GP marginal likelihood value", gp.log_marginal_likelihood_value_) # -137.57
 
 plt.errorbar(z_values, H_values, yerr=dH_values, fmt='.', label='CC Data', capsize=2)
 plt.plot(z_pred, H_pred, label='GP Mean (RBF)', color='gray')
