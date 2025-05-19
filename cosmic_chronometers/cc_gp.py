937e618e40ee5c4b5353db66caf9115635a3764c
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-05-19 13:56:48 +0200

    cc+gp+cov

--- cosmic_chronometers/cc_gp.py
+++ cosmic_chronometers/cc_gp.py
@@ -2,33 +2,43 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import ConstantKernel as C, PairwiseKernel
+from sklearn.gaussian_process.kernels import ConstantKernel as C, RBF, WhiteKernel
 from y2005cc.data import get_data
 
 legend, z_values, H_values, cov_matrix = get_data()
 dH_values = np.sqrt(np.diag(cov_matrix))  # simple diagonal covariance matrix
 z_values = z_values.reshape(-1, 1)
 
+H_std = np.std(H_values)
+H_mean = np.mean(H_values)
+normalized_H = (H_values - H_mean) / H_std
+normalised_sigma = dH_values / H_std
+
 z_pred = np.linspace(0, z_values.max(), 500).reshape(-1, 1)
 
 # Define DotProduct kernel
+kernel = C() * RBF() + WhiteKernel(0.005, 'fixed')
 gp = GaussianProcessRegressor(
-    kernel = C(114**2) * PairwiseKernel(metric='poly', gamma=0.02),
-    alpha = dH_values**2,
-    normalize_y = False,
-    n_restarts_optimizer = 10,
+    kernel=kernel,
+    alpha=normalised_sigma**2,
+    normalize_y=False,
+    n_restarts_optimizer=10,
 )
-gp.fit(z_values.reshape(-1,1), H_values.reshape(-1,1))
+gp.fit(z_values.reshape(-1,1), normalized_H.reshape(-1,1))
 
-H_pred, sigma = gp.predict(z_pred, return_std=True)
-print("H0", f"{H_pred[0]:.2f} ± {sigma[0]:.2f}") # H0 65.40 ± 4.97 km/s/Mpc
-print("GP score:", gp.score(z_values, H_values)) # 0.9
+H_pred, cov_pred = gp.predict(z_pred, return_cov=True)
+cov_pred = cov_pred * H_std**2
+H_pred = H_pred * H_std + H_mean
+sigma = np.sqrt(np.diag(cov_pred))
+
+print("H0", f"{H_pred[0]:.2f} ± {sigma[0]:.2f}") # H0 73.06 ± 6.88 km/s/Mpc
+print("GP score:", gp.score(z_values, normalized_H)) # 0.881
 print("GP marginal likelihood value", gp.log_marginal_likelihood_value_) # -138.24
 print("GP kernel", gp.kernel_)
 
 plt.style.use('dark_background')
 plt.errorbar(z_values, H_values, yerr=dH_values, fmt='.', label='CC Data', capsize=2)
-plt.plot(z_pred, H_pred, label='GP Mean $PairwiseKernel(poly)$')
+plt.plot(z_pred, H_pred, label='GP Mean $RBF(0.872)$')
 plt.fill_between(z_pred.ravel(), H_pred - sigma, H_pred + sigma, alpha=0.5, label=r'$1\sigma$')
 plt.fill_between(z_pred.ravel(), H_pred - 2 * sigma, H_pred + 2 * sigma, alpha=0.3, label=r'$2\sigma$')
 plt.xlabel('z')
@@ -40,21 +50,27 @@ plt.grid(True)
 plt.savefig('cosmic_chronometers/cc_gp.png', dpi=500)
 plt.close()
 
+plt.imshow(cov_pred, cmap='hot', interpolation='none')
+plt.colorbar()
+plt.title('Covariance Matrix')
+plt.savefig('cosmic_chronometers/cc_gp_cov_matrix.png', dpi=500)
+plt.close()
+
 # Likelihood function
-c0 = np.logspace(2, 7, num=100)
-gamma = np.logspace(-5, 3, num=100)
-c0_grid, gamma_grid = np.meshgrid(c0, gamma)
+c0 = np.logspace(-3, 5, num=100)
+length_scale = np.logspace(-5, 4, num=100)
+c0_grid, length_scale_grid = np.meshgrid(c0, length_scale)
 
 log_marginal_likelihood = [
-    gp.log_marginal_likelihood(theta=np.log([c0_v, gamma_v]))
-    for c0_v, gamma_v in zip(c0_grid.ravel(), gamma_grid.ravel())
+    gp.log_marginal_likelihood(theta=np.log([c0_v, scale_v]))
+    for c0_v, scale_v in zip(c0_grid.ravel(), length_scale_grid.ravel())
 ]
-log_marginal_likelihood = np.reshape(log_marginal_likelihood, gamma_grid.shape)
+log_marginal_likelihood = np.reshape(log_marginal_likelihood, length_scale_grid.shape)
 vmin, vmax = (-log_marginal_likelihood).min(), 150
 level = np.around(np.logspace(np.log10(vmin), np.log10(vmax), num=20), decimals=1)
 plt.contour(
     c0_grid,
-    gamma_grid,
+    length_scale_grid,
     -log_marginal_likelihood,
     levels=level,
     norm=LogNorm(vmin=vmin, vmax=vmax),
@@ -63,7 +79,7 @@ plt.colorbar()
 plt.xscale("log")
 plt.yscale("log")
 plt.xlabel("$c_0$")
-plt.ylabel("$\gamma$")
+plt.ylabel("length scale")
 plt.title("Negative log-marginal-likelihood")
 plt.savefig('cosmic_chronometers/cc_gp_likelihood.png', dpi=500)
 plt.close()
