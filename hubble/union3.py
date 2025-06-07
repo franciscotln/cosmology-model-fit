c86088bcf2ca3f9a79b1993af06225657f0ee94e
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-06-07 13:18:29 +0200

    union3

--- hubble/union3.py
+++ hubble/union3.py
@@ -9,41 +9,38 @@ from multiprocessing import Pool
 from .plotting import plot_predictions, print_color, plot_residuals
 from y2023union3.data import get_data
 
-legend, z_values, distance_moduli_values, cov_matrix = get_data()
+legend, z_values, mu_vals, cov_matrix = get_data()
 
 cho = cho_factor(cov_matrix)
 
-# Speed of light (km/s)
-C = 299792.458
-
-# Hubble constant (km/s/Mpc)
-H0 = 70
+C = 299792.458  # Speed of light (km/s)
+H0 = 70  # Hubble constant (km/s/Mpc)
 
 
 z = np.linspace(0, np.max(z_values), num=4000)
 one_plus_z = 1 + z
 
 
-def integral_of_e_z(params):
+def h_over_h0(params):
     omega_m, w0 = params[1], params[2]
-    h_over_h0 = np.sqrt(
-        omega_m * one_plus_z**3
-        + (1 - omega_m) * ((2 * one_plus_z**2) / (1 + one_plus_z**2)) ** (3 * (1 + w0))
-    )
-    integral_values = cumulative_trapezoid(1 / h_over_h0, z, initial=0)
+    evolving_de = ((2 * one_plus_z**2) / (1 + one_plus_z**2)) ** (3 * (1 + w0))
+    return np.sqrt(omega_m * one_plus_z**3 + (1 - omega_m) * evolving_de)
+
+
+def integral_of_e_z(params):
+    integral_values = cumulative_trapezoid(1 / h_over_h0(params), z, initial=0)
     return np.interp(z_values, z, integral_values)
 
 
 # Flat model
-def model_distance_modulus(params):
-    deltaM = params[0]
+def distance_modulus(params):
     a0_over_ae = 1 + z_values
     comoving_distance = (C / H0) * integral_of_e_z(params)
-    return deltaM + 25 + 5 * np.log10(a0_over_ae * comoving_distance)
+    return params[0] + 25 + 5 * np.log10(a0_over_ae * comoving_distance)
 
 
 def chi_squared(params):
-    delta = distance_moduli_values - model_distance_modulus(params)
+    delta = mu_vals - distance_modulus(params)
     return np.dot(delta, cho_solve(cho, delta))
 
 
@@ -51,7 +48,7 @@ def log_likelihood(params):
     return -0.5 * chi_squared(params)
 
 
-bounds = np.array([(-0.5, 0.5), (0, 1), (-1.6, 0.5)])  # ΔM  # Ωm  # w0
+bounds = np.array([(-0.6, 0.6), (0, 1), (-2, 0)])  # ΔM  # Ωm  # w0
 
 
 def log_prior(params):
@@ -87,47 +84,39 @@ def main():
     samples = sampler.get_chain(discard=discarded_steps, flat=True)
 
     [
-        [deltaM_16, deltaM_50, deltaM_84],
+        [dM_16, dM_50, dM_84],
         [omega_16, omega_50, omega_84],
         [w0_16, w0_50, w0_84],
     ] = np.percentile(samples, [16, 50, 84], axis=0).T
 
-    best_fit_params = [deltaM_50, omega_50, w0_50]
+    best_fit_params = [dM_50, omega_50, w0_50]
 
-    predicted_distances = model_distance_modulus(best_fit_params)
-    residuals = distance_moduli_values - predicted_distances
-
-    skewness = skew(residuals)
+    predicted_distances = distance_modulus(best_fit_params)
+    residuals = mu_vals - predicted_distances
 
     # Calculate R-squared
-    average_distance_modulus = np.mean(distance_moduli_values)
     ss_res = np.sum(residuals**2)
-    ss_tot = np.sum((distance_moduli_values - average_distance_modulus) ** 2)
-    r_squared = 1 - (ss_res / ss_tot)
+    ss_tot = np.sum((mu_vals - np.mean(mu_vals)) ** 2)
+    r2 = 1 - (ss_res / ss_tot)
 
     # Calculate root mean square deviation
     rmsd = np.sqrt(np.mean(residuals**2))
 
-    delta_M_label = (
-        f"{deltaM_50:.4f} +{deltaM_84-deltaM_50:.4f}/-{deltaM_50-deltaM_16:.4f}"
-    )
+    dM_label = f"{dM_50:.4f} +{dM_84-dM_50:.4f}/-{dM_50-dM_16:.4f}"
     omega_label = f"{omega_50:.4f} +{omega_84-omega_50:.4f}/-{omega_50-omega_16:.4f}"
     w0_label = f"{w0_50:.4f} +{w0_84-w0_50:.4f}/-{w0_50-w0_16:.4f}"
 
     print_color("Dataset", legend)
     print_color("z range", f"{z_values[0]:.3f} - {z_values[-1]:.3f}")
     print_color("Sample size", len(z_values))
-    print_color("ΔM", delta_M_label)
+    print_color("ΔM", dM_label)
     print_color("Ωm", omega_label)
     print_color("w0", w0_label)
-    print_color("R-squared (%)", f"{100 * r_squared:.2f}")
+    print_color("R-squared (%)", f"{100 * r2:.2f}")
     print_color("RMSD (mag)", f"{rmsd:.3f}")
-    print_color("Skewness of residuals", f"{skewness:.3f}")
+    print_color("Skewness of residuals", f"{skew(residuals):.3f}")
     print_color("Chi squared", f"{chi_squared(best_fit_params):.4f}")
-    print_color(
-        "Reduced chi squared",
-        chi_squared(best_fit_params) / (len(z_values) - len(best_fit_params)),
-    )
+    print_color("Degs of freedom", len(z_values) - len(best_fit_params))
 
     labels = ["ΔM", "Ωm", "$w_0$"]
     corner.corner(
@@ -150,7 +139,7 @@ def main():
     plot_predictions(
         legend=legend,
         x=z_values,
-        y=distance_moduli_values,
+        y=mu_vals,
         y_err=sigma_mu,
         y_model=predicted_distances,
         label=f"Best fit: $\Omega_m$={omega_50:.4f}",
@@ -172,7 +161,7 @@ Sample size: 22
 
 Flat ΛCDM
 
-ΔM: -0.0698 +0.0878/-0.0874
+ΔM: -0.0698 +0.0878/-0.0874 mag
 Ωm: 0.3570 +0.0274/-0.0266
 w0: -1
 wa: 0
@@ -186,7 +175,7 @@ degrees of freedom: 20
 
 Flat wCDM
 
-ΔM: -0.0601 +0.0876/-0.0876
+ΔM: -0.0601 +0.0876/-0.0876 mag
 Ωm: 0.2532 +0.0883/-0.1113
 w0: -0.7483 +0.1568/-0.1893 (1.45 sigma)
 wa: 0
@@ -199,20 +188,20 @@ degrees of freedom: 19
 ==============================
 
 Flat alternative: w(z) = w0 - (1 + w0) * (((1 + z)**2 - 1) / ((1 + z)**2 + 1))
-ΔM: -0.0569 +0.0887/-0.0884
-Ωm: 0.2877 +0.0604/-0.0641
-w0: -0.7605 +0.1444/-0.1752
+ΔM: -0.0561 +0.0888/-0.0888 mag
+Ωm: 0.2872 +0.0605/-0.0646
+w0: -0.7601 +0.1448/-0.1744 (1.38 - 1.66 sigma)
 wa: 0
 R-squared (%): 99.94
-RMSD (mag): 0.053
-Skewness of residuals: -1.091
-Chi squared: 21.8792
-degrees of freedom: 19
+RMSD (mag): 0.054
+Skewness of residuals: -1.107
+Chi squared: 21.8765
+Degs of freedom: 19
 
 =============================
 
 Flat w0waCDM
-ΔM: -0.0326 +0.0913/-0.0907
+ΔM: -0.0326 +0.0913/-0.0907 mag
 Ωm: 0.4449 +0.0523/-0.0800
 w0: -0.5456 +0.2772/-0.2322 (1.78 sigma)
 wa: -4.3188 +2.6906/-3.1919 (1.47 sigma)
--- y2023union3/data.py
+++ y2023union3/data.py
@@ -1,25 +1,20 @@
-import os
 import pandas as pd
 import numpy as np
 
-path_to_data = os.path.dirname(os.path.abspath(__file__)) + '/raw-data/'
+data_frame = pd.read_csv("y2023union3/raw-data/bins_union_3.csv")
+cov_data = np.genfromtxt("y2023union3/raw-data/covariance.txt")
 
-data_frame = pd.read_csv(path_to_data + 'bins_union_3.txt', sep = ' ')
-cov_data = pd.read_csv(path_to_data + 'cov_bins_union_3.txt')
-
-n = data_frame['zcmb'].size
-covariance_matrix = np.array(cov_data['cov_mu'], dtype=np.float64, copy=True).reshape((n, n))
-
-z_values = np.array(data_frame['zcmb'], dtype = np.float64, copy = True)
-distance_moduli_values = np.array(data_frame['mb'], dtype = np.float64, copy = True)
-
+n = data_frame["zcmb"].size
+covariance_matrix = cov_data.reshape((n, n))
+z_values = data_frame["zcmb"].to_numpy()
+mu_values = data_frame["mu"].to_numpy()
 sort_indices = np.argsort(z_values)
-bin = sort_indices[0:22]
+
 
 def get_data():
     return (
-        'Union 3 - Bins',
-        z_values[bin],
-        distance_moduli_values[bin],
-        covariance_matrix[bin, :][:, bin],
+        "Union3 - 22 Bins",
+        z_values[sort_indices],
+        mu_values[sort_indices],
+        covariance_matrix[sort_indices, :][:, sort_indices],
     )
