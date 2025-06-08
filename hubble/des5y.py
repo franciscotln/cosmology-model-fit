8ae6ccccb44d5f07b0d9f21d6bc4f57dcbac8403
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-06-08 15:22:17 +0200

    des5y

--- hubble/des5y.py
+++ hubble/des5y.py
@@ -7,43 +7,36 @@ from scipy.linalg import cho_factor, cho_solve
 from scipy.integrate import cumulative_trapezoid
 from multiprocessing import Pool
 from .plotting import plot_predictions, print_color, plot_residuals
-from y2024DES.data import get_data
+from y2024DES.data import get_data, effective_sample_size
 
-legend, z_values, z_hel_values, distance_modulus_values, cov_matrix = get_data()
+legend, z_cmb_vals, z_hel_vals, observed_mu_vals, covmat = get_data()
+cho = cho_factor(covmat)
 
-# Speed of light (km/s)
-C = 299792.458
+C = 299792.458  # Speed of light (km/s)
+H0 = 70  # Hubble constant km/s/Mpc
 
-# inverse covariance matrix
-cho = cho_factor(cov_matrix)
-
-# Hubble constant km/s/Mpc
-H0 = 70
-
-# Grid
-z = np.linspace(0, np.max(z_values), num=2500)
-one_plus_z = 1 + z
+grid = np.linspace(0, np.max(z_cmb_vals), num=2500)
+one_plus_z = 1 + grid
 
 
-# Flat
-def integral_e_z(params):
-    omega_m, w0 = params[1], params[2]
-    Ez = np.sqrt(
-        omega_m * one_plus_z**3
-        + (1 - omega_m) * ((2 * one_plus_z**2) / (1 + one_plus_z**2)) ** (3 * (1 + w0))
-    )
-    integral_values = cumulative_trapezoid(1 / Ez, z, initial=0)
-    return np.interp(z_values, z, integral_values)
+def integral_Ez(Om, w0=-1):
+    Ode = 1 - Om
+    evolving_de = ((2 * one_plus_z**2) / (1 + one_plus_z**2)) ** (3 * (1 + w0))
+    Ez = np.sqrt(Om * one_plus_z**3 + Ode * evolving_de)
+    integral_vals = cumulative_trapezoid(1 / Ez, grid, initial=0)
+    return np.interp(z_cmb_vals, grid, integral_vals)
 
 
-def model_distance_modulus(params):
-    deltaM = params[0]
-    comoving_distance = (C / H0) * integral_e_z(params)
-    return deltaM + 25 + 5 * np.log10((1 + z_hel_values) * comoving_distance)
+def dL(params):
+    return (1 + z_hel_vals) * (C / H0) * integral_Ez(*params[1:])
+
+
+def theory_mu(params):
+    return params[0] + 25 + 5 * np.log10(dL(params))
 
 
 def chi_squared(params):
-    delta = distance_modulus_values - model_distance_modulus(params)
+    delta = observed_mu_vals - theory_mu(params)
     return np.dot(delta, cho_solve(cho, delta))
 
 
@@ -51,13 +44,7 @@ def log_likelihood(params):
     return -0.5 * chi_squared(params)
 
 
-bounds = np.array(
-    [
-        (-0.5, 0.5),  # ΔM
-        (0, 0.9),  # Ωm
-        (-3, 1),  # w0
-    ]
-)
+bounds = np.array([(-0.6, 0.6), (0, 0.8), (-1.5, 0)])  # ΔM  # Ωm  # w0
 
 
 def log_prior(params):
@@ -77,13 +64,13 @@ def log_probability(params):
 def main():
     steps_to_discard = 500
     ndim = len(bounds)
-    nwalkers = 8 * ndim
-    n_steps = steps_to_discard + 14000
-    initial_pos = np.random.uniform(bounds[:, 0], bounds[:, 1], size=(nwalkers, ndim))
+    nwalkers = 10 * ndim
+    nsteps = steps_to_discard + 12000
+    initial_state = np.random.uniform(bounds[:, 0], bounds[:, 1], size=(nwalkers, ndim))
 
     with Pool(10) as pool:
         sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, pool=pool)
-        sampler.run_mcmc(initial_pos, n_steps, progress=True)
+        sampler.run_mcmc(initial_state, nsteps, progress=True)
 
     samples = sampler.get_chain(discard=steps_to_discard, flat=True)
 
@@ -95,47 +82,43 @@ def main():
         print("Autocorrelation time could not be computed")
 
     [
-        [deltaM_16, deltaM_50, deltaM_84],
-        [omega_16, omega_50, omega_84],
+        [dM_16, dM_50, dM_84],
+        [Om_16, Om_50, Om_84],
         [w0_16, w0_50, w0_84],
-    ] = np.percentile(samples, [16, 50, 84], axis=0).T
+    ] = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
 
-    best_fit_params = [deltaM_50, omega_50, w0_50]
+    best_fit_params = [dM_50, Om_50, w0_50]
 
-    predicted_distance_modulus_values = model_distance_modulus(best_fit_params)
-    residuals = distance_modulus_values - predicted_distance_modulus_values
-
-    skewness = stats.skew(residuals)
+    theory_mu_vals = theory_mu(best_fit_params)
+    residuals = observed_mu_vals - theory_mu_vals
 
     # Calculate R-squared
-    average_distance_modulus = np.mean(distance_modulus_values)
     ss_res = np.sum(residuals**2)
-    ss_tot = np.sum((distance_modulus_values - average_distance_modulus) ** 2)
+    ss_tot = np.sum((observed_mu_vals - np.mean(observed_mu_vals)) ** 2)
     r_squared = 1 - (ss_res / ss_tot)
 
     # Calculate root mean square deviation
     rmsd = np.sqrt(np.mean(residuals**2))
 
     # Print the values in the console
-    deltaM_label = (
-        f"{deltaM_50:.4f} +{deltaM_84-deltaM_50:.4f} -{deltaM_50-deltaM_16:.4f}"
-    )
-    omega_label = f"{omega_50:.4f} +{omega_84-omega_50:.4f} -{omega_50-omega_16:.4f}"
-    w0_label = f"{w0_50:.4f} +{w0_84-w0_50:.4f} -{w0_50-w0_16:.4f}"
+    dM_label = f"{dM_50:.3f} +{dM_84-dM_50:.3f} -{dM_50-dM_16:.3f}"
+    Om_label = f"{Om_50:.3f} +{Om_84-Om_50:.3f} -{Om_50-Om_16:.3f}"
+    w0_label = f"{w0_50:.2f} +{w0_84-w0_50:.2f} -{w0_50-w0_16:.2f}"
 
     print_color("Dataset", legend)
-    print_color("z range", f"{z_values[0]:.3f} - {z_values[-1]:.3f}")
-    print_color("Sample size", len(z_values))
-    print_color("ΔM", deltaM_label)
-    print_color("Ωm", omega_label)
+    print_color("z range", f"{z_cmb_vals[0]:.3f} - {z_cmb_vals[-1]:.3f}")
+    print_color("Sample size", len(z_cmb_vals))
+    print_color("ΔM", dM_label)
+    print_color("Ωm", Om_label)
     print_color("w0", w0_label)
     print_color("R-squared (%)", f"{100 * r_squared:.2f}")
     print_color("RMSD (mag)", f"{rmsd:.3f}")
-    print_color("Skewness of residuals", f"{skewness:.3f}")
-    print_color("Chi squared", chi_squared(best_fit_params))
+    print_color("Skewness of residuals", f"{stats.skew(residuals):.3f}")
+    print_color("Chi squared", f"{chi_squared(best_fit_params):.2f}")
+    print_color("Effective deg of freedom", effective_sample_size - ndim)
 
     # plot posterior distribution from samples
-    labels = ["ΔM", "Ωm", "$w_0$"]
+    labels = ["ΔM", "$Ω_m$", "$w_0$"]
     corner.corner(
         samples,
         labels=labels,
@@ -151,21 +134,17 @@ def main():
     )
     plt.show()
 
-    y_err = np.sqrt(cov_matrix.diagonal())
-
-    # Plot the predictions
+    y_err = np.sqrt(covmat.diagonal())
     plot_predictions(
         legend=legend,
-        x=z_values,
-        y=distance_modulus_values,
+        x=z_cmb_vals,
+        y=observed_mu_vals,
         y_err=y_err,
-        y_model=predicted_distance_modulus_values,
-        label=f"$\Omega_m$={omega_label}",
+        y_model=theory_mu_vals,
+        label=f"Model: $\Omega_m$={Om_label}",
         x_scale="log",
     )
-
-    # Plot the residual analysis
-    plot_residuals(z_values=z_values, residuals=residuals, y_err=y_err, bins=40)
+    plot_residuals(z_values=z_cmb_vals, residuals=residuals, y_err=y_err, bins=40)
 
 
 if __name__ == "__main__":
@@ -192,24 +171,24 @@ Chi squared: 1640.08
 
 Flat wCDM
 ΔM: 0.031 +0.013 -0.013
-Ωm: 0.266 +0.072 -0.096
+Ωm: 0.267 +0.072 -0.095
 w0: -0.80 +0.14 -0.16
 wa: 0
 R-squared (%): 98.40
 RMSD (mag): 0.264
 Skewness of residuals: 3.415
-Chi squared: 1638.52068
+Chi squared: 1638.52
 
 ==============================
 
 Flat w0 - (1 + w0) * (((1 + z)**2 - 1) / ((1 + z)**2 + 1))
 ΔM: 0.032 +0.013 -0.013
-Ωm: 0.292 +0.050 -0.054
+Ωm: 0.293 +0.050 -0.054
 w0: -0.81 +0.12 -0.14
 wa: 0
 R-squared (%): 98.40
 RMSD (mag): 0.264
-Skewness of residuals: 3.417
+Skewness of residuals: 3.416
 Chi squared: 1638.18
 
 ==============================
