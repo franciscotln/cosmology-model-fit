81e7560610fd170a4f59f90b4fe9ef98e7b5b229
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-06-09 18:54:35 +0200

    refactor cc+union3

--- cosmic_chronometers/cc_union3.py
+++ cosmic_chronometers/cc_union3.py
@@ -1,83 +1,86 @@
 import numpy as np
 import emcee
-from getdist import MCSamples, plots
+import corner
 from scipy.integrate import cumulative_trapezoid
+from scipy.linalg import cho_factor, cho_solve
 import matplotlib.pyplot as plt
 from multiprocessing import Pool
-from y2023union3.data import get_data
+from y2023union3.data import get_data as get_sn_data
 from y2005cc.data import get_data as get_cc_data
 from hubble.plotting import plot_predictions as plot_sn_predictions
 
-_, z_cc_vals, H_cc_vals, cov_matrix_cc = get_cc_data()
-legend, z_vals, distance_moduli_values, cov_matrix_sn = get_data()
-inverse_cov_cc = np.linalg.inv(cov_matrix_cc)
-inverse_cov_sn = np.linalg.inv(cov_matrix_sn)
+legend_cc, z_cc_vals, H_cc_vals, cov_matrix_cc = get_cc_data()
+legend_sn, z_sn_vals, mu_vals, cov_matrix_sn = get_sn_data()
+cho_cc = cho_factor(cov_matrix_cc)
+cho_sn = cho_factor(cov_matrix_sn)
 
-c = 299792.458 # Speed of light in km/s
+c = 299792.458  # Speed of light in km/s
 
 
-def h_over_h0_model(z, params):
-    O_m, w0 = params[2], params[3]
+def Ez(z, O_m, w0):
     sum = 1 + z
-    return np.sqrt(O_m * sum**3 + (1 - O_m) * ((2 * sum**2) / (1 + sum**2))**(3 * (1 + w0)))
+    evolving_de = ((2 * sum**2) / (1 + sum**2)) ** (3 * (1 + w0))
+    return np.sqrt(O_m * sum**3 + (1 - O_m) * evolving_de)
 
 
-def integral_e_z(zs, params):
-    z = np.linspace(0, np.max(zs), num=3000)
-    integral_values = cumulative_trapezoid(1/h_over_h0_model(z, params), z, initial=0)
-    return np.interp(zs, z, integral_values)
+grid = np.linspace(0, np.max(z_sn_vals), num=3000)
 
 
-def model_distance_modulus(z, params):
-    delta_M, h0 = params[0], params[1]
-    comoving_distance = (c/h0) * integral_e_z(z, params)
-    return delta_M + 25 + 5 * np.log10((1 + z) * comoving_distance)
+def integral_Ez(params):
+    y = 1 / Ez(grid, *params[2:])
+    integral_values = cumulative_trapezoid(y=y, x=grid, initial=0)
+    return np.interp(z_sn_vals, grid, integral_values)
+
+
+def mu_theory(params):
+    dM, h0 = params[0], params[1]
+    dL = (1 + z_sn_vals) * (c / h0) * integral_Ez(params)
+    return dM + 25 + 5 * np.log10(dL)
 
 
 def plot_cc_predictions(params):
-    h0 = params[1]
     z_smooth = np.linspace(0, max(z_cc_vals), 100)
     plt.figure(figsize=(8, 6))
     plt.errorbar(
         x=z_cc_vals,
         y=H_cc_vals,
         yerr=np.sqrt(np.diag(cov_matrix_cc)),
-        fmt='.',
-        color='blue',
+        fmt=".",
+        color="blue",
         alpha=0.4,
         label="CC data",
         capsize=2,
         linestyle="None",
     )
-    plt.plot(z_smooth, H_z(z_smooth, params), color='green', alpha=0.5)
+    plt.plot(z_smooth, H_z(z_smooth, params), color="green", alpha=0.5)
     plt.xlabel("Redshift (z)")
     plt.ylabel(r"$H(z)$")
     plt.xlim(0, np.max(z_cc_vals) + 0.2)
     plt.legend()
-    plt.title(f"Cosmic chronometers: $H_0$={h0:.2f} km/s/Mpc")
+    plt.title(f"{legend_cc}: $H_0$={params[1]:.1f} km/s/Mpc")
     plt.show()
 
 
 def H_z(z, params):
-    h0 = params[1]
-    return h0 * h_over_h0_model(z, params)
+    return params[1] * Ez(z, *params[2:])
 
 
-bounds = np.array([
-    (-0.55, 0.45), # ΔM
-    (55, 80),      # H0
-    (0.1, 0.7),    # Ωm
-    (-5.0, 3.0),   # w0
-    (-4.5, 4.5),   # wa
-])
+bounds = np.array(
+    [
+        (-0.7, 0.5),  # ΔM
+        (55, 80),  # H0
+        (0.1, 0.7),  # Ωm
+        (-1.5, 0),  # w0
+    ]
+)
 
 
 def chi_squared(params):
-    delta_sn = distance_moduli_values - model_distance_modulus(z_vals, params)
-    chi_sn = np.dot(delta_sn, np.dot(inverse_cov_sn, delta_sn))
+    delta_sn = mu_vals - mu_theory(params)
+    chi_sn = np.dot(delta_sn, cho_solve(cho_sn, delta_sn))
 
     cc_delta = H_cc_vals - H_z(z_cc_vals, params)
-    chi_cc = np.dot(cc_delta, np.dot(inverse_cov_cc, cc_delta))
+    chi_cc = np.dot(cc_delta, cho_solve(cho_cc, cc_delta))
 
     return chi_sn + chi_cc
 
@@ -101,10 +104,12 @@ def log_probability(params):
 
 def main():
     ndim = len(bounds)
-    nwalkers = 25
+    nwalkers = 8 * ndim
     burn_in = 500
     nsteps = 20000 + burn_in
-    initial_pos = np.random.default_rng().uniform(bounds[:, 0], bounds[:, 1], size=(nwalkers, ndim))
+    initial_pos = np.random.default_rng().uniform(
+        bounds[:, 0], bounds[:, 1], size=(nwalkers, ndim)
+    )
 
     with Pool(10) as pool:
         sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, pool=pool)
@@ -119,50 +124,44 @@ def main():
     samples = sampler.get_chain(discard=burn_in, flat=True)
 
     [
-        [delta_M_16, delta_M_50, delta_M_84],
+        [dM_16, dM_50, dM_84],
         [h0_16, h0_50, h0_84],
-        [omega_16, omega_50, omega_84],
+        [Om_16, Om_50, Om_84],
         [w0_16, w0_50, w0_84],
-        [wa_16, wa_50, wa_84],
     ] = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
 
-    best_fit = [delta_M_50, h0_50, omega_50, w0_50, wa_50]
+    best_fit = [dM_50, h0_50, Om_50, w0_50]
+    deg_of_freedom = z_sn_vals.size + z_cc_vals.size - len(best_fit)
 
-    deg_of_freedom = z_vals.size + z_cc_vals.size - len(best_fit)
-
-    print(f"ΔM: {delta_M_50:.4f} +{(delta_M_84 - delta_M_50):.4f} -{(delta_M_50 - delta_M_16):.4f}")
-    print(f"H0: {h0_50:.4f} +{(h0_84 - h0_50):.4f} -{(h0_50 - h0_16):.4f}")
-    print(f"Ωm: {omega_50:.4f} +{(omega_84 - omega_50):.4f} -{(omega_50 - omega_16):.4f}")
-    print(f"w0: {w0_50:.4f} +{(w0_84 - w0_50):.4f} -{(w0_50 - w0_16):.4f}")
-    print(f"wa: {wa_50:.4f} +{(wa_84 - wa_50):.4f} -{(wa_50 - wa_16):.4f}")
-    print(f"Chi squared: {chi_squared(best_fit):.4f}")
+    print(f"ΔM: {dM_50:.3f} +{(dM_84 - dM_50):.3f} -{(dM_50 - dM_16):.3f}")
+    print(f"H0: {h0_50:.1f} +{(h0_84 - h0_50):.1f} -{(h0_50 - h0_16):.1f}")
+    print(f"Ωm: {Om_50:.3f} +{(Om_84 - Om_50):.3f} -{(Om_50 - Om_16):.3f}")
+    print(f"w0: {w0_50:.2f} +{(w0_84 - w0_50):.2f} -{(w0_50 - w0_16):.2f}")
+    print(f"Chi squared: {chi_squared(best_fit):.2f}")
     print(f"Degrees of freedom: {deg_of_freedom}")
 
     plot_cc_predictions(best_fit)
     plot_sn_predictions(
-        legend=legend,
-        x=z_vals,
-        y=distance_moduli_values,
+        legend=legend_sn,
+        x=z_sn_vals,
+        y=mu_vals,
         y_err=np.sqrt(np.diag(cov_matrix_sn)),
-        y_model=model_distance_modulus(z_vals, best_fit),
-        label=f"Best fit: $H_0$={h0_50:.2f} km/s/Mpc, $\Omega_m$={omega_50:.4f}",
-        x_scale="log"
+        y_model=mu_theory(best_fit),
+        label=f"Best fit: $H_0$={h0_50:.2f} km/s/Mpc, $\Omega_m$={Om_50:.4f}",
+        x_scale="log",
     )
-
-    labels = ["ΔM", "H_0", "Ωm", "w_0", "w_a"]
-    gdsamples = MCSamples(
-        samples=samples,
-        names=labels,
-        labels=labels,
-        settings={"fine_bins_2D": 256, "smooth_scale_2D": 0.9}
-    )
-    g = plots.get_subplot_plotter()
-    g.triangle_plot(
-        gdsamples,
-        Filled=False,
-        contour_levels=[0.68, 0.95],
-        title_limit=True,
-        diag1d_kwargs={"normed": True},
+    corner.corner(
+        samples,
+        labels=["ΔM", "$H_0$", "$Ωm$", "$w_0$"],
+        quantiles=[0.159, 0.5, 0.841],
+        show_titles=True,
+        title_fmt=".3f",
+        smooth=1.5,
+        smooth1d=1.5,
+        bins=100,
+        levels=(0.393, 0.864),  # 1 and 2 sigmas in 2D
+        fill_contours=False,
+        plot_datapoints=False,
     )
     plt.show()
 
