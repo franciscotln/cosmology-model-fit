27989b9d65b1b4a8442aafb7b2e84531fe3ea8c6
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-06-11 00:07:36 +0200

    cc+pantheon

--- cosmic_chronometers/cc_pantheon.py
+++ cosmic_chronometers/cc_pantheon.py
@@ -17,32 +17,28 @@ cov_cc_cho = cho_factor(cov_matrix_cc)
 c = 299792.458  # Speed of light in km/s
 
 
-def h_over_h0_model(z, params):
-    O_m, w0 = params[2], params[3]
+def Ez(z, O_m, w0=-1):
     sum = 1 + z
-    return np.sqrt(
-        O_m * sum**3 + (1 - O_m) * ((2 * sum**2) / (1 + sum**2)) ** (3 * (1 + w0))
-    )
+    evolving_de = ((2 * sum**2) / (1 + sum**2)) ** (3 * (1 + w0))
+    return np.sqrt(O_m * sum**3 + (1 - O_m) * evolving_de)
 
 
 z_grid_sn = np.linspace(0, np.max(z_vals), num=2000)
 
 
-def integral_e_z(params):
-    integral_values = cumulative_trapezoid(
-        1 / h_over_h0_model(z_grid_sn, params), z_grid_sn, initial=0
-    )
+def integral_Ez(params):
+    y = 1 / Ez(z_grid_sn, *params[2:])
+    integral_values = cumulative_trapezoid(y=y, x=z_grid_sn, initial=0)
     return np.interp(z_vals, z_grid_sn, integral_values)
 
 
-def model_apparent_mag(params):
+def apparent_mag_theory(params):
     h0, M = params[0], params[1]
-    comoving_distance = (c / h0) * integral_e_z(params)
+    comoving_distance = (c / h0) * integral_Ez(params)
     return M + 25 + 5 * np.log10((1 + z_hel_vals) * comoving_distance)
 
 
 def plot_cc_predictions(params):
-    h0 = params[0]
     z_smooth = np.linspace(0, max(z_cc_vals), 100)
     plt.errorbar(
         x=z_cc_vals,
@@ -60,13 +56,12 @@ def plot_cc_predictions(params):
     plt.ylabel(r"$H(z)$")
     plt.xlim(0, np.max(z_cc_vals) + 0.2)
     plt.legend()
-    plt.title(f"{cc_legend}: $H_0$={h0:.2f} km/s/Mpc")
+    plt.title(f"{cc_legend}: $H_0$={params[0]:.1f} km/s/Mpc")
     plt.show()
 
 
 def H_z(z, params):
-    h0 = params[0]
-    return h0 * h_over_h0_model(z, params)
+    return params[0] * Ez(z, *params[2:])
 
 
 bounds = np.array(
@@ -75,13 +70,12 @@ bounds = np.array(
         (-20, -19),  # M
         (0.15, 0.70),  # Ωm
         (-1.5, 0.0),  # w0
-        (-3.5, 3.5),  # wa
     ]
 )
 
 
 def chi_squared(params):
-    delta_sn = apparent_mag_values - model_apparent_mag(params)
+    delta_sn = apparent_mag_values - apparent_mag_theory(params)
     chi_sn = np.dot(delta_sn, cho_solve(cov_sn_cho, delta_sn))
 
     delta_cc = H_cc_vals - H_z(z_cc_vals, params)
@@ -131,22 +125,18 @@ def main():
     [
         [h0_16, h0_50, h0_84],
         [M_16, M_50, M_84],
-        [omega_16, omega_50, omega_84],
+        [Om_16, Om_50, Om_84],
         [w0_16, w0_50, w0_84],
-        [wa_16, wa_50, wa_84],
     ] = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
 
-    best_fit = [h0_50, M_50, omega_50, w0_50, wa_50]
+    best_fit = [h0_50, M_50, Om_50, w0_50]
 
     deg_of_freedom = z_vals.size + z_cc_vals.size - len(best_fit)
 
-    print(f"H0: {h0_50:.2f} +{(h0_84 - h0_50):.2f} -{(h0_50 - h0_16):.2f}")
+    print(f"H0: {h0_50:.1f} +{(h0_84 - h0_50):.1f} -{(h0_50 - h0_16):.1f}")
     print(f"M: {M_50:.3f} +{(M_84 - M_50):.3f} -{(M_50 - M_16):.3f}")
-    print(
-        f"Ωm: {omega_50:.3f} +{(omega_84 - omega_50):.3f} -{(omega_50 - omega_16):.3f}"
-    )
+    print(f"Ωm: {Om_50:.3f} +{(Om_84 - Om_50):.3f} -{(Om_50 - Om_16):.3f}")
     print(f"w0: {w0_50:.3f} +{(w0_84 - w0_50):.3f} -{(w0_50 - w0_16):.3f}")
-    print(f"wa: {wa_50:.3f} +{(wa_84 - wa_50):.3f} -{(wa_50 - wa_16):.3f}")
     print(f"Chi squared: {chi_squared(best_fit):.2f}")
     print(f"Degrees of freedom: {deg_of_freedom}")
 
@@ -156,12 +146,12 @@ def main():
         x=z_vals,
         y=apparent_mag_values,
         y_err=np.sqrt(np.diag(cov_matrix_sn)),
-        y_model=model_apparent_mag(best_fit),
-        label=f"Best fit: $H_0$={h0_50:.2f} km/s/Mpc, $\Omega_m$={omega_50:.3f}",
+        y_model=apparent_mag_theory(best_fit),
+        label=f"Best fit: $H_0$={h0_50:.1f} km/s/Mpc, $\Omega_m$={Om_50:.3f}",
         x_scale="log",
     )
 
-    labels = [r"$H_0$", "M", f"$\Omega_m$", r"$w_0$", r"$w_a$"]
+    labels = ["$H_0$", "M", "$\Omega_m$", "$w_0$"]
     corner.corner(
         samples,
         labels=labels,
@@ -184,45 +174,33 @@ if __name__ == "__main__":
 
 """
 Flat ΛCDM: w(z) = -1
-H0: 67.04 +3.37 -3.32 km/s/Mpc
-M: -19.445 +0.105 -0.109 mag
+H0: 66.9 +3.4 -3.3 km/s/Mpc
+M: -19.449 +0.105 -0.110
+Ωm: 0.331 +0.018 -0.017
 w0: -1
 wa: 0
-Ωm: 0.331 +0.018 -0.017
-Chi squared: 1417.45
+Chi squared: 1417.49
 Degrees of freedom: 1619
 
 ==============================
 
 Flat wCDM: w(z) = w0
-H0: 67.44 +3.64 -3.54 km/s/Mpc
-M: -19.430 +0.114 -0.118 mag
-Ωm: 0.313 +0.047 -0.053
-w0: -0.950 +0.115 -0.133
+H0: 67.4 +3.6 -3.6 km/s/Mpc
+M: -19.432 +0.113 -0.119 mag
+Ωm: 0.312 +0.047 -0.052
+w0: -0.946 +0.114 -0.129
 wa: 0
-Chi squared: 1418.03
+Chi squared: 1417.27
 Degrees of freedom: 1618
 
 ==============================
 
 Flat alternative: w(z) = w0 - (1 + w0) * (((1 + z)**2 - 1) / ((1 + z)**2 + 1))
-H0: 67.45 +3.65 -3.54 km/s/Mpc
-M: -19.429 +0.115 -0.118 mag
-Ωm: 0.316 +0.039 -0.039
-w0: -0.948 +0.107 -0.125 (0.35 - 0.41 sigma)
+H0: 67.3 +3.6 -3.5 km/s/Mpc
+M: -19.435 +0.113 -0.116 mag
+Ωm: 0.319 +0.038 -0.039
+w0: -0.956 +0.108 -0.123
 wa: 0
-Chi squared: 1417.18
+Chi squared: 1417.27
 Degrees of freedom: 1618
-
-==============================
-
-Flat w0waCDM: w(z) = w0 + wa * z/(1 + z)
-H0: 67.5435 +1.3550 -1.3776 km/s/Mpc
-M: -19.4252 +0.0413 -0.0429
-Ωm: 0.3371 +0.0529 -0.0850
-w0: -0.9321 +0.0990 -0.1089 (0.62 - 0.69 sigma)
-wa: -0.3567 +0.9664 -1.3853
-f: 0.7145 +0.1046 -0.0844 (2.73 - 3.38 sigma)
-Chi squared: 1433.5840
-Degrees of freedom: 1616
 """
