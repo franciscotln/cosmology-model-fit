8bad08cfdcf677a7633f3e12255b9b4c0e6139cb
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-06-11 10:22:01 +0200

    gp cc

--- cosmic_chronometers/cc_gp.py
+++ cosmic_chronometers/cc_gp.py
@@ -17,12 +17,8 @@ H_mean = np.mean(H_values)
 normalized_H = (H_values - H_mean) / H_std
 normalised_sigma = dH_values / H_std
 
-fixed_noise_to_avoid_singular_matrix = WhiteKernel(
-    noise_level=0.005, noise_level_bounds="fixed"
-)
-kernel = C(0.5**2) * DotProduct(sigma_0=2.5) ** 4 + fixed_noise_to_avoid_singular_matrix
 gp = GaussianProcessRegressor(
-    kernel=kernel,
+    kernel=C((1 / H_std) ** 2) * DotProduct() ** 4 + WhiteKernel(1e-4, "fixed"),
     alpha=normalised_sigma**2,
     normalize_y=False,
     n_restarts_optimizer=20,
@@ -34,29 +30,22 @@ H_pred, cov_pred = gp.predict(z_pred, return_cov=True)
 cov_pred = cov_pred * H_std**2
 H_pred = H_pred * H_std + H_mean
 sigma = np.sqrt(np.diag(cov_pred))
+likelihood = gp.log_marginal_likelihood_value_ - H_values.size * np.log(H_std)
 
-print("H0", f"{H_pred[0]:.2f} ± {sigma[0]:.2f}")
-# H0 67.3 +- 5.8 km/s/Mpc
-print("GP score:", gp.score(z_values, normalized_H))
-# 0.898
+print("H0", f"{H_pred[0]:.1f} ± {sigma[0]:.1f}")  # H0 67.4 +- 4.9 km/s/Mpc
+print("GP score:", gp.score(z_values, normalized_H))  # 0.898
+print("Likelihood", likelihood)  # -137.29
 print(
-    "GP marginal likelihood value",
-    gp.log_marginal_likelihood_value_ - H_values.size * np.log(H_std),
-)
-# -137.56
-print("GP kernel", gp.kernel_)
-# 0.0258**2 * DotProduct(sigma_0=2.66) ** 4 + WhiteKernel(noise_level=0.005)
-print("cov mat condition number", np.linalg.cond(cov_pred))
-# 703
+    "GP kernel", gp.kernel_
+)  # 0.026**2 * DotProduct(sigma_0=2.64) ** 4 + WhiteKernel(noise_level=0.0001)
 
+flat_z = z_pred.ravel()
 plt.style.use("dark_background")
-plt.errorbar(z_values, H_values, yerr=dH_values, fmt=".", label="CC Data", capsize=2)
-plt.plot(z_pred, H_pred, label="GP Mean")
+plt.errorbar(z_values, H_values, yerr=dH_values, fmt=".", label="CCH", capsize=2)
+plt.plot(flat_z, H_pred, label="GP Mean")
+plt.fill_between(flat_z, H_pred - sigma, H_pred + sigma, alpha=0.8, label=r"$1\sigma$")
 plt.fill_between(
-    z_pred.ravel(), H_pred - sigma, H_pred + sigma, alpha=0.5, label=r"$1\sigma$"
-)
-plt.fill_between(
-    z_pred.ravel(),
+    flat_z,
     H_pred - 2 * sigma,
     H_pred + 2 * sigma,
     alpha=0.3,
@@ -65,7 +54,7 @@ plt.fill_between(
 plt.xlabel("z")
 plt.xlim(0, np.max(z_values) + 0.1)
 plt.ylabel("H(z) [km/s/Mpc]")
-plt.title(f"Gaussian Process $H_0$: {H_pred[0]:.1f} ± {sigma[0]:.1f} km/s/Mpc")
+plt.title(f"GP $H_0$: {H_pred[0]:.1f} ± {sigma[0]:.1f} km/s/Mpc")
 plt.legend()
 plt.grid(True)
 plt.savefig("cosmic_chronometers/cc_gp.png", dpi=300)
