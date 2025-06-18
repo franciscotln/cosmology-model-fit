dbfc52c0eaf0b1e1facded6f9d90c0e226f9d57f
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-06-18 10:14:58 +0200

    cc with gaussian process using full covariance and learnable scaling factor for over/underestimated uncertainties

--- cosmic_chronometers/cc_gp.py
+++ cosmic_chronometers/cc_gp.py
@@ -1,47 +1,108 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import ConstantKernel as C, Matern
+import torch
+import gpytorch
+from gpytorch.models import ExactGP
+from gpytorch.kernels import ScaleKernel, RBFKernel
+from gpytorch.mlls import ExactMarginalLogLikelihood
+from gpytorch.distributions import MultivariateNormal
+from gpytorch.means import ConstantMean
+from gpytorch.priors import NormalPrior
+from .gp_lib import FixedNoiseGaussianLikelihood
 from y2005cc.data import get_data
 
-legend, z_values, H_values, cov_matrix = get_data()
-dH_values = np.sqrt(np.diag(cov_matrix))  # simple diagonal covariance matrix
-z_values = z_values.reshape(-1, 1)
+legend, z, H, cov_mat = get_data()
 
-gp = GaussianProcessRegressor(
-    kernel=C(np.mean(H_values) ** 2) * Matern(length_scale=2.5, nu=4.5),
-    alpha=dH_values**2,
-    normalize_y=False,
-    n_restarts_optimizer=20,
+h_mean = np.mean(H)
+h_std = np.std(H)
+
+z_values = torch.tensor(z, dtype=torch.float32).reshape(-1)
+H_values = torch.tensor((H - h_mean) / h_std, dtype=torch.float32).reshape(-1)
+cov_matrix = torch.tensor(cov_mat / h_std**2, dtype=torch.float32)
+
+
+class GaussianProcessModel(ExactGP):
+    def __init__(self, train_x, train_y, likelihood):
+        super(GaussianProcessModel, self).__init__(train_x, train_y, likelihood)
+        self.covar_module = ScaleKernel(
+            RBFKernel(lengthscale_prior=NormalPrior(z.max(), 0.1 * z.max()))
+        )
+        self.mean_module = ConstantMean()
+
+    def forward(self, x):
+        return MultivariateNormal(
+            mean=self.mean_module(x), covariance_matrix=self.covar_module(x)
+        )
+
+
+likelihood = FixedNoiseGaussianLikelihood(
+    noise=cov_matrix, learn_additional_noise=False, learn_noise_scale=True
 )
-gp.fit(z_values.reshape(-1, 1), H_values.reshape(-1, 1))
+model = GaussianProcessModel(train_x=z_values, train_y=H_values, likelihood=likelihood)
 
-z_pred = np.linspace(0, z_values.max(), 100).reshape(-1, 1)
-H_pred, cov_pred = gp.predict(z_pred, return_cov=True)
-sigma = np.sqrt(np.diag(cov_pred))
-likelihood = gp.log_marginal_likelihood_value_
+model.train()
+likelihood.train()
 
-print("H0", f"{H_pred[0]:.1f} ± {sigma[0]:.1f}")  # H0 67.1 +- 5.8 km/s/Mpc
-print("GP score:", gp.score(z_values, H_values))  # 0.902
-print("Likelihood", likelihood)  # -138.46
-print("GP kernel", gp.kernel_)  # 129**2 * Matern(length_scale=2.25, nu=4.5)
+optimizer = torch.optim.Adam(params=model.parameters(), lr=0.01)
+mll = ExactMarginalLogLikelihood(model.likelihood, model)
 
-flat_z = z_pred.ravel()
-plt.style.use("dark_background")
-plt.errorbar(z_values, H_values, yerr=dH_values, fmt=".", label="CCH", capsize=2)
-plt.plot(flat_z, H_pred, label="GP Mean")
-plt.fill_between(flat_z, H_pred - sigma, H_pred + sigma, alpha=0.8, label=r"$1\sigma$")
+training_iterations = 5000
+for i in range(training_iterations):
+    optimizer.zero_grad()
+    output = model(z_values)
+    loss = -mll(output, H_values)
+    loss.backward()
+    (
+        print(
+            "Iter %d/%d - Loss: %.4f | output scale: %.4f | length scale: %.4f | noise scale: %.3f"
+            % (
+                i + 1,
+                training_iterations,
+                loss.item() - H.size * np.log(h_std),
+                model.covar_module.outputscale.item() * h_std**2,
+                model.covar_module.base_kernel.lengthscale.item(),
+                model.likelihood.noise_covar.noise_scale.item(),
+            )
+        )
+        if i % 10 == 0
+        else None
+    )
+    optimizer.step()
+
+model.eval()
+likelihood.eval()
+
+X_test = torch.linspace(0, 2, 40)
+test_noise = torch.full_like(X_test, 0.0001)
+
+with torch.no_grad(), gpytorch.settings.fast_pred_var():
+    observed_pred = likelihood(model(X_test), noise=test_noise)
+    H_pred = observed_pred.mean
+    pred_var = observed_pred.variance
+    cov_pred = observed_pred.covariance_matrix * h_std**2
+    pred_std = torch.sqrt(pred_var) * h_std
+    H_pred = H_pred * h_std + h_mean
+    scale = model.likelihood.noise_covar.noise_scale.item() ** 2
+
+plt.style.use("mpl20")
+plt.errorbar(
+    x=z, y=H, yerr=np.sqrt(np.diag(cov_mat / scale)), fmt=".", label="CCH", capsize=2
+)
+plt.plot(X_test, H_pred, label="GP Mean")
 plt.fill_between(
-    flat_z,
-    H_pred - 2 * sigma,
-    H_pred + 2 * sigma,
+    X_test, H_pred - pred_std, H_pred + pred_std, alpha=0.5, label=r"$1\sigma$"
+)
+plt.fill_between(
+    X_test,
+    H_pred - 2 * pred_std,
+    H_pred + 2 * pred_std,
     alpha=0.3,
     label=r"$2\sigma$",
 )
+plt.xlim(0, 2)
 plt.xlabel("z")
-plt.xlim(0, np.max(z_values) + 0.1)
 plt.ylabel("H(z) [km/s/Mpc]")
-plt.title(f"GP $H_0$: {H_pred[0]:.1f} ± {sigma[0]:.1f} km/s/Mpc")
+plt.title(f"GP $H_0$: {H_pred[0]:.1f} ± {pred_std[0]:.1f} km/s/Mpc")
 plt.legend()
 plt.grid(True)
 plt.savefig("cosmic_chronometers/cc_gp.png", dpi=300)
---
+++ cosmic_chronometers/gp_lib.py
@@ -0,0 +1,208 @@
+from typing import Any, Optional, Union
+from copy import deepcopy
+import torch
+import gpytorch
+from gpytorch.likelihoods import _GaussianLikelihoodBase
+from gpytorch.distributions import MultivariateNormal
+from gpytorch.likelihoods.noise_models import HomoskedasticNoise
+import gpytorch.settings as settings
+import gpytorch.utils.warnings as warnings
+
+LinearOperator = gpytorch.linear_operator.operators.LinearOperator
+DenseLinearOperator = gpytorch.linear_operator.operators.DenseLinearOperator
+DiagLinearOperator = gpytorch.linear_operator.operators.DiagLinearOperator
+ZeroLinearOperator = gpytorch.linear_operator.operators.ZeroLinearOperator
+
+
+class FixedGaussianNoise(gpytorch.Module):
+    def __init__(self, noise: torch.Tensor, learn_noise_scale: bool = False) -> None:
+        """
+        noise can be either a 1D tensor (for diagonal noise)
+        or a 2D symmetric positive semi-definite matrix (for full cov).
+        """
+        super().__init__()
+
+        # Check if full covariance matrix or vector
+        if noise.ndim == 2:
+            # Assume full covariance matrix
+            if not torch.allclose(noise, noise.transpose(-1, -2)):
+                raise ValueError("Covariance matrix must be symmetric.")
+        elif noise.ndim == 1:
+            min_noise = settings.min_fixed_noise.value(noise.dtype)
+            if noise.lt(min_noise).any():
+                warnings.warn(
+                    "Very small noise values detected. Rounding small noise values up.",
+                    warnings.NumericalWarning,
+                )
+                noise = noise.clamp_min(min_noise)
+        else:
+            raise ValueError("Noise must be either a vector or a square matrix.")
+
+        self.noise = noise
+
+        self.register_parameter(
+            "noise_scale",
+            torch.nn.Parameter(torch.ones(()), requires_grad=learn_noise_scale),
+        )
+
+    def forward(
+        self,
+        *params: Any,
+        shape: Optional[torch.Size] = None,
+        noise: Optional[torch.Tensor] = None,
+        **kwargs: Any,
+    ) -> LinearOperator:
+        raw_noise = noise if noise is not None else self.noise
+        scaled_noise = raw_noise * self.noise_scale**2
+
+        if scaled_noise.ndim == 2:
+            return DenseLinearOperator(scaled_noise)
+        else:
+            return DiagLinearOperator(scaled_noise)
+
+    def _apply(self, fn):
+        self.noise = fn(self.noise)
+        return super()._apply(fn)
+
+
+class FixedNoiseGaussianLikelihood(_GaussianLikelihoodBase):
+    r"""
+    A Likelihood that assumes fixed heteroscedastic noise. This is useful when you have fixed, known observation
+    noise for each training example.
+
+    Note that this likelihood takes an additional argument when you call it, `noise`, that adds a specified amount
+    of noise to the passed MultivariateNormal. This allows for adding known observational noise to test data.
+
+    .. note::
+        This likelihood can be used for exact or approximate inference.
+
+    :param noise: Known observation noise (variance) for each training example.
+    :type noise: torch.Tensor (... x N)
+    :param learn_additional_noise: Set to true if you additionally want to
+        learn added diagonal noise, similar to GaussianLikelihood.
+    :type learn_additional_noise: bool, optional
+    :param batch_shape: The batch shape of the learned noise parameter (default
+        []) if :obj:`learn_additional_noise=True`.
+    :type batch_shape: torch.Size, optional
+
+    :var torch.Tensor noise: :math:`\sigma^2` parameter (noise)
+
+    .. note::
+        FixedNoiseGaussianLikelihood has an analytic marginal distribution.
+
+    Example:
+        >>> train_x = torch.randn(55, 2)
+        >>> noises = torch.ones(55) * 0.01
+        >>> likelihood = FixedNoiseGaussianLikelihood(noise=noises, learn_additional_noise=True)
+        >>> pred_y = likelihood(gp_model(train_x))
+        >>>
+        >>> test_x = torch.randn(21, 2)
+        >>> test_noises = torch.ones(21) * 0.02
+        >>> pred_y = likelihood(gp_model(test_x), noise=test_noises)
+    """
+
+    def __init__(
+        self,
+        noise: torch.Tensor,
+        learn_additional_noise: Optional[bool] = False,
+        learn_noise_scale: Optional[bool] = False,
+        batch_shape: Optional[torch.Size] = torch.Size(),
+        **kwargs: Any,
+    ) -> None:
+        super().__init__(
+            noise_covar=FixedGaussianNoise(
+                noise=noise, learn_noise_scale=learn_noise_scale
+            )
+        )
+
+        # super().__init__(noise_covar=FixedGaussianNoise(noise=noise))
+        self.second_noise_covar: Optional[HomoskedasticNoise] = None
+        if learn_additional_noise:
+            noise_prior = kwargs.get("noise_prior", None)
+            noise_constraint = kwargs.get("noise_constraint", None)
+            self.second_noise_covar = HomoskedasticNoise(
+                noise_prior=noise_prior,
+                noise_constraint=noise_constraint,
+                batch_shape=batch_shape,
+            )
+
+    @property
+    def noise(self) -> torch.Tensor:
+        return self.noise_covar.noise + self.second_noise
+
+    @noise.setter
+    def noise(self, value: torch.Tensor) -> None:
+        self.noise_covar.initialize(noise=value)
+
+    @property
+    def second_noise(self) -> Union[float, torch.Tensor]:
+        if self.second_noise_covar is None:
+            return 0.0
+        else:
+            return self.second_noise_covar.noise
+
+    @second_noise.setter
+    def second_noise(self, value: torch.Tensor) -> None:
+        if self.second_noise_covar is None:
+            raise RuntimeError(
+                "Attempting to set secondary learned noise for FixedNoiseGaussianLikelihood, "
+                "but learn_additional_noise must have been False!"
+            )
+        self.second_noise_covar.initialize(noise=value)
+
+    def get_fantasy_likelihood(self, **kwargs: Any) -> "FixedNoiseGaussianLikelihood":
+        if "noise" not in kwargs:
+            raise RuntimeError(
+                "FixedNoiseGaussianLikelihood.fantasize requires a `noise` kwarg"
+            )
+
+        old_noise_covar = self.noise_covar
+        self.noise_covar = None
+        fantasy_liklihood = deepcopy(self)
+        self.noise_covar = old_noise_covar
+
+        old_noise = old_noise_covar.noise
+        new_noise = kwargs.get("noise")
+
+        if old_noise.ndim == 1 and new_noise.ndim == 1:
+            cat_noise = torch.cat([old_noise, new_noise], -1)
+        elif old_noise.ndim == 2 and new_noise.ndim == 2:
+            cat_noise = torch.block_diag(old_noise, new_noise)
+        else:
+            raise ValueError(
+                "Noise tensors must have the same dimensions (both 1D or both 2D)."
+            )
+
+        fantasy_liklihood.noise_covar = FixedGaussianNoise(noise=cat_noise)
+        return fantasy_liklihood
+
+    def _shaped_noise_covar(
+        self, base_shape: torch.Size, *params: Any, **kwargs: Any
+    ) -> Union[torch.Tensor, LinearOperator]:
+        if len(params) > 0:
+            # we can infer the shape from the params
+            shape = None
+        else:
+            # here shape[:-1] is the batch shape requested, and shape[-1] is `n`, the number of points
+            shape = base_shape
+
+        res = self.noise_covar(*params, shape=shape, **kwargs)
+
+        if self.second_noise_covar is not None:
+            res = res + self.second_noise_covar(*params, shape=shape, **kwargs)
+        elif isinstance(res, ZeroLinearOperator):
+            warnings.warn(
+                "You have passed data through a FixedNoiseGaussianLikelihood that did not match the size "
+                "of the fixed noise, *and* you did not specify noise. This is treated as a no-op.",
+                warnings.GPInputWarning,
+            )
+
+        return res
+
+    def marginal(
+        self, function_dist: MultivariateNormal, *args: Any, **kwargs: Any
+    ) -> MultivariateNormal:
+        r"""
+        :return: Analytic marginal :math:`p(\mathbf y)`.
+        """
+        return super().marginal(function_dist, *args, **kwargs)
