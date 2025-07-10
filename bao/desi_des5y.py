653ab247c36c910f8f9c764edbb90c1a583571a4
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-07-10 10:03:44 +0200

    remove prior from desi_des5y

--- bao/desi_des5y.py
+++ bao/desi_des5y.py
@@ -29,17 +29,17 @@ grid = np.linspace(0, np.max(z_cmb), num=2000)
 
 def integral_Ez(params):
     x = grid
-    y = 1 / Ez(grid, *params[3:])
+    y = 1 / Ez(grid, *params[2:])
     return np.interp(z_cmb, grid, cumulative_trapezoid(y=y, x=x, initial=0))
 
 
 def theory_mu(params):
-    dM, H0 = params[0], params[2]
-    return dM + 25 + 5 * np.log10((1 + z_hel) * (c / H0) * integral_Ez(params))
+    dL = (1 + z_hel) * c * integral_Ez(params)
+    return params[0] + 25 + 5 * np.log10(dL)
 
 
 def H_z(z, params):
-    return params[2] * Ez(z, *params[3:])
+    return Ez(z, *params[2:])
 
 
 def DH_z(z, params):
@@ -57,9 +57,9 @@ def DV_z(z, params):
 
 
 bao_quantity_funcs = {
-    "DV_over_rs": lambda z, params: DV_z(z, params) / params[1],
-    "DM_over_rs": lambda z, params: DM_z(z, params) / params[1],
-    "DH_over_rs": lambda z, params: DH_z(z, params) / params[1],
+    "DV_over_rs": lambda z, params: DV_z(z, params) / (params[1] * 100),
+    "DM_over_rs": lambda z, params: DM_z(z, params) / (params[1] * 100),
+    "DH_over_rs": lambda z, params: DH_z(z, params) / (params[1] * 100),
 }
 
 
@@ -80,21 +80,17 @@ def chi_squared(params):
 
 bounds = np.array(
     [
-        (-0.5, 0.5),  # ΔM
-        (115, 160),  # r_d
-        (50, 80),  # H0
+        (-10, -8.5),  # ΔM
+        (90, 110),  # r_d * h
         (0.1, 0.7),  # Ωm
-        (-3, 0),  # w0
+        (-2, 0),  # w0
     ]
 )
 
 
-# Prior from Planck 2018 https://arxiv.org/abs/1807.06209 table 1 (Combined column)
-# Ωm x ​h^2 = 0.1428 ± 0.0011. Prior width increased by 70% to 0.00187
 def log_prior(params):
     if np.all((bounds[:, 0] < params) & (params < bounds[:, 1])):
-        Om_x_h2 = params[3] * (params[2] / 100) ** 2
-        return -0.5 * ((0.1428 - Om_x_h2) / 0.00187) ** 2
+        return 0
     return -np.inf
 
 
@@ -135,16 +131,14 @@ def main():
     [
         [dM_16, dM_50, dM_84],
         [rd_16, rd_50, rd_84],
-        [H0_16, H0_50, H0_84],
         [Om_16, Om_50, Om_84],
         [w0_16, w0_50, w0_84],
     ] = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
 
-    best_fit = [dM_50, rd_50, H0_50, Om_50, w0_50]
+    best_fit = [dM_50, rd_50, Om_50, w0_50]
 
     print(f"ΔM: {dM_50:.3f} +{(dM_84 - dM_50):.3f} -{(dM_50 - dM_16):.3f}")
-    print(f"r_d: {rd_50:.2f} +{(rd_84 - rd_50):.2f} -{(rd_50 - rd_16):.2f}")
-    print(f"H0: {H0_50:.2f} +{(H0_84 - H0_50):.2f} -{(H0_50 - H0_16):.2f}")
+    print(f"r_d * h: {rd_50:.2f} +{(rd_84 - rd_50):.2f} -{(rd_50 - rd_16):.2f}")
     print(f"Ωm: {Om_50:.3f} +{(Om_84 - Om_50):.3f} -{(Om_50 - Om_16):.3f}")
     print(f"w0: {w0_50:.3f} +{(w0_84 - w0_50):.3f} -{(w0_50 - w0_16):.3f}")
     print(f"Chi squared: {chi_squared(best_fit):.2f}")
@@ -154,7 +148,7 @@ def main():
         theory_predictions=lambda z, qty: theory_predictions(z, qty, best_fit),
         data=bao_data,
         errors=np.sqrt(np.diag(cov_matrix_bao)),
-        title=f"{bao_legend}: $r_d$={rd_50:.2f} Mpc",
+        title=bao_legend,
     )
     plot_sn_predictions(
         legend=sn_legend,
@@ -162,11 +156,11 @@ def main():
         y=mu_values,
         y_err=np.sqrt(np.diag(cov_matrix_sn)),
         y_model=theory_mu(best_fit),
-        label=f"Model: $H_0$={H0_50:.3f}",
+        label=f"Model: $Ω_m$={Om_50:.3f}",
         x_scale="log",
     )
 
-    labels = ["$Δ_M$", "$r_d$", "$H_0$", "$Ω_M$", "$w_0$"]
+    labels = ["$Δ_M$", "$r_d x h$", "$Ω_M$", "$w_0$"]
     corner.corner(
         samples,
         labels=labels,
@@ -198,9 +192,8 @@ if __name__ == "__main__":
 
 """
 Flat ΛCDM
-ΔM: -0.073 +0.027 -0.027 mag
-r_d: 148.25 +1.49 -1.46 Mpc
-H0: 67.82 +0.98 -0.97 km/s/Mpc
+ΔM: -9.230 +0.006 -0.006 mag
+r_d * h: 100.55 +0.67 -0.66 Mpc
 Ωm: 0.310 +0.008 -0.008
 w0: -1
 wa: 0
@@ -210,36 +203,22 @@ Degrees of freedom: 1838
 ==============================
 
 Flat wCDM
-ΔM: 0.002 +0.038 -0.037 mag
-r_d: 142.77 +2.30 -2.37 Mpc
-H0: 69.26 +1.14 -1.12 km/s/Mpc
+ΔM: -9.200 +0.011 -0.011 mag
+r_d * h: 98.85 +0.83 -0.81 Mpc
 Ωm: 0.298 +0.009 -0.009
-w0: -0.871 +0.038 -0.038
+w0: -0.872 +0.039 -0.038 (3.28 - 3.37 sigma)
 wa: 0
-Chi squared: 1648.10
-Degrees of freedom: 1837
+Chi squared: 1648.11 (Δ chi2 10.86)
+Degrees of freedom: 1838
 
 ===============================
 
 Flat w0 - (1 + w0) * (((1 + z)**2 - 1) / ((1 + z)**2 + 1))
-ΔM: -0.020 +0.031 -0.031 mag
-r_d: 144.31 +1.86 -1.84 Mpc
-H0: 68.40 +1.00 -0.98 km/s/Mpc
+ΔM: -9.195 +0.012 -0.012 mag
+r_d * h: 98.71 +0.85 -0.83 Mpc
 Ωm: 0.305 +0.008 -0.008
-w0: -0.850 +0.042 -0.042 (3.57 sigma)
+w0: -0.850 +0.042 -0.043 (3.49 - 3.57 sigma)
 wa: 0
-Chi squared: 1646.98
-Degrees of freedom: 1837
-
-===============================
-
-Flat w0waCDM
-ΔM: -0.071 +0.047 -0.038 mag
-r_d: 148.08 +2.59 -3.25 Mpc
-H0: 66.54 +1.67 -1.34 km/s/Mpc
-Ωm: 0.322 +0.013 -0.015
-w0: -0.783 +0.074 -0.068
-wa: -0.729 +0.454 -0.458
-Chi squared: 1645.45
-Degrees of freedom: 1836
+Chi squared: 1646.98 (Δ chi2 11.99)
+Degrees of freedom: 1838
 """
