7092f6455802c273b3111748320fce0389fdedb3
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-09-29 11:04:40 +0200

    desi_pantheon_cc with f_cc

--- bao/desi_pantheon_cc.py
+++ bao/desi_pantheon_cc.py
@@ -16,8 +16,10 @@ cc_legend, z_cc_vals, H_cc_vals, cov_matrix_cc = get_cc_data()
 legend, z_sn_vals, z_sn_hel_vals, apparent_mag_values, cov_matrix_sn = get_data()
 bao_legend, bao_data, cov_matrix_bao = get_bao_data()
 cho_cc = cho_factor(cov_matrix_cc)
-cho_sn = cho_factor(cov_matrix_sn)
 cho_bao = cho_factor(cov_matrix_bao)
+cho_sn = cho_factor(cov_matrix_sn)
+logdet_cc = np.linalg.slogdet(cov_matrix_cc)[1]
+N_cc = len(z_cc_vals)
 
 c = 299792.458  # Speed of light in km/s
 
@@ -30,7 +32,8 @@ def Ez(z, params):
     O_m, w0 = params[3], params[4]
     one_plus_z = 1 + z
     cubed = one_plus_z**3
-    rho_de = (2 * cubed / (1 + cubed)) ** (2 * (1 + w0))
+    # rho_de = (2 * cubed / (1 + cubed)) ** (2 * (1 + w0))
+    rho_de = cubed**(1 + w0)  # wCDM
     return np.sqrt(O_m * cubed + (1 - O_m) * rho_de)
 
 
@@ -99,12 +102,14 @@ bounds = np.array(
         (115, 170),  # r_d
         (0.15, 0.7),  # Ωm
         (-3, 0),  # w0
+        (0.4, 2.5),  # f_cc
     ],
     dtype=np.float64,
 )
 
 
 def chi_squared(params):
+    f_cc = params[-1]
     delta_sn = apparent_mag_values - sn_apparent_mag(params)
     chi_sn = np.dot(delta_sn, cho_solve(cho_sn, delta_sn, check_finite=False))
 
@@ -112,7 +117,7 @@ def chi_squared(params):
     chi_bao = np.dot(delta_bao, cho_solve(cho_bao, delta_bao, check_finite=False))
 
     delta_cc = H_cc_vals - H_z(z_cc_vals, params)
-    chi_cc = np.dot(delta_cc, cho_solve(cho_cc, delta_cc, check_finite=False))
+    chi_cc = f_cc**2 * np.dot(delta_cc, cho_solve(cho_cc, delta_cc, check_finite=False))
 
     return chi_sn + chi_bao + chi_cc
 
@@ -125,7 +130,9 @@ def log_prior(params):
 
 
 def log_likelihood(params):
-    return -0.5 * chi_squared(params)
+    f_cc = params[-1]
+    normalization_cc = N_cc * np.log(2 * np.pi) + logdet_cc - 2 * N_cc * np.log(f_cc)
+    return -0.5 * chi_squared(params) - 0.5 * normalization_cc
 
 
 def log_probability(params):
@@ -137,15 +144,25 @@ def log_probability(params):
 
 def main():
     ndim = len(bounds)
-    nwalkers = 8 * ndim
-    burn_in = 500
-    nsteps = 12500 + burn_in
+    nwalkers = 85 * ndim
+    burn_in = 100
+    nsteps = 1400 + burn_in
     initial_pos = np.random.default_rng().uniform(
         bounds[:, 0], bounds[:, 1], size=(nwalkers, ndim)
     )
 
     with Pool(10) as pool:
-        sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, pool=pool)
+        sampler = emcee.EnsembleSampler(
+            nwalkers,
+            ndim,
+            log_probability,
+            pool=pool,
+            moves=[
+                (emcee.moves.KDEMove(), 0.5),
+                (emcee.moves.DEMove(), 0.4),
+                (emcee.moves.DESnookerMove(), 0.1),
+            ],
+        )
         sampler.run_mcmc(initial_pos, nsteps, progress=True)
 
     try:
@@ -162,14 +179,16 @@ def main():
         [rd_16, rd_50, rd_84],
         [omega_16, omega_50, omega_84],
         [w0_16, w0_50, w0_84],
+        [f_cc_16, f_cc_50, f_cc_84],
     ] = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
 
-    best_fit = [h0_50, M_50, rd_50, omega_50, w0_50]
+    best_fit = [h0_50, M_50, rd_50, omega_50, w0_50, f_cc_50]
 
     deg_of_freedom = (
         z_sn_vals.size + bao_data["value"].size + z_cc_vals.size - len(best_fit)
     )
 
+    print(f"f_cc: {f_cc_50:.2f} +{(f_cc_84 - f_cc_50):.2f} -{(f_cc_50 - f_cc_16):.2f}")
     print(f"H0: {h0_50:.2f} +{(h0_84 - h0_50):.2f} -{(h0_50 - h0_16):.2f}")
     print(f"M: {M_50:.3f} +{(M_84 - M_50):.3f} -{(M_50 - M_16):.3f}")
     print(f"r_d: {rd_50:.2f} +{(rd_84 - rd_50):.2f} -{(rd_50 - rd_16):.2f}")
@@ -196,7 +215,7 @@ def main():
         x_scale="log",
     )
 
-    labels = ["$H_0$", "M", "$r_d$", "Ωm", "$w_0$"]
+    labels = ["$H_0$", "M", "$r_d$", "Ωm", "$w_0$", "$f_{CC}$"]
     corner.corner(
         samples,
         labels=labels,
@@ -212,6 +231,16 @@ def main():
     )
     plt.show()
 
+    _, axes = plt.subplots(ndim, figsize=(10, 7), sharex=True)
+    chains_samples = sampler.get_chain(discard=0, flat=False)
+    for i in range(ndim):
+        axes[i].plot(chains_samples[:, :, i], color="black", alpha=0.3)
+        axes[i].set_ylabel(labels[i])
+        axes[i].axvline(x=burn_in, color="red", linestyle="--", alpha=0.5)
+        axes[i].axhline(y=best_fit[i], color="white", linestyle="--", alpha=0.5)
+    axes[ndim - 1].set_xlabel("chain step")
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
@@ -219,33 +248,36 @@ if __name__ == "__main__":
 
 """
 Flat ΛCDM: w(z) = -1
-H0: 68.52 +3.36 -3.24
-M: -19.406 +0.104 -0.105
-r_d: 147.35 +7.25 -6.80
+f_cc: 1.47 +0.19 -0.18
+H0: 68.6 +2.3 -2.3 km/s/Mpc
+M: -19.40 +0.07 -0.07 mag
+r_d: 147.1 +4.9 -4.6 Mpc
 Ωm: 0.305 +0.008 -0.008
 w0: -1
-Chi squared: 1431.02
-Degrees of freedom: 1632
+Chi squared: 1448.49
+Degrees of freedom: 1631
 
 ===============================
 
 Flat wCDM: w(z) = w0
-H0: 67.73 +3.23 -3.26
-M: -19.419 +0.100 -0.107
-r_d: 147.29 +7.33 -6.56
+f_cc: 1.47 +0.18 -0.18
+H0: 67.8 +2.3 -2.3 km/s/Mpc
+M: -19.417 +0.071 -0.074 mag
+r_d: 147.1 +5.0 -4.7 Mpc
 Ωm: 0.298 +0.009 -0.009
-w0: -0.915 +0.039 -0.040
-Chi squared: 1426.55 (Δ chi2 = 4.47 from ΛCDM)
-Degrees of freedom: 1631
+w0: -0.917 +0.040 -0.040
+Chi squared: 1443.87 (Δ chi2 = 4.62 from ΛCDM)
+Degrees of freedom: 1630
 
 ===============================
 
 Flat: w(z) = -1 + 2 * (1 + w0) / (1 + (1 + z)**3)
-H0: 67.80 +3.30 -3.23 km/s/Mpc
-M: -19.415 +0.102 -0.106 mag
-r_d: 147.09 +7.27 -6.74 Mpc
+f_cc: 1.47 +0.18 -0.18
+H0: 67.8 +2.3 -2.3 km/s/Mpc
+M: -19.42 +0.07 -0.07 mag
+r_d: 147.0 +5.0 -4.6 Mpc
 Ωm: 0.304 +0.008 -0.008
-w0: -0.898 +0.047 -0.047
-Chi squared: 1426.36 (Δ chi2 = 4.66 from ΛCDM)
-Degrees of freedom: 1631
+w0: -0.900 +0.047 -0.048
+Chi squared: 1443.62 (Δ chi2 = 4.87 from ΛCDM)
+Degrees of freedom: 1630
 """
