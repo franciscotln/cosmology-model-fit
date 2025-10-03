c07f021f01359c812718f903c2ac57343f5a08a0
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-10-03 20:04:25 +0200

    bao+bbn+theta*

---
+++ bao/desi_bbn_theta_star.py
@@ -0,0 +1,294 @@
+from numba import njit
+import numpy as np
+import emcee
+import corner
+from scipy.constants import c as c0
+from scipy.linalg import cho_factor, cho_solve
+import matplotlib.pyplot as plt
+from multiprocessing import Pool
+from y2025BAO.data import get_data as get_bao_data
+import cmb.data_chen_compression as cmb
+import y2024BBN.prior_lcdm as bbn
+from .plot_predictions import plot_bao_predictions
+
+c = c0 / 1000  # speed of light in km/s
+Or_h2 = cmb.Omega_r_h2()
+
+bao_legend, bao_data, bao_cov_matrix = get_bao_data()
+cho_bao = cho_factor(bao_cov_matrix)
+
+# arXiv:2503.14738v2
+theta_100 = 1.04110
+theta_100_err = 0.00053
+
+
+@njit
+def Ez(z, params):
+    H0, Om, w0 = params[0], params[1], params[3]
+    h = H0 / 100
+    Or = Or_h2 / h**2
+    Ode = 1 - Om - Or
+
+    one_plus_z = 1 + z
+    cubed = one_plus_z**3
+    rho_de = (2 * cubed / (1 + cubed)) ** (2 * (1 + w0))
+    return np.sqrt(Or * one_plus_z**4 + Om * cubed + Ode * rho_de)
+
+
+@njit
+def H_z(z, params):
+    return params[0] * Ez(z, params)
+
+
+@njit
+def DH_z(z, params):
+    return c / H_z(z, params)
+
+
+@njit
+def DM_z(z, params):
+    result = np.empty(z.size, dtype=np.float64)
+    for i in range(z.size):
+        zp = z[i]
+        x = np.linspace(0, zp, num=max(250, int(250 * zp)))
+        y = DH_z(x, params)
+        result[i] = np.trapz(y=y, x=x)
+    return result
+
+
+@njit
+def DV_z(z, params):
+    DH = DH_z(z, params)
+    DM = DM_z(z, params)
+    return (z * DH * DM**2) ** (1 / 3)
+
+
+qty_map = {
+    "DV_over_rs": 0,
+    "DM_over_rs": 1,
+    "DH_over_rs": 2,
+}
+
+quantities = np.array([qty_map[q] for q in bao_data["quantity"]], dtype=np.int32)
+
+
+@njit
+def bao_predictions(z, qty, params):
+    H0, Om, Obh2 = params[0], params[1], params[2]
+    Omh2 = Om * (H0 / 100) ** 2
+    rd = cmb.r_drag(wb=Obh2, wm=Omh2)
+
+    results = np.empty(z.size, dtype=np.float64)
+    DV_mask = qty == 0
+    DM_mask = qty == 1
+    DH_mask = qty == 2
+    results[DH_mask] = DH_z(z[DH_mask], params)
+    results[DM_mask] = DM_z(z[DM_mask], params)
+    results[DV_mask] = DV_z(z[DV_mask], params)
+    return results / rd
+
+
+def theta_100_theory(params):
+    H0, Om, wb = params[0], params[1], params[2]
+    wm = Om * (H0 / 100) ** 2
+    zstar = cmb.z_star(wb, wm)
+    rs_star = cmb.rs_z(Ez, zstar, params, H0, wb)
+    DA_star = cmb.DA_z(Ez, zstar, params, H0)
+    return 100 * rs_star / ((1 + zstar) * DA_star)
+
+
+def chi_squared(params):
+    delta_bbn = bbn.Obh2 - params[2]
+    chi2_bbn = (delta_bbn / bbn.Obh2_sigma) ** 2
+
+    delta_theta_100 = theta_100 - theta_100_theory(params)
+    chi2_theta_100 = (delta_theta_100 / theta_100_err) ** 2
+
+    delta_bao = bao_data["value"] - bao_predictions(bao_data["z"], quantities, params)
+    chi_bao = delta_bao.dot(cho_solve(cho_bao, delta_bao, check_finite=False))
+
+    return chi2_bbn + chi2_theta_100 + chi_bao
+
+
+bounds = np.array(
+    [
+        (55, 75),  # H0
+        (0.20, 0.50),  # Ωm
+        (0.020, 0.025),  # Ωb * h^2
+        (-1.5, 0),  # w0
+    ],
+    dtype=np.float64,
+)
+
+
+@njit
+def log_prior(params):
+    if np.all((bounds[:, 0] < params) & (params < bounds[:, 1])):
+        return 0
+    return -np.inf
+
+
+def log_likelihood(params):
+    return -0.5 * chi_squared(params)
+
+
+def log_probability(params):
+    lp = log_prior(params)
+    if np.isinf(lp):
+        return -np.inf
+    return lp + log_likelihood(params)
+
+
+def main():
+    ndim = len(bounds)
+    nwalkers = 150
+    burn_in = 200
+    nsteps = 2000 + burn_in
+    initial_pos = np.zeros((nwalkers, ndim))
+
+    for dim, (lower, upper) in enumerate(bounds):
+        initial_pos[:, dim] = np.random.uniform(lower, upper, nwalkers)
+
+    with Pool(5) as pool:
+        sampler = emcee.EnsembleSampler(
+            nwalkers,
+            ndim,
+            log_probability,
+            pool=pool,
+            moves=[
+                (emcee.moves.KDEMove(), 0.30),
+                (emcee.moves.DEMove(), 0.56),
+                (emcee.moves.DESnookerMove(), 0.14),
+            ],
+        )
+        sampler.run_mcmc(initial_pos, nsteps, progress=True)
+
+    try:
+        tau = sampler.get_autocorr_time()
+        print("auto-correlation time", tau)
+        print("acceptance fraction", np.mean(sampler.acceptance_fraction))
+        print("effective samples", ndim * nwalkers * nsteps / np.max(tau))
+    except emcee.autocorr.AutocorrError as e:
+        print("Autocorrelation time could not be computed", e)
+
+    chains_samples = sampler.get_chain(discard=burn_in, flat=False)
+    samples = sampler.get_chain(discard=burn_in, flat=True)
+
+    pct = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
+    H0_16, H0_50, H0_84 = pct[0]
+    Om_16, Om_50, Om_84 = pct[1]
+    Obh2_16, Obh2_50, Obh2_84 = pct[2]
+    w0_16, w0_50, w0_84 = pct[3]
+
+    best_fit = np.array([H0_50, Om_50, Obh2_50, w0_50], dtype=np.float64)
+
+    Om_h2_samples = samples[:, 1] * (samples[:, 0] / 100) ** 2
+    z_st_samples = cmb.z_star(samples[:, 2], Om_h2_samples)
+    z_dr_samples = cmb.z_drag(samples[:, 2], Om_h2_samples)
+    r_drag_samples = cmb.r_drag(samples[:, 2], Om_h2_samples)
+
+    Omh2_16, Omh2_50, Omh2_84 = np.percentile(Om_h2_samples, [15.9, 50, 84.1])
+    r_d_16, r_d_50, r_d_84 = np.percentile(r_drag_samples, [15.9, 50, 84.1])
+    z_st_16, z_st_50, z_st_84 = np.percentile(z_st_samples, [15.9, 50, 84.1])
+    z_d_16, z_d_50, z_d_84 = np.percentile(z_dr_samples, [15.9, 50, 84.1])
+
+    print(f"H0: {H0_50:.2f} +{(H0_84 - H0_50):.2f} -{(H0_50 - H0_16):.2f} km/s/Mpc")
+    print(f"Ωm: {Om_50:.3f} +{(Om_84 - Om_50):.3f} -{(Om_50 - Om_16):.3f}")
+    print(
+        f"Ωm h^2: {Omh2_50:.5f} +{(Omh2_84 - Omh2_50):.5f} -{(Omh2_50 - Omh2_16):.5f}"
+    )
+    print(
+        f"Ωb h^2: {Obh2_50:.5f} +{(Obh2_84 - Obh2_50):.5f} -{(Obh2_50 - Obh2_16):.5f}"
+    )
+    print(f"w0: {w0_50:.3f} +{(w0_84 - w0_50):.3f} -{(w0_50 - w0_16):.3f}")
+    print(f"z*: {z_st_50:.2f} +{(z_st_84 - z_st_50):.2f} -{(z_st_50 - z_st_16):.2f}")
+    print(f"z_drag: {z_d_50:.2f} +{(z_d_84 - z_d_50):.2f} -{(z_d_50 - z_d_16):.2f}")
+    print(f"r_s(z*) = {cmb.rs_z(Ez, z_st_50, best_fit, H0_50, Obh2_50):.2f} Mpc")
+    print(
+        f"r_s(z_drag) = {r_d_50:.2f} +{(r_d_84 - r_d_50):.2f} -{(r_d_50 - r_d_16):.2f} Mpc"
+    )
+    print(f"Chi squared: {chi_squared(best_fit):.2f}")
+
+    plot_bao_predictions(
+        theory_predictions=lambda z, qty: bao_predictions(z, qty, best_fit),
+        data=bao_data,
+        errors=np.sqrt(np.diag(bao_cov_matrix)),
+        title=bao_legend,
+    )
+    labels = ["$H_0$", "$Ω_m$", "$Ω_b h^2$", "$w_0$"]
+    corner.corner(
+        samples,
+        labels=labels,
+        quantiles=[0.159, 0.5, 0.841],
+        show_titles=True,
+        title_fmt=".4f",
+        bins=100,
+        fill_contours=False,
+        plot_datapoints=False,
+        smooth=2.0,
+        smooth1d=2.0,
+        levels=(0.393, 0.864),
+    )
+    plt.show()
+
+    plt.figure(figsize=(16, 1.5 * ndim))
+    for n in range(ndim):
+        plt.subplot2grid((ndim, 1), (n, 0))
+        plt.plot(chains_samples[:, :, n], alpha=0.3)
+        plt.ylabel(labels[n])
+        plt.xlim(0, None)
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+
+"""
+*******************************
+Dataset: DESI DR2 2024 + θ∗ + BBN
+*******************************
+
+Flat ΛCDM w(z) = -1
+H0: 68.60 +0.46 -0.47 km/s/Mpc
+Ωm: 0.295 +0.004 -0.004
+Ωm h^2: 0.13898 +0.00119 -0.00118
+Ωb h^2: 0.02216 +0.00052 -0.00053
+w0: -0.752 +0.504 -0.503
+z*: 1088.80 +0.55 -0.52
+z_drag: 1059.13 +1.24 -1.26
+r_s(z*) = 145.53 Mpc
+r_s(z_drag) = 148.19 +0.71 -0.71 Mpc
+Chi squared: 10.35
+Degs of freedom: 12
+
+===============================
+
+Flat wCDM w(z) = w0
+H0: 67.75 +1.12 -1.08 km/s/Mpc
+Ωm: 0.301 +0.008 -0.008
+Ωm h^2: 0.13794 +0.00170 -0.00175
+Ωb h^2: 0.02222 +0.00055 -0.00054
+w0: -0.960 +0.047 -0.048
+z*: 1088.67 +0.58 -0.56
+z_drag: 1059.18 +1.27 -1.27
+r_s(z*) = 145.78 Mpc
+r_s(z_drag) = 148.43 +0.77 -0.76 Mpc
+Chi squared: 9.58
+Degs of freedom: 11
+
+===============================
+
+Flat w(z) = -1 + 2 * (1 + w0) / (1 + (1 + z)**3)
+H0: 66.95 +1.50 -1.41 km/s/Mpc
+Ωm: 0.308 +0.012 -0.012
+Ωm h^2: 0.13809 +0.00142 -0.00140
+Ωb h^2: 0.02223 +0.00054 -0.00054
+w0: -0.887 +0.095 -0.098
+z*: 1088.66 +0.56 -0.55
+z_drag: 1059.22 +1.27 -1.27
+r_s(z*) = 145.73 Mpc
+r_s(z_drag) = 148.37 +0.73 -0.73 Mpc
+Chi squared: 8.95
+Degs of freedom: 11
+"""
---
+++ bao/desi_omh2.py
@@ -0,0 +1,247 @@
+from numba import njit
+import numpy as np
+import emcee
+import corner
+from scipy.constants import c as c0
+import matplotlib.pyplot as plt
+from y2025BAO.data import get_data
+from .plot_predictions import plot_bao_predictions, plot_bao_residuals
+
+c = c0 / 1000  # Speed of light in km/s
+
+legend, data, cov_matrix = get_data()
+cho = np.linalg.cholesky(cov_matrix)
+cho_T = cho.T
+
+# Planck prior
+Omh2_planck = 0.1432
+Omh2_planck_sigma = 0.0013 * 2.0  # 2 sigma
+
+
+@njit
+def H_z(z, params):
+    h, Om, w0, _ = params
+    OL = 1 - Om
+    one_plus_z = 1 + z
+    cubed = one_plus_z**3
+    rho_de = (2 * cubed / (1 + cubed)) ** (2 * (1 + w0))
+    return 100 * h * np.sqrt(Om * cubed + OL * rho_de)
+
+
+@njit
+def DH_z(z, params):
+    return c / H_z(z, params)
+
+
+@njit
+def DM_z(z, params):
+    result = np.empty(z.size, dtype=np.float64)
+    for i in range(z.size):
+        zp = z[i]
+        x = np.linspace(0, zp, num=max(250, int(250 * zp)))
+        y = DH_z(x, params)
+        result[i] = np.trapz(y=y, x=x)
+    return result
+
+
+@njit
+def DV_z(z, params):
+    DH = DH_z(z, params)
+    DM = DM_z(z, params)
+    return (z * DH * DM**2) ** (1 / 3)
+
+
+@njit
+def bao_theory(z, qty, params):
+    DV_mask = qty == 0
+    DM_mask = qty == 1
+    DH_mask = qty == 2
+    results = np.empty(z.size, dtype=np.float64)
+    results[DH_mask] = DH_z(z[DH_mask], params)
+    results[DM_mask] = DM_z(z[DM_mask], params)
+    results[DV_mask] = DV_z(z[DV_mask], params)
+    return results / params[3]
+
+
+bounds = np.array(
+    [
+        (0.500, 0.800),  # h
+        (0.100, 0.500),  # Ωm
+        (-2.00, 0.00),  # w0
+        (130.0, 160.0),  # rd
+    ],
+    dtype=np.float64,
+)
+
+
+qty_map = {
+    "DV_over_rs": 0,
+    "DM_over_rs": 1,
+    "DH_over_rs": 2,
+}
+
+quantities = np.array([qty_map[q] for q in data["quantity"]], dtype=np.int32)
+
+
+@njit
+def chi_squared(params):
+    Omh2 = params[1] * params[0] ** 2
+    Omh2_prior = (Omh2 - Omh2_planck) / Omh2_planck_sigma
+    chi2_prior = Omh2_prior**2
+
+    delta = data["value"] - bao_theory(data["z"], quantities, params)
+    y = np.linalg.solve(cho, delta)
+    x = np.linalg.solve(cho_T, y)
+    return np.dot(delta, x) + chi2_prior
+
+
+@njit
+def log_prior(params):
+    if not np.all((bounds[:, 0] < params) & (params < bounds[:, 1])):
+        return -np.inf
+    return 0.0
+
+
+@njit
+def log_likelihood(params):
+    return -0.5 * chi_squared(params)
+
+
+def log_probability(params):
+    lp = log_prior(params)
+    if np.isinf(lp):
+        return -np.inf
+    return lp + log_likelihood(params)
+
+
+def main():
+    n_dim = len(bounds)
+    n_walkers = 160
+    burn_in = 200
+    nsteps = 2000 + burn_in
+    initial_pos = np.zeros((n_walkers, n_dim))
+
+    for dim, (lower, upper) in enumerate(bounds):
+        initial_pos[:, dim] = np.random.uniform(lower, upper, n_walkers)
+
+    sampler = emcee.EnsembleSampler(
+        n_walkers,
+        n_dim,
+        log_probability,
+        moves=[
+            (emcee.moves.KDEMove(), 0.30),
+            (emcee.moves.DEMove(), 0.56),
+            (emcee.moves.DESnookerMove(), 0.14),
+        ],
+    )
+    sampler.run_mcmc(initial_pos, nsteps, progress=True)
+
+    try:
+        tau = sampler.get_autocorr_time()
+        print("Auto-correlation time:", tau)
+        print("Effective samples:", n_dim * n_walkers * nsteps / np.max(tau))
+        print("Acceptance fraction:", np.mean(sampler.acceptance_fraction))
+    except emcee.autocorr.AutocorrError as e:
+        print("Autocorrelation time could not be computed", e)
+
+    samples = sampler.get_chain(discard=burn_in, flat=True)
+
+    [
+        [h_16, h_50, h_84],
+        [Om_16, Om_50, Om_84],
+        [w0_16, w0_50, w0_84],
+        [rd_16, rd_50, rd_84],
+    ] = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
+
+    best_fit = np.array([h_50, Om_50, w0_50, rd_50], dtype=np.float64)
+    residuals = data["value"] - bao_theory(data["z"], quantities, best_fit)
+    SS_res = np.sum(residuals**2)
+    SS_tot = np.sum((data["value"] - np.mean(data["value"])) ** 2)
+    r2 = 1 - SS_res / SS_tot
+
+    print(f"h: {h_50:.3f} +{(h_84 - h_50):.3f} -{(h_50 - h_16):.3f}")
+    print(f"Ωm: {Om_50:.3f} +{Om_84-Om_50:.3f} -{Om_50-Om_16:.3f}")
+    print(f"w0: {w0_50:.3f} +{(w0_84 - w0_50):.3f} -{(w0_50 - w0_16):.3f}")
+    print(f"rd: {rd_50:.2f} +{(rd_84 - rd_50):.2f} -{(rd_50 - rd_16):.2f}")
+    print(f"Chi squared: {chi_squared(best_fit):.2f}")
+    print(f"Degs of freedom: {1 + data['value'].size  - len(best_fit)}")
+    print(f"R^2: {r2:.4f}")
+    print(f"RMSD: {np.sqrt(np.mean(residuals**2)):.3f}")
+
+    plot_bao_predictions(
+        theory_predictions=lambda z, qty: bao_theory(z, qty, best_fit),
+        data=data,
+        errors=np.sqrt(np.diag(cov_matrix)),
+        title=f"{legend}: $H_0$={100 * h_50:.1f} km/s/Mpc, $Ω_m$={Om_50:.3f}",
+    )
+    plot_bao_residuals(data, residuals, np.sqrt(np.diag(cov_matrix)))
+
+    labels = ["$h$", "$Ω_m$", "$w_0$", "$r_d$"]
+    corner.corner(
+        samples,
+        labels=labels,
+        quantiles=[0.159, 0.5, 0.841],
+        show_titles=True,
+        title_fmt=".3f",
+        bins=100,
+        fill_contours=False,
+        plot_datapoints=False,
+        smooth=2.0,
+        smooth1d=2.0,
+        levels=(0.393, 0.864),  # 1 and 2 sigmas in 2D
+    )
+    plt.show()
+
+    plt.figure(figsize=(16, 1.5 * n_dim))
+    for n in range(n_dim):
+        plt.subplot2grid((n_dim, 1), (n, 0))
+        plt.plot(sampler.get_chain(discard=burn_in)[:, :, n], alpha=0.3)
+        plt.ylabel(labels[n])
+        plt.xlim(0, None)
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+*******************************
+Dataset: DESI DR2 2024 + Ωm * h^2 Planck 2018
+*******************************
+
+Flat ΛCDM:
+h: 0.693 +0.012 -0.012
+Ωm: 0.298 +0.009 -0.008
+w0: -1
+rd: 146.41 +1.81 -1.76
+Chi squared: 10.27
+Degs of freedom: 11
+R^2: 0.9987
+RMSD: 0.305
+
+===============================
+
+Flat wCDM:
+h: 0.694 +0.012 -0.012
+Ωm: 0.297 +0.009 -0.009
+w0: -0.915 +0.076 -0.079
+rd: 143.92 +2.97 -3.13
+Chi squared: 9.08
+Degs of freedom: 10
+R^2: 0.9989
+RMSD: 0.281
+
+===============================
+
+Flat alternative: w(z) = -1 + 2 * (1 + w0) / (1 + (1 + z)**3)
+h: 0.682 +0.014 -0.014
+Ωm: 0.308 +0.012 -0.012
+w0: -0.832 +0.120 -0.128
+rd: 144.61 +2.29 -2.24
+Chi squared: 8.45
+Degs of freedom: 10
+R^2: 0.9990
+RMSD: 0.266
+"""
