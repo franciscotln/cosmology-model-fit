e1a55c860af7e2404b41f9f50ddb89de6673396c
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-10-08 15:05:58 +0200

    fsigma8 data

---
+++ y2018fs8/data.py
@@ -0,0 +1,11 @@
+# covariances from arXiv:1806.10822v2
+import numpy as np
+
+pathname = "y2018fs8/raw/"
+data = np.genfromtxt(pathname + "fs8.csv", delimiter=",", names=True, dtype=np.float64)
+cov_mat = np.genfromtxt(pathname + "fs8_cov.dat", dtype=np.float64)
+
+z_values = data["z"]
+fs8_values = data["fs8"]
+fs8_err = data["fs8_err"]
+om_fid = data["omega_fid"]
