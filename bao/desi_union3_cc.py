df96b89a18fe59db0994d51eeacc57c2f495730f
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-10-09 16:05:44 +0200

    cc_union3_desi

--- bao/desi_union3_cc.py
+++ bao/desi_union3_cc.py
@@ -1,15 +1,12 @@
 from numba import njit
 import numpy as np
-import emcee
-import corner
 from scipy.integrate import cumulative_trapezoid
 from scipy.linalg import cho_factor, cho_solve
-import matplotlib.pyplot as plt
-from multiprocessing import Pool
 from y2023union3.data import get_data
 from y2005cc.data import get_data as get_cc_data
 from y2025BAO.data import get_data as get_bao_data
 from sn.plotting import plot_predictions as plot_sn_predictions
+from cosmic_chronometers.plot_predictions import plot_cc_predictions
 from .plot_predictions import plot_bao_predictions
 
 cc_legend, z_cc_vals, H_cc_vals, cov_matrix_cc = get_cc_data()
@@ -29,10 +26,11 @@ one_plus_z_sn = 1 + z_sn_vals
 
 @njit
 def Ez(z, params):
-    O_m, w0 = params[4], params[5]
+    O_m, w0, wa = params[4], params[5], params[6]
     one_plus_z = 1 + z
     cubed = one_plus_z**3
-    rho_de = (2 * cubed / (1 + cubed)) ** (2 * (1 + w0))
+    # rho_de = (2 * cubed / (1 + cubed)) ** (2 * (1 + w0))
+    rho_de = one_plus_z ** (3 * (1 + w0 + wa)) * np.exp(-3 * wa * z / one_plus_z)
     return np.sqrt(O_m * cubed + (1 - O_m) * rho_de)
 
 
@@ -95,12 +93,13 @@ def bao_theory(z, qty, params):
 
 bounds = np.array(
     [
-        (0.4, 2.5),  # f_cc
+        (0.1, 1.5),  # f_cc
         (-0.7, 0.7),  # ΔM
         (55, 80),  # H0
         (125, 170),  # r_d
         (0.2, 0.7),  # Ωm
-        (-2.0, 0.0),  # w0
+        (-2.0, 1.0),  # w0
+        (-3.0, 2.0),
     ],
     dtype=np.float64,
 )
@@ -115,12 +114,16 @@ def chi_squared(params):
     chi_bao = np.dot(delta_bao, cho_solve(cho_bao, delta_bao, check_finite=False))
 
     delta_cc = H_cc_vals - H_z(z_cc_vals, params)
-    chi_cc = f_cc**2 * np.dot(delta_cc, cho_solve(cho_cc, delta_cc, check_finite=False))
+    chi_cc = f_cc**-2 * np.dot(
+        delta_cc, cho_solve(cho_cc, delta_cc, check_finite=False)
+    )
     return chi_sn + chi_bao + chi_cc
 
 
 @njit
 def log_prior(params):
+    if params[5] + params[6] > 0:
+        return -np.inf
     if np.all((bounds[:, 0] < params) & (params < bounds[:, 1])):
         return 0.0
     return -np.inf
@@ -128,7 +131,7 @@ def log_prior(params):
 
 def log_likelihood(params):
     f_cc = params[0]
-    normalization_cc = N_cc * np.log(2 * np.pi) + logdet_cc - 2 * N_cc * np.log(f_cc)
+    normalization_cc = N_cc * np.log(2 * np.pi) + logdet_cc + 2 * N_cc * np.log(f_cc)
     return -0.5 * chi_squared(params) - 0.5 * normalization_cc
 
 
@@ -140,6 +143,10 @@ def log_probability(params):
 
 
 def main():
+    import emcee, corner
+    import matplotlib.pyplot as plt
+    from multiprocessing import Pool
+
     ndim = len(bounds)
     nwalkers = 150
     burn_in = 200
@@ -179,9 +186,10 @@ def main():
         [rd_16, rd_50, rd_84],
         [Om_16, Om_50, Om_84],
         [w0_16, w0_50, w0_84],
+        [wa_16, wa_50, wa_84],
     ] = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
 
-    best_fit = np.array([f_cc_50, dM_50, h0_50, rd_50, Om_50, w0_50], dtype=np.float64)
+    best_fit = np.array([f_cc_50, dM_50, h0_50, rd_50, Om_50, w0_50, wa_50], dtype=np.float64)
 
     Omh2_samples = samples[:, 4] * samples[:, 2] ** 2 / 100**2
     Omh2_16, Omh2_50, Omh2_84 = np.percentile(Omh2_samples, [15.9, 50, 84.1])
@@ -195,10 +203,9 @@ def main():
     print(f"H0: {h0_50:.1f} +{(h0_84 - h0_50):.1f} -{(h0_50 - h0_16):.1f}")
     print(f"r_d: {rd_50:.1f} +{(rd_84 - rd_50):.1f} -{(rd_50 - rd_16):.1f}")
     print(f"Ωm: {Om_50:.3f} +{(Om_84 - Om_50):.3f} -{(Om_50 - Om_16):.3f}")
-    print(
-        f"Ωm h^2: {Omh2_50:.4f} +{(Omh2_84 - Omh2_50):.4f} -{(Omh2_50 - Omh2_16):.4f}"
-    )
+    print(f"ωm: {Omh2_50:.4f} +{(Omh2_84 - Omh2_50):.4f} -{(Omh2_50 - Omh2_16):.4f}")
     print(f"w0: {w0_50:.3f} +{(w0_84 - w0_50):.3f} -{(w0_50 - w0_16):.3f}")
+    print(f"wa: {wa_50:.3f} +{(wa_84 - wa_50):.3f} -{(wa_50 - wa_16):.3f}")
     print(f"Chi squared: {chi_squared(best_fit):.2f}")
     print(f"Degrees of freedom: {deg_of_freedom}")
 
@@ -217,8 +224,15 @@ def main():
         label=f"Best fit: $H_0$={h0_50:.2f}, $\Omega_m$={Om_50:.3f}",
         x_scale="log",
     )
+    plot_cc_predictions(
+        H_z=lambda z: H_z(z, best_fit),
+        z=z_cc_vals,
+        H=H_cc_vals,
+        H_err=np.sqrt(np.diag(cov_matrix_cc)) * f_cc_50,
+        label=f"{cc_legend} $H_0$: {h0_50:.1f} km/s/Mpc",
+    )
 
-    labels = ["$f_{CCH}$", "ΔM", "$H_0$", "$r_d$", "Ωm", "$w_0$"]
+    labels = ["$f_{CCH}$", "ΔM", "$H_0$", "$r_d$", "Ωm", "$w_0$", "$w_a$"]
     corner.corner(
         samples,
         labels=labels,
@@ -251,60 +265,61 @@ if __name__ == "__main__":
 
 """
 Flat ΛCDM: w(z) = -1
-f_cc: 1.48 +0.18 -0.18
-ΔM: -0.120 +0.114 -0.115
-H0: 68.7 +2.3 -2.3
-r_d: 147.1 +5.0 -4.6
-Ωm: 0.305 +0.008 -0.008
-Ωm h^2: 0.1437 +0.0095 -0.0094
+f_cc: 0.70 +0.10 -0.08
+ΔM: -0.118 +0.114 -0.117 mag
+H0: 68.7 +2.3 -2.4 km/s/Mpc
+r_d: 147.0 +5.1 -4.7 Mpc
+Ωm: 0.304 +0.008 -0.008
+ωm: 0.1436 +0.0097 -0.0096
 w0: -1
-Chi squared: 71.19
+Chi squared: 69.1
 Degrees of freedom: 63
 
 ==============================
 
 Flat wCDM: w(z) = w0
-f_cc: 1.46 +0.18 -0.17
-ΔM: -0.159 +0.115 -0.116 mag
-H0: 67.1 +2.3 -2.3 km/s/Mpc
-r_d: 147.3 +5.0 -4.6 Mpc
-Ωm: 0.299 +0.009 -0.009
-Ωm h^2: 0.1342 +0.0100 -0.0096
-w0: -0.870 +0.050 -0.052
-Chi squared: 64.41
+f_cc: 0.70 +0.10 -0.08
+ΔM: -0.158 +0.115 -0.118 mag
+H0: 67.1 +2.4 -2.4 km/s/Mpc
+r_d: 147.3 +5.1 -4.9 Mpc
+Ωm: 0.298 +0.009 -0.009
+ωm: 0.1342 +0.0103 -0.0097
+w0: -0.870 +0.051 -0.051
+Chi squared: 62.6 (Δ chi2 = 6.5 compared to ΛCDM)
 Degrees of freedom: 62
 
 ==============================
 
 Flat alternative: w(z) = -1 + 2 * (1 + w0) / (1 + (1 + z)**3)
-f_cc: 1.46 +0.18 -0.18
-ΔM: -0.165 +0.117 -0.116 mag
-H0: 66.7 +2.4 -2.3 km/s/Mpc
-r_d: 147.2 +4.9 -4.7 Mpc
+f_cc: 0.71 +0.10 -0.08
+ΔM: -0.165 +0.117 -0.119 mag
+H0: 66.7 +2.4 -2.4 km/s/Mpc
+r_d: 147.2 +5.2 -4.8 Mpc
 Ωm: 0.310 +0.009 -0.008
-Ωm h^2: 0.1379 +0.0096 -0.0092
-w0: -0.812 +0.065 -0.067
-Chi squared: 62.73 (Δ chi2 8.46 from ΛCDM)
+ωm: 0.1378 +0.0099 -0.0095
+w0: -0.811 +0.065 -0.066
+Chi squared: 60.7 (Δ chi2 = 8.4 compared to ΛCDM)
 Degrees of freedom: 62
 
 ===============================
 
 Flat w0waCDM w(z) = w0 + wa * z / (1 + z)
-f_cc: 1.45 +0.18 -0.18
-ΔM: -0.167 +0.116 -0.118 mag
+f_cc: 0.71 +0.10 -0.08
+ΔM: -0.167 +0.116 -0.117 mag
 H0: 66.3 +2.4 -2.4 km/s/Mpc
-r_d: 147.1 +5.1 -4.7 Mpc
-Ωm: 0.329 +0.016 -0.019
-w0: -0.725 +0.113 -0.107
-wa: -0.887 +0.564 -0.555
-Chi squared: 61.22
+r_d: 147.1 +5.2 -4.8 Mpc
+Ωm: 0.329 +0.016 -0.018
+ωm: 0.1441 +0.0115 -0.0113
+w0: -0.722 +0.113 -0.106
+wa: -0.901 +0.554 -0.561
+Chi squared: 59.1 (Δ chi2 = 10.0 compared to ΛCDM)
 Degrees of freedom: 61
 Correlation matrix:
-[[ 1.       0.00752  0.01645 -0.01413 -0.03293 -0.05057  0.04854]
- [ 0.00752  1.       0.64539 -0.6261  -0.09129 -0.10317  0.04608]
- [ 0.01645  0.64539  1.      -0.92733 -0.2588  -0.28832  0.1947 ]
- [-0.01413 -0.6261  -0.92733  1.       0.00794 -0.01275  0.01455]
- [-0.03293 -0.09129 -0.2588   0.00794  1.       0.75963 -0.87139]
- [-0.05057 -0.10317 -0.28832 -0.01275  0.75963  1.      -0.88604]
- [ 0.04854  0.04608  0.1947   0.01455 -0.87139 -0.88604  1.     ]]
+[[ 1.      -0.01544 -0.02956  0.03362  0.01976  0.04016 -0.03784]
+ [-0.01544  1.       0.65363 -0.63626 -0.08695 -0.09796  0.03614]
+ [-0.02956  0.65363  1.      -0.93058 -0.25339 -0.28492  0.18545]
+ [ 0.03362 -0.63626 -0.93058  1.       0.00176 -0.01002  0.02232]
+ [ 0.01976 -0.08695 -0.25339  0.00176  1.       0.77201 -0.8719 ]
+ [ 0.04016 -0.09796 -0.28492 -0.01002  0.77201  1.      -0.89085]
+ [-0.03784  0.03614  0.18545  0.02232 -0.8719  -0.89085  1.     ]]
 """
