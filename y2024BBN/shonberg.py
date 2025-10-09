1476d780424f64a5e89be4532076769a148a4410
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-10-09 10:21:06 +0200

    bbn

--- y2024BBN/shonberg.py
+++ y2024BBN/shonberg.py
@@ -2,14 +2,10 @@ import numpy as np
 
 # arXiv:2401.15054v2
 # https://github.com/cosmodesi/desilike/blob/main/desilike/likelihoods/bbn/bbn.yaml
+# https://github.com/schoeneberg/2024_bbn_results/blob/5d5079bed04ce747a83fcf9f8bc04904a6f490e7/covmats.txt#L14
 
-data = np.array(
-    [
-        0.02196,  # Ωb * h²
-        2.944,  # N_eff
-    ],
-    dtype=np.float64,
-)
+# Ωb * h², N_eff with ∆Neff: -0.14
+data = np.array([0.02196, 3.044 - 0.14], dtype=np.float64)
 
 cov_matrix = np.array(
     [
