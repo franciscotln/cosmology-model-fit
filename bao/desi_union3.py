6f7aab5798395904a1a51ebe2f0e48379030cb82
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-10-12 00:46:51 +0200

    log evidence

--- bao/desi_union3.py
+++ bao/desi_union3.py
@@ -1,15 +1,9 @@
 from numba import njit
 import numpy as np
-import emcee
-import corner
 from scipy.integrate import cumulative_trapezoid
 from scipy.linalg import cho_factor, cho_solve
-import matplotlib.pyplot as plt
-from multiprocessing import Pool
 from y2023union3.data import get_data
 from y2025BAO.data import get_data as get_bao_data
-from sn.plotting import plot_predictions as plot_sn_predictions
-from .plot_predictions import plot_bao_predictions
 
 sn_legend, z_sn_vals, mu_vals, cov_matrix_sn = get_data()
 bao_legend, bao_data, bao_cov_matrix = get_bao_data()
@@ -114,9 +108,9 @@ bounds = np.array(
 
 @njit
 def log_prior(params):
-    if np.all((bounds[:, 0] < params) & (params < bounds[:, 1])):
-        return 0.0
-    return -np.inf
+    if not np.all((bounds[:, 0] < params) & (params < bounds[:, 1])):
+        return -np.inf
+    return 0.0
 
 
 def log_likelihood(params):
@@ -131,14 +125,20 @@ def log_probability(params):
 
 
 def main():
+    import emcee, corner
+    import matplotlib.pyplot as plt
+    from multiprocessing import Pool
+    from sn.plotting import plot_predictions as plot_sn_predictions
+    from .plot_predictions import plot_bao_predictions
+    from gelman_rubin import gelman_rubin
+    from log_evidence import log_evidence
+
+    np.random.seed(42)
     ndim = len(bounds)
     nwalkers = 150
     burn_in = 200
     nsteps = 2000 + burn_in
-    initial_pos = np.zeros((nwalkers, ndim))
-
-    for dim, (lower, upper) in enumerate(bounds):
-        initial_pos[:, dim] = np.random.uniform(lower, upper, nwalkers)
+    initial_pos = np.random.uniform(bounds[:, 0], bounds[:, 1], size=(nwalkers, ndim))
 
     with Pool(5) as pool:
         sampler = emcee.EnsembleSampler(
@@ -164,7 +164,9 @@ def main():
 
     chains_samples = sampler.get_chain(discard=burn_in, flat=False)
     samples = sampler.get_chain(discard=burn_in, flat=True)
-    print(np.array2string(np.corrcoef(samples, rowvar=False), precision=5))
+
+    print("Gelman-Rubin:", gelman_rubin(chains_samples))
+    print(f"Log Evidence: {log_evidence(samples, log_probability):.2f}")
 
     [
         [dM_16, dM_50, dM_84],
@@ -179,7 +181,7 @@ def main():
     print(f"H0: {H0_50:.2f} +{(H0_84 - H0_50):.2f} -{(H0_50 - H0_16):.2f} km/s/Mpc")
     print(f"Ωm: {Om_50:.3f} +{(Om_84 - Om_50):.3f} -{(Om_50 - Om_16):.3f}")
     print(f"w0: {w0_50:.3f} +{(w0_84 - w0_50):.3f} -{(w0_50 - w0_16):.3f}")
-    print(f"Chi squared: {chi_squared(best_fit):.4f}")
+    print(f"Chi squared: {chi_squared(best_fit):.2f}")
     print(f"Degs of freedom: {bao_data['value'].size + z_sn_vals.size - len(best_fit)}")
 
     plot_bao_predictions(
@@ -234,131 +236,47 @@ DESI BAO DR2 2025
 
 Flat ΛCDM
 r_d: 147.09 Mpc (fixed)
-ΔM: -0.119 +0.089 -0.089 mag
-H0: 68.69 +0.47 -0.48 km/s/Mpc
+ΔM: -0.119 +0.089 -0.088 mag
+H0: 68.69 +0.47 -0.47 km/s/Mpc
 Ωm: 0.304 +0.008 -0.008
-w0: -1
 Chi squared: 38.82
+Log Evidence: -25.57
 Degs of freedom: 32
-Correlation matrix:
-[[ 1.       0.15248 -0.13541]
- [ 0.15248  1.      -0.91781]
- [-0.13541 -0.91781  1.     ]]
 
 ===============================
 
 Flat wCDM
 r_d: 147.09 Mpc (fixed)
-ΔM: -0.156 +0.089 -0.088 mag
-H0: 67.12 +0.74 -0.73 km/s/Mpc
+ΔM: -0.156 +0.089 -0.089 mag
+H0: 67.12 +0.75 -0.73 km/s/Mpc
 Ωm: 0.298 +0.009 -0.009
-w0: -0.866 +0.051 -0.050
-Chi squared: 32.17 (Δ chi2 6.65)
+w0: -0.866 +0.050 -0.051
+Chi squared: 32.16
+Log Evidence: -24.28 (Bayes factor 1.29 over ΛCDM)
 Degs of freedom: 31
-Correlation matrix:
-[[ 1.       0.2271  -0.06492 -0.17294]
- [ 0.2271   1.      -0.19514 -0.81673]
- [-0.06492 -0.19514  1.      -0.35745]
- [-0.17294 -0.81673 -0.35745  1.     ]]
 
 ===============================
 
 Flat -1 + 2 * (1 + w0) / (1 + (1 + z)**3)
 r_d: 147.09 Mpc (fixed)
-ΔM: -0.163 +0.089 -0.090 mag
-H0: 66.66 +0.82 -0.80 km/s/Mpc
+ΔM: -0.164 +0.088 -0.090 mag
+H0: 66.66 +0.81 -0.81 km/s/Mpc
 Ωm: 0.310 +0.009 -0.008
-w0: -0.803 +0.065 -0.066
-Chi squared: 30.37 (Δ chi2 8.45)
+w0: -0.803 +0.066 -0.065
+Chi squared: 30.37
+Log Evidence: -23.17 (Bayes factor 2.40 over ΛCDM)
 Degs of freedom: 31
-Correlation matrix:
-[[ 1.       0.22998 -0.17029 -0.18202]
- [ 0.22998  1.      -0.67101 -0.8569 ]
- [-0.17029 -0.67101  1.       0.26096]
- [-0.18202 -0.8569   0.26096  1.     ]]
 
 ===============================
 
 Flat w0waCDM
 r_d: 147.09 Mpc (fixed)
-ΔM: -0.166 +0.091 -0.091 mag
-H0: 66.23 +0.92 -0.91 km/s/Mpc
-Ωm: 0.330 +0.016 -0.018
-w0: -0.700 +0.117 -0.111
-wa: -0.996 +0.569 -0.567
-Chi squared: 28.79 (Δ chi2 10.03)
+ΔM: -0.168 +0.089 -0.090 mag
+H0: 66.21 +0.89 -0.88 km/s/Mpc
+Ωm: 0.331 +0.015 -0.017
+w0: -0.698 +0.113 -0.107
+wa: -1.006 +0.548 -0.554
+Chi squared: 28.80
+Log Evidence: -22.19 (Bayes factor 3.38 over ΛCDM)
 Degs of freedom: 30
-Correlation matrix:
-[[ 1.       0.23428 -0.14014 -0.15026  0.08527]
- [ 0.23428  1.      -0.68941 -0.81009  0.57255]
- [-0.14014 -0.68941  1.       0.7572  -0.85998]
- [-0.15026 -0.81009  0.7572   1.      -0.89353]
- [ 0.08527  0.57255 -0.85998 -0.89353  1.     ]]
-
-*******************************
-SDSS BAO DR16 compilation 2020
-*******************************
-
-Flat ΛCDM
-r_d: 147.09 Mpc (fixed)
-ΔM: -0.137 +0.091 -0.090 mag
-H0: 68.09 +0.65 -0.65 km/s/Mpc
-Ωm: 0.313 +0.015 -0.014
-w0: -1
-Chi squared: 39.873
-Degs of freedom: 36
-Correlation matrix:
-[[ 1.       0.21127 -0.17231]
- [ 0.21127  1.      -0.85116]
- [-0.17231 -0.85116  1.     ]]
-
-===============================
-
-Flat wCDM
-r_d: 147.09 Mpc (fixed)
-ΔM: -0.171 +0.091 -0.091 mag
-H0: 66.52 +0.84 -0.83 km/s/Mpc
-Ωm: 0.285 +0.018 -0.019
-w0: -0.807 +0.067 -0.068
-Chi squared: 31.999 (Δ chi2 7.874)
-Degs of freedom: 35
-Correlation matrix:
-[[ 1.       0.24296 -0.01448 -0.15654]
- [ 0.24296  1.       0.01761 -0.71864]
- [-0.01448  0.01761  1.      -0.63004]
- [-0.15654 -0.71864 -0.63004  1.     ]]
-
-===============================
-
-Flat -1 + 2 * (1 + w0) / (1 + (1 + z)**3)
-r_d: 147.09 Mpc (fixed)
-ΔM: -0.171 +0.091 -0.092 mag
-H0: 66.35 +0.88 -0.86 km/s/Mpc
-Ωm: 0.303 +0.015 -0.014
-w0: -0.771 +0.077 -0.079
-Chi squared: 31.834 (Δ chi2 8.039)
-Degs of freedom: 35
-Correlation matrix:
-[[ 1.       0.24304 -0.12174 -0.15046]
- [ 0.24304  1.      -0.38784 -0.75141]
- [-0.12174 -0.38784  1.      -0.18599]
- [-0.15046 -0.75141 -0.18599  1.     ]]
-
-===============================
-
-Flat w0waCDM
-r_d: 147.09 Mpc (fixed)
-ΔM: -0.171 +0.091 -0.090 mag
-H0: 66.35 +0.93 -0.92 km/s/Mpc
-Ωm: 0.300 +0.032 -0.067
-w0: -0.753 +0.109 -0.093
-wa: -0.303 +0.790 -0.925
-Chi squared: 32.6992
-Degs of freedom: 34
-Correlation matrix:
-[[ 1.       0.23258 -0.04115 -0.11096  0.02962]
- [ 0.23258  1.      -0.37163 -0.70946  0.43873]
- [-0.04115 -0.37163  1.       0.30448 -0.80707]
- [-0.11096 -0.70946  0.30448  1.      -0.7151 ]
- [ 0.02962  0.43873 -0.80707 -0.7151   1.     ]]
 """
