27149eb3b2bb7b0f75991779a10eae408afbaaf7
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-10-16 09:51:16 +0200

    desi cmb pantheon

--- bao/desi_cmb_pantheon.py
+++ bao/desi_cmb_pantheon.py
@@ -1,33 +1,32 @@
 from numba import njit
 import numpy as np
-import emcee
-import corner
-from scipy.integrate import cumulative_trapezoid
-from scipy.linalg import cho_factor, cho_solve
-import matplotlib.pyplot as plt
-from multiprocessing import Pool
-import cmb.data_chen_compression as cmb
+from scipy.linalg import cho_factor, solve_triangular
+import cmb.data_desi_compression as cmb
 from y2022pantheonSHOES.data import get_data
 from y2025BAO.data import get_data as get_bao_data
-from sn.plotting import plot_predictions as plot_sn_predictions
-from .plot_predictions import plot_bao_predictions
-
 
 c = cmb.c  # km/s
 Or_h2 = cmb.Omega_r_h2()
 
 sn_legend, z_cmb, z_hel, mb_values, cov_matrix_sn = get_data()
 bao_legend, bao_data, bao_cov_matrix = get_bao_data()
-cho_sn = cho_factor(cov_matrix_sn)
-cho_bao = cho_factor(bao_cov_matrix)
+
+cho_sn = cho_factor(cov_matrix_sn, lower=True)[0]
+cho_bao = cho_factor(bao_cov_matrix, lower=True)[0]
+cho_sn_T = cho_sn.T
+cho_bao_T = cho_bao.T
 
 sn_grid = np.linspace(0, np.max(z_cmb), num=1000)
+dx_sn = np.diff(sn_grid)
 one_plus_z_hel = 1 + z_hel
+
+bao_grid = np.linspace(0, np.max(bao_data["z"]), num=1000)
+dx_bao = np.diff(bao_grid)
 
 
 @njit
 def Ez(z, params):
-    H0, Om, w0 = params[1], params[2], params[4]
+    H0, Om, w0 = params[0], params[1], params[4]
     h = H0 / 100
     Or = Or_h2 / h**2
     Ode = 1 - Om - Or
@@ -37,20 +36,25 @@ def Ez(z, params):
     return np.sqrt(Or * one_plus_z**4 + Om * one_plus_z**3 + Ode * rho_de)
 
 
-def integral_Ez(params):
-    integral_values = cumulative_trapezoid(1 / Ez(sn_grid, params), sn_grid, initial=0)
-    return np.interp(z_cmb, sn_grid, integral_values)
+@njit
+def DM(grid, zs, dx, params):
+    dh_grid = DH_z(grid, params)
+    dy = (dh_grid[:-1] + dh_grid[1:]) / 2
+    cum_dm = np.zeros(grid.size)
+    cum_dm[1:] = np.cumsum(dx * dy)
+    dms = np.interp(zs, grid, cum_dm)
+    return dms
 
 
+@njit
 def apparent_mag(params):
-    H0, M = params[1], params[-1]
-    dL = one_plus_z_hel * (c / H0) * integral_Ez(params)
-    return M + 25 + 5 * np.log10(dL)
+    dL = one_plus_z_hel * DM(sn_grid, z_cmb, dx_sn, params)
+    return params[3] + 25 + 5 * np.log10(dL)
 
 
 @njit
 def H_z(z, params):
-    return params[1] * Ez(z, params)
+    return params[0] * Ez(z, params)
 
 
 @njit
@@ -60,13 +64,7 @@ def DH_z(z, params):
 
 @njit
 def DM_z(z, params):
-    result = np.empty(z.size, dtype=np.float64)
-    for i in range(z.size):
-        zp = z[i]
-        x = np.linspace(0, zp, num=max(250, int(250 * zp)))
-        y = DH_z(x, params)
-        result[i] = np.trapz(y=y, x=x)
-    return result
+    return DM(bao_grid, z, dx_bao, params)
 
 
 @njit
@@ -81,11 +79,16 @@ qty_map = {
     "DM_over_rs": 1,
     "DH_over_rs": 2,
 }
+
 quantities = np.array([qty_map[q] for q in bao_data["quantity"]], dtype=np.int64)
 
 
-@njit
 def bao_theory(z, qty, params):
+    H0, Om, Obh2 = params[0], params[1], params[2]
+    h = H0 / 100
+    z_drag = cmb.z_drag(wb=Obh2, wm=Om * h**2)
+    rd = cmb.rs_z(Ez, z_drag, params, H0, Obh2)
+
     results = np.empty(z.size, dtype=np.float64)
     DV_mask = qty == 0
     DM_mask = qty == 1
@@ -93,41 +96,48 @@ def bao_theory(z, qty, params):
     results[DH_mask] = DH_z(z[DH_mask], params)
     results[DM_mask] = DM_z(z[DM_mask], params)
     results[DV_mask] = DV_z(z[DV_mask], params)
-    return results / params[0]
+    return results / rd
+
+
+def solve_triang(L, L_T, delta):
+    y = solve_triangular(L, delta, lower=True, check_finite=False)
+    z = solve_triangular(L_T, y, lower=False, check_finite=False)
+    return delta @ z
 
 
 def chi_squared(params):
-    H0, Om, Obh2 = params[1], params[2], params[3]
+    H0, Om, Obh2 = params[0], params[1], params[2]
 
     delta = cmb.DISTANCE_PRIORS - cmb.cmb_distances(Ez, params, H0, Om, Obh2)
     chi2_cmb = np.dot(delta, np.dot(cmb.inv_cov_mat, delta))
 
     delta_bao = bao_data["value"] - bao_theory(bao_data["z"], quantities, params)
-    chi_bao = np.dot(delta_bao, cho_solve(cho_bao, delta_bao, check_finite=False))
+    chi_bao = solve_triang(cho_bao, cho_bao_T, delta_bao)
 
     delta_sn = mb_values - apparent_mag(params)
-    chi_sn = np.dot(delta_sn, cho_solve(cho_sn, delta_sn, check_finite=False))
+    chi_sn = solve_triang(cho_sn, cho_sn_T, delta_sn)
 
     return chi2_cmb + chi_bao + chi_sn
 
 
 bounds = np.array(
     [
-        (120, 160),  # rd
-        (60, 75),  # H0
+        (60.0, 75.0),  # H0
         (0.1, 0.6),  # Ωm
-        (0.019, 0.025),  # Ωb * h^2
-        (-2, 0),  # w0
-        (-20, -19),  # M
+        (0.019, 0.025),  # ωb = Ωb * h^2
+        (-20.0, -19.0),  # M
+        (-1.5, 0.0),  # w0
     ],
     dtype=np.float64,
 )
+
+normalization = -np.sum(np.log(bounds[:, 1] - bounds[:, 0]))
 
 
 @njit
 def log_prior(params):
     if np.all((bounds[:, 0] < params) & (params < bounds[:, 1])):
-        return 0
+        return normalization
     return -np.inf
 
 
@@ -143,26 +153,28 @@ def log_probability(params):
 
 
 def main():
+    import emcee, corner
+    import matplotlib.pyplot as plt
+    from multiprocessing import Pool
+    from log_evidence import log_evidence
+    from sn.plotting import plot_predictions as plot_sn_predictions
+    from .plot_predictions import plot_bao_predictions
+
     ndim = len(bounds)
     nwalkers = 150
     burn_in = 200
     nsteps = 2000 + burn_in
-    initial_pos = np.zeros((nwalkers, ndim))
-
-    for dim, (lower, upper) in enumerate(bounds):
-        initial_pos[:, dim] = np.random.uniform(lower, upper, nwalkers)
+    np.random.seed(42)
+    initial_pos = np.random.uniform(bounds[:, 0], bounds[:, 1], (nwalkers, ndim))
+    moves = [
+        (emcee.moves.KDEMove(), 0.30),
+        (emcee.moves.DEMove(), 0.56),
+        (emcee.moves.DESnookerMove(), 0.14),
+    ]
 
     with Pool(6) as pool:
         sampler = emcee.EnsembleSampler(
-            nwalkers,
-            ndim,
-            log_probability,
-            pool=pool,
-            moves=[
-                (emcee.moves.KDEMove(), 0.30),
-                (emcee.moves.DEMove(), 0.56),
-                (emcee.moves.DESnookerMove(), 0.14),
-            ],
+            nwalkers, ndim, log_probability, pool=pool, moves=moves
         )
         sampler.run_mcmc(initial_pos, nsteps, progress=True)
 
@@ -175,31 +187,33 @@ def main():
         print("Autocorrelation time could not be computed", e)
 
     samples = sampler.get_chain(discard=burn_in, flat=True)
+    chains_samples = sampler.get_chain(discard=burn_in, flat=False)
+    log_probs = sampler.get_log_prob(discard=burn_in, flat=True)
 
     pct = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
-    rd_16, rd_50, rd_84 = pct[0]
-    H0_16, H0_50, H0_84 = pct[1]
-    Om_16, Om_50, Om_84 = pct[2]
-    Obh2_16, Obh2_50, Obh2_84 = pct[3]
+    H0_16, H0_50, H0_84 = pct[0]
+    Om_16, Om_50, Om_84 = pct[1]
+    Obh2_16, Obh2_50, Obh2_84 = pct[2]
+    M_16, M_50, M_84 = pct[3]
     w0_16, w0_50, w0_84 = pct[4]
-    M_16, M_50, M_84 = pct[5]
 
-    best_fit = np.array([rd_50, H0_50, Om_50, Obh2_50, w0_50, M_50], dtype=np.float64)
+    best_fit = np.percentile(samples, 50, axis=0)
 
     Omh2_50 = Om_50 * (H0_50 / 100) ** 2
     z_st = cmb.z_star(Obh2_50, Omh2_50)
+    z_d = cmb.z_drag(Obh2_50, Omh2_50)
 
-    print(f"rd: {rd_50:.2f} +{(rd_84 - rd_50):.2f} -{(rd_50 - rd_16):.2f} Mpc")
     print(f"H0: {H0_50:.2f} +{(H0_84 - H0_50):.2f} -{(H0_50 - H0_16):.2f} km/s/Mpc")
     print(f"Ωm: {Om_50:.3f} +{(Om_84 - Om_50):.3f} -{(Om_50 - Om_16):.3f}")
-    print(
-        f"Ωb h^2: {Obh2_50:.5f} +{(Obh2_84 - Obh2_50):.5f} -{(Obh2_50 - Obh2_16):.5f}"
-    )
+    print(f"ωb: {Obh2_50:.5f} +{(Obh2_84 - Obh2_50):.5f} -{(Obh2_50 - Obh2_16):.5f}")
     print(f"w0: {w0_50:.3f} +{(w0_84 - w0_50):.3f} -{(w0_50 - w0_16):.3f}")
     print(f"M: {M_50:.3f} +{(M_84 - M_50):.3f} -{(M_50 - M_16):.3f}")
     print(f"z*: {z_st:.2f}")
     print(f"r*: {cmb.rs_z(Ez, z_st, best_fit, H0_50, Obh2_50):.2f} Mpc")
+    print(f"z_d: {z_d:.2f}")
+    print(f"rd: {cmb.rs_z(Ez, z_d, best_fit, H0_50, Obh2_50):.2f} Mpc")
     print(f"Chi squared: {chi_squared(best_fit):.2f}")
+    print(f"Log evidence: {log_evidence(samples, log_probs, log_probability):.2f}")
 
     plot_bao_predictions(
         theory_predictions=lambda z, qty: bao_theory(z, qty, best_fit),
@@ -216,13 +230,13 @@ def main():
         label=f"Model: $Ω_m$={Om_50:.3f}",
         x_scale="log",
     )
-    labels = ["$r_d$", "$H_0$", "$Ω_m$", "$Ω_b h^2$", "$w_0$", "M"]
+    labels = ["$H_0$", "$Ω_m$", "$ω_b$", "M", "$w_0$"]
     corner.corner(
         samples,
         labels=labels,
         quantiles=[0.159, 0.5, 0.841],
         show_titles=True,
-        title_fmt=".4f",
+        title_fmt=".3f",
         bins=100,
         fill_contours=False,
         plot_datapoints=False,
@@ -232,7 +246,6 @@ def main():
     )
     plt.show()
 
-    chains_samples = sampler.get_chain(discard=burn_in, flat=False)
     plt.figure(figsize=(16, 1.5 * ndim))
     for n in range(ndim):
         plt.subplot2grid((ndim, 1), (n, 0))
@@ -248,57 +261,64 @@ if __name__ == "__main__":
 
 """
 Flat ΛCDM w(z) = -1
-rd: 148.11 +0.51 -0.50 Mpc
-H0: 67.88 +0.43 -0.41 km/s/Mpc
-Ωm: 0.310 +0.006 -0.006
-Ωb h^2: 0.02244 +0.00013 -0.00013
-w0: -0.998 +0.680 -0.676
-M: -19.425 +0.012 -0.012
-z*: 1088.76
-r*: 144.37 Mpc
-Chi squared: 1417.36
+H0: 68.36 +0.28 -0.29 km/s/Mpc
+Ωm: 0.300 +0.004 -0.004
+ωb: 0.02237 +0.00012 -0.00012
+M: -19.413 +0.009 -0.009
+z*: 1088.67
+r*: 145.09 Mpc
+z_d: 1059.69
+rd: 147.68 Mpc
+Chi squared: 1420.19
+Log evidence: -727.14
 Degrees of freedom: 1602
 
 ===============================
 
 Flat wCDM w(z) = w0
-rd: 148.11 +0.52 -0.51 Mpc
-H0: 67.66 +0.61 -0.60 km/s/Mpc
-Ωm: 0.311 +0.006 -0.006
-Ωb h^2: 0.02246 +0.00014 -0.00014
-w0: -0.987 +0.024 -0.025
-M: -19.430 +0.015 -0.015
-z*: 1088.71
-r*: 144.44 Mpc
-Chi squared: 1417.09
+H0: 67.78 +0.57 -0.56 km/s/Mpc
+Ωm: 0.304 +0.005 -0.005
+ωb: 0.02242 +0.00012 -0.00012
+w0: -0.973 +0.023 -0.023 (prior width 1.5: -1.5 to 0.0)
+M: -19.426 +0.014 -0.014
+z*: 1088.57
+r*: 145.21 Mpc
+z_d: 1059.76
+rd: 147.79 Mpc
+Chi squared: 1418.84
+Log evidence: -729.70
 Degrees of freedom: 1601
 
 ===============================
 
 Flat w(z) = -1 + 2 * (1 + w0) / (1 + (1 + z)**3)
-rd: 148.03 +0.51 -0.51 Mpc
-H0: 67.37 +0.60 -0.60 km/s/Mpc
-Ωm: 0.314 +0.006 -0.006
-Ωb h^2: 0.02248 +0.00013 -0.00013
-w0: -0.954 +0.038 -0.039
-M: -19.434 +0.014 -0.014
-z*: 1088.67
-r*: 144.49 Mpc
-Chi squared: 1416.02
+H0: 67.42 +0.58 -0.58 km/s/Mpc
+Ωm: 0.307 +0.006 -0.005
+ωb: 0.02241 +0.00012 -0.00012
+w0: -0.931 +0.037 -0.037 (prior width 1.5: -1.5 to 0.0)
+M: -19.431 +0.013 -0.013
+z*: 1088.57
+r*: 145.21 Mpc
+z_d: 1059.76
+rd: 147.79 Mpc
+Chi squared: 1416.88
+Log evidence: -728.24
 Degrees of freedom: 1601
 
 ===============================
 
 Flat w(z) = w0 + wa * z / (1 + z)
-rd: 147.63 +0.54 -0.53 Mpc
-H0: 67.51 +0.60 -0.60 km/s/Mpc
-Ωm: 0.315 +0.006 -0.006
-Ωb h^2: 0.02237 +0.00014 -0.00014
-w0: -0.866 +0.057 -0.056
-wa: -0.515 +0.217 -0.232
-M: -19.421 +0.015 -0.015
-z*: 1088.89
-r*: 144.20 Mpc
-Chi squared: 1411.91
+H0: 67.51 +0.58 -0.58 km/s/Mpc
+Ωm: 0.310 +0.006 -0.005
+ωb: 0.02230 +0.00013 -0.00013
+w0: -0.853 +0.054 -0.053 (prior width 1.5: -1.5 to 0.0)
+wa: -0.498 +0.201 -0.213 (prior width 3.0: -2.0 to 1.0)
+M: -19.420 +0.014 -0.014
+z*: 1088.79
+r*: 144.91 Mpc
+z_d: 1059.60
+rd: 147.51 Mpc
+Chi squared: 1413.05
+Log evidence: -728.35
 Degrees of freedom: 1600
 """
