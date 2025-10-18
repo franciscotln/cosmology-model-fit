8b769c0804d85a192aeaf1d48c91344a041c1a0a
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-10-18 19:39:41 +0200

    field

--- field.py
+++ field.py
@@ -4,24 +4,26 @@ from scipy.integrate import cumulative_trapezoid
 from scipy.interpolate import interp1d
 
 
-# values from BAO + CC + DES5Y
+# values from BAO + CCH + DES5Y
 G = 1  # 6.6743e-11 # m^3/kg/s^2 normalised
 c = 1  # 299792458 # m/s normalised
 Rho_de_0 = 1  # normalised
-H0 = 1  # normalised. Fit gives: 67.1 # km/s/Mpc
-O_m = 0.307
-O_r = 4 * 10**-5  # Radiation densit
+H0 = 67.1  # Hubble constant in km/s/Mpc
+Om = 0.308
+Or = 4.1835e-05 / (H0 / 100) ** 2  # Radiation density
 w0 = -0.833  # Equation of state parameter from fit
 
-# Range of scale factors
-a_vals = np.linspace(1e-3, 3, 2000)
+a_min = 1e-8
+a_max = 4
+N_a = 5000
+a_vals = np.linspace(a_min, a_max, N_a)
 
-# Dark energy equation of state
+
 w_de = lambda a: -1 + 2 * (1 + w0) * a**3 / (1 + a**3)
 
 Rho_de = lambda a: Rho_de_0 * (2 / (1 + a**3)) ** (2 * (1 + w0))
 
-H = lambda a: H0 * np.sqrt(O_m * a**-3 + O_r * a**-4 + (1 - O_m) * Rho_de(a) / Rho_de_0)
+H = lambda a: np.sqrt(Om * a**-3 + Or * a**-4 + (1 - Om - Or) * Rho_de(a) / Rho_de_0)
 
 V_phi = lambda a: (1 - w_de(a)) * Rho_de(a) / 2
 
@@ -40,10 +42,10 @@ d_phi_dt_val = d_phi_da(a_vals) * H(a_vals) * a_vals
 # Scalar field
 plt.figure(figsize=(8, 5))
 plt.plot(a_vals, phi_vals, label=r"$\phi(a)$")
-plt.axvline(x=1, color="r", linestyle="--", label="Current time")
+plt.axvline(x=1, color="r", linestyle="--", label="Present time")
 plt.xlabel(r"$a$")
 plt.ylabel(r"$\phi(a)$")
-plt.xlim(0, 2)
+plt.xlim(0, None)
 plt.ylim(0, max(phi_vals))
 plt.title(r"Scalar Field $\phi(a)$")
 plt.legend()
@@ -55,6 +57,7 @@ plt.figure(figsize=(8, 5))
 plt.plot(phi_plot, V_of_phi(phi_plot), label=r"$V(\phi)$")
 plt.xlabel(r"$\phi$")
 plt.ylabel(r"$V(\phi)$")
+plt.xlim(0, None)
 plt.title(r"Scalar Field Potential $V(\phi)$")
 plt.legend()
 plt.grid(True)
@@ -70,23 +73,46 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
+
 dt_da = lambda a: 1 / (a * H(a))
 t_vals = cumulative_trapezoid(dt_da(a_vals), a_vals, initial=0)
-a_of_t = interp1d(t_vals, a_vals, bounds_error=False, fill_value="extrapolate")
-phi_of_t = lambda t: np.interp(t, t_vals, phi_vals)
-t_grid = np.linspace(min(t_vals), max(t_vals), 1000)
 
-# Kinetic term
+Hubble_time_Gyr = 9.77813 / (H0 / 100)  # Hubble time in Gyr
+t_vals_Gyr = t_vals * Hubble_time_Gyr
+a_of_t = interp1d(t_vals_Gyr, a_vals, bounds_error=False, fill_value="extrapolate")
+phi_of_t = lambda t: np.interp(t, t_vals_Gyr, phi_vals)
+t_grid_Gyr = np.linspace(min(t_vals_Gyr), max(t_vals_Gyr), 1000)
+t_today_Gyr = np.interp(1, a_vals, t_vals_Gyr)
+
+# Convert time from 1/H0 units to Gyr
+# H0 in km/s/Mpc, 1/H0 in seconds: 1 Mpc = 3.085677581e19 km
+t_vals_Gyr = t_vals * 9.77813 / (H0 / 100)
+t_grid_Gyr = np.linspace(min(t_vals_Gyr), max(t_vals_Gyr), 1000)
+t_today_Gyr = np.interp(1, a_vals, t_vals_Gyr)
+
+t_at_a1 = np.interp(1, a_vals, t_vals)
+print(f"Hubble time (Gyr): {Hubble_time_Gyr}")
+print(f"t_today (Gyr): {t_today_Gyr}")
+
+
+# Kinetic term (in Gyr)
 plt.figure(figsize=(8, 5))
-plt.plot(t_vals, 0.5 * d_phi_dt_val**2, label=r"Kinetic term")
-plt.xlabel("t")
+plt.plot(t_vals_Gyr, 0.5 * d_phi_dt_val**2, label=r"Kinetic term")
+plt.xlabel("t [Gyr]")
 plt.ylabel(r"0.5 * $\left(\frac{d\phi}{dt}\right)^2$")
 plt.title(r"Scalar Field Kinetic Term $\left(\frac{d\phi}{dt}\right)^2$")
 plt.axvline(
-    x=t_vals[np.argmax(0.5 * d_phi_dt_val**2)],
+    x=t_vals_Gyr[np.argmax(0.5 * d_phi_dt_val**2)],
     color="g",
     linestyle="--",
     label="Max speed",
+    alpha=0.5,
+)
+plt.axvline(
+    x=t_today_Gyr,
+    color="r",
+    linestyle="--",
+    label="Present time",
     alpha=0.5,
 )
 plt.grid(True)
@@ -95,10 +121,21 @@ plt.show()
 
 
 plt.figure(figsize=(8, 5))
-plt.plot(t_grid, phi_of_t(t_grid), label=r"$\phi(t)$")
-plt.xlabel(r"$t$")
+plt.plot(t_grid_Gyr, phi_of_t(t_grid_Gyr), label=r"$\phi(t)$")
+plt.xlabel(r"$t$ [Gyr]")
 plt.ylabel(r"$\phi(t)$")
-plt.title(r"Scalar Field $\phi$ vs Time $t$")
+plt.title(r"Scalar Field $\phi$ vs Time $t$ [Gyr]")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+plt.figure(figsize=(8, 5))
+plt.plot(t_grid_Gyr, a_of_t(t_grid_Gyr), label=r"$a(t)$")
+plt.xlabel(r"$t$ [Gyr]")
+plt.ylabel(r"$a(t)$")
+plt.title(r"Scale Factor $a$ vs Time $t$ [Gyr]")
+plt.axvline(x=t_today_Gyr, color="r", linestyle="--", label="Present time", alpha=0.5)
 plt.legend()
 plt.grid(True)
 plt.show()
