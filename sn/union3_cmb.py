9301110d031f6529829a8ad00a6d936da2630d58
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-10-22 13:40:08 +0200

    union3+cmb

--- sn/union3_cmb.py
+++ sn/union3_cmb.py
@@ -1,7 +1,6 @@
 from numba import njit
 import numpy as np
-from scipy.integrate import cumulative_trapezoid
-from scipy.linalg import cho_factor, cho_solve
+from scipy.linalg import cho_factor, solve_triangular
 from y2023union3.data import get_data
 import cmb.data_union3_compression as cmb
 
@@ -9,15 +8,17 @@ c = cmb.c  # km/s
 O_r_h2 = cmb.Omega_r_h2()
 
 sn_legend, z_sn_vals, mu_vals, cov_matrix_sn = get_data()
-cho_sn = cho_factor(cov_matrix_sn)
 
-sn_grid = np.linspace(0, np.max(z_sn_vals), num=1000)
+cho_sn = cho_factor(cov_matrix_sn, lower=True)[0]
+cho_cmb = cho_factor(cmb.covariance, lower=True)[0]
+
+z_grid = np.linspace(0, np.max(z_sn_vals) + 0.1, num=1000)
+dx = np.diff(z_grid)
 
 
 @njit
 def Ez(z, params):
-    H0, Om, w0 = params[0], params[1], params[3]
-    h = H0 / 100
+    h, Om, w0 = params[0] / 100, params[1], params[3]
     Or = O_r_h2 / h**2
     Ode = 1 - Om - Or
     one_plus_z = 1 + z
@@ -27,20 +28,34 @@ def Ez(z, params):
     return np.sqrt(Or * one_plus_z**4 + Om * cubed + Ode * rho_de)
 
 
+@njit
+def DM_z(z, params):
+    dh_grid = (c / params[0]) / Ez(z_grid, params)
+    dy = (dh_grid[:-1] + dh_grid[1:]) / 2
+    cum_dm = np.zeros(z_grid.size)
+    cum_dm[1:] = np.cumsum(dx * dy)
+    return np.interp(z, z_grid, cum_dm)
+
+
+@njit
 def mu_theory(params):
-    H0, mag_offset = params[0], params[-1]
-    integral_vals = cumulative_trapezoid(1 / Ez(sn_grid, params), sn_grid, initial=0)
-    I = np.interp(z_sn_vals, sn_grid, integral_vals)
-    return mag_offset + 25 + 5 * np.log10((1 + z_sn_vals) * I * c / H0)
+    dL = (1 + z_sn_vals) * DM_z(z_sn_vals, params)
+    return params[-1] + 25 + 5 * np.log10(dL)
+
+
+def solve_triang(cho_L, delta):
+    y = solve_triangular(cho_L, delta, lower=True, check_finite=False)
+    return np.dot(y, y)
 
 
 def chi_squared(params):
     H0, Om, Ob_h2 = params[0], params[1], params[2]
-    delta = cmb.DISTANCE_PRIORS - cmb.cmb_distances(Ez, params, H0, Om, Ob_h2)
-    chi2_cmb = np.dot(delta, np.dot(cmb.inv_cov_mat, delta))
+
+    delta_cmb = cmb.DISTANCE_PRIORS - cmb.cmb_distances(Ez, params, H0, Om, Ob_h2)
+    chi2_cmb = solve_triang(cho_cmb, delta_cmb)
 
     delta_sn = mu_vals - mu_theory(params)
-    chi_sn = np.dot(delta_sn, cho_solve(cho_sn, delta_sn, check_finite=False))
+    chi_sn = solve_triang(cho_sn, delta_sn)
 
     return chi2_cmb + chi_sn
 
@@ -50,7 +65,7 @@ bounds = np.array(
         (60, 75),  # H0
         (0.1, 0.45),  # Ωm
         (0.019, 0.025),  # ωb
-        (-2.0, 0.0),  # w0
+        (-1.5, 0.0),  # w0
         (-0.7, 0.7),  # ΔM
     ],
     dtype=np.float64,
@@ -78,10 +93,10 @@ def log_probability(params):
 
 
 def main():
-    import emcee, corner
-    import matplotlib.pyplot as plt
+    import emcee
     from multiprocessing import Pool
     from .plotting import plot_predictions
+    from corner_plot import plot_corner_and_chains
     from gelman_rubin import gelman_rubin
     from log_evidence import log_evidence
 
@@ -121,15 +136,17 @@ def main():
     print("Gelman-Rubin:", gelman_rubin(chains_samples))
 
     one_sigma_percentiles = [15.9, 50, 84.1]
-
     pct = np.percentile(samples, one_sigma_percentiles, axis=0).T
-    H0_16, H0_50, H0_84 = pct[0]
-    Om_16, Om_50, Om_84 = pct[1]
-    Obh2_16, Obh2_50, Obh2_84 = pct[2]
-    w0_16, w0_50, w0_84 = pct[3]
-    dM_16, dM_50, dM_84 = pct[4]
+    [
+        (H0_16, H0_50, H0_84),
+        (Om_16, Om_50, Om_84),
+        (Obh2_16, Obh2_50, Obh2_84),
+        (w0_16, w0_50, w0_84),
+        (dM_16, dM_50, dM_84),
+    ] = pct
 
-    best_fit = np.array([H0_50, Om_50, Obh2_50, w0_50, dM_50], dtype=np.float64)
+    best_fit = np.percentile(samples, 50, axis=0)
+    degrees_of_freedom = len(mu_vals) + len(cmb.DISTANCE_PRIORS) - len(best_fit)
 
     Omh2_samples = samples[:, 1] * (samples[:, 0] / 100) ** 2
     z_star_samples = cmb.z_star(samples[:, 2], Omh2_samples)
@@ -151,6 +168,7 @@ def main():
     print(f"r_d: {cmb.rs_z(Ez, z_d_50, best_fit, H0_50, Obh2_50):.2f} Mpc")
     print(f"Chi squared: {chi_squared(best_fit):.1f}")
     print(f"Log Evidence: {log_evidence(samples, log_probs, log_probability):.1f}")
+    print(f"Degrees of freedom: {degrees_of_freedom}")
 
     plot_predictions(
         legend=sn_legend,
@@ -158,33 +176,14 @@ def main():
         y=mu_vals,
         y_err=np.sqrt(np.diag(cov_matrix_sn)),
         y_model=mu_theory(best_fit),
-        label=f"Best fit: $Ω_m$={Om_50:.3f}",
+        label=f"$Ω_m$={Om_50:.3f}",
         x_scale="log",
     )
-    labels = ["$H_0$", "$Ω_m$", "$ω_m$", "$w_0$", "$Δ_M$"]
-    corner.corner(
-        samples,
-        labels=labels,
-        quantiles=[0.159, 0.5, 0.841],
-        show_titles=True,
-        title_fmt=".4f",
-        bins=100,
-        fill_contours=False,
-        plot_datapoints=False,
-        smooth=2.0,
-        smooth1d=2.0,
-        levels=(0.393, 0.864),
+    plot_corner_and_chains(
+        labels=["$H_0$", "$Ω_m$", "$ω_m$", "$w_0$", "$Δ_M$"],
+        flat_samples=samples,
+        samples=chains_samples,
     )
-    plt.show()
-
-    plt.figure(figsize=(16, 1.5 * ndim))
-    for n in range(ndim):
-        plt.subplot2grid((ndim, 1), (n, 0))
-        plt.plot(chains_samples[:, :, n], alpha=0.3)
-        plt.ylabel(labels[n])
-        plt.xlim(0, None)
-    plt.tight_layout()
-    plt.show()
 
 
 if __name__ == "__main__":
@@ -200,66 +199,70 @@ Sample size: 22
 Flat ΛCDM w(z) = -1
 H0: 67.11 +0.56 -0.56 km/s/Mpc
 Ωm: 0.319 +0.008 -0.008
-ωm: 0.14359 +0.00119 -0.00120
+ωm: 0.14358 +0.00120 -0.00119
 ωb: 0.02234 +0.00014 -0.00014
 w0: -1
 wa: 0
-ΔM: -0.167 +0.088 -0.088
-z*: 1092.00 +0.27 -0.27
+ΔM: -0.168 +0.088 -0.088
+z*: 1091.99 +0.27 -0.27
 z_drag: 1059.88 +0.29 -0.29
 r*: 144.00 Mpc
 r_d: 146.84 Mpc
 Chi squared: 26.2
+Log Evidence: -25.9
 Degrees of freedom: 21
 
 ===============================
 
 Flat wCDM w(z) = w0
-H0: 65.20 +1.22 -1.21 km/s/Mpc
+H0: 65.20 +1.20 -1.22 km/s/Mpc
 Ωm: 0.336 +0.014 -0.013
-ωm: 0.14293 +0.00127 -0.00124
-ωb: 0.02240 +0.00015 -0.00014
-w0: -0.925 +0.043 -0.043
+ωm: 0.14293 +0.00126 -0.00124
+ωb: 0.02239 +0.00014 -0.00014
+w0: -0.925 +0.043 -0.042 (prior width 1.5: -1.5 to 0.0)
 wa: 0
-ΔM: -0.219 +0.093 -0.095
-z*: 1091.86 +0.28 -0.28
-z_drag: 1059.95 +0.29 -0.29
+ΔM: -0.220 +0.095 -0.095
+z*: 1091.87 +0.28 -0.28
+z_drag: 1059.94 +0.29 -0.29
 r*: 144.14 Mpc
-r_d: 146.97 Mpc
-Chi squared: 23.2 (Δ chi2 3.0)
+r_d: 146.96 Mpc
+Chi squared: 23.2
+Log Evidence: -27.1
 Degrees of freedom: 20
 
 ===============================
 
 Flat w(z) = -1 + 2 * (1 + w0) / (1 + (1 + z)^3)
-H0: 65.30 +1.08 -1.05 km/s/Mpc
+H0: 65.29 +1.07 -1.06 km/s/Mpc
 Ωm: 0.335 +0.012 -0.012
-ωm: 0.14289 +0.00125 -0.00125
+ωm: 0.14289 +0.00126 -0.00124
 ωb: 0.02240 +0.00014 -0.00014
-w0: -0.873 +0.066 -0.066
-wa = d w(z=0)/dz = -1.5*(1 + w0)
-ΔM: -0.213 +0.091 -0.092
+w0: -0.873 +0.067 -0.065 (prior width 1.5: -1.5 to 0.0)
+wa: d w(z) / dz at z=0 = -1.5 * (1 + w0)
+ΔM: -0.214 +0.092 -0.092
 z*: 1091.86 +0.28 -0.28
 z_drag: 1059.95 +0.29 -0.29
-r*: 144.16 Mpc
-r_d: 146.98 Mpc
-Chi squared: 22.5 (Δ chi2 3.7)
+r*: 144.15 Mpc
+r_d: 146.97 Mpc
+Chi squared: 22.5
+Log Evidence: -26.3
 Degrees of freedom: 20
 
 ===============================
 
 Flat w0waCDM w(z) = w0 + wa * z / (1 + z)
-H0: 66.48 +1.31 -1.39 km/s/Mpc
-Ωm: 0.324 +0.015 -0.013
-ωm: 0.14305 +0.00128 -0.00123
-ωb: 0.02239 +0.00014 -0.00014
-w0: -0.690 +0.153 -0.158
-wa: -1.099 +0.730 -0.737
-ΔM: -0.160 +0.101 -0.100
+H0: 66.52 +1.33 -1.38 km/s/Mpc
+Ωm: 0.323 +0.015 -0.013
+ωm: 0.14308 +0.00124 -0.00126
+ωb: 0.02238 +0.00014 -0.00014
+w0: -0.686 +0.154 -0.160 (prior width 1.5: -1.5 to 0.0)
+wa: -1.127 +0.739 -0.742 (prior width 8.5: -5.5 to 3.0)
+ΔM: -0.157 +0.099 -0.098
 z*: 1091.89 +0.28 -0.28
-z_drag: 1059.94 +0.29 -0.29
-r*: 144.11 Mpc
+z_drag: 1059.93 +0.29 -0.29
+r*: 144.12 Mpc
 r_d: 146.94 Mpc
-Chi squared: 21.8 (Δ chi2 4.4)
+Chi squared: 22.2
+Log Evidence: -27.2
 Degrees of freedom: 19
 """
