c82d13d46120ed8da7276dc0f81cf680a3c4db23
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-11-02 23:09:12 +0100

    bbn chen

--- y2024BBN/prior_lcdm_chen.py
+++ y2024BBN/prior_lcdm_chen.py
@@ -1,4 +1,10 @@
 # arXiv:2509.11743v1 (2025)
 Obh2 = 0.02232
 Obh2_sigma = 0.00033
-N_eff = 3.044
+N_eff = 3.045
+
+"""
+Using D/H=(25.47±0.25)x10-6 from PDG 2024 Review
+Obh2 = 0.02221
+Obh2_sigma = 0.00031
+"""
