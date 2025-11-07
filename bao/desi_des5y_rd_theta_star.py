8b23252361666f989b8cc489b3127c6de55aee36
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-11-07 13:51:15 +0100

    very strong evidence for thawing quintessence

--- bao/desi_des5y_rd_theta_star.py
+++ bao/desi_des5y_rd_theta_star.py
@@ -229,12 +229,12 @@ arXiv:2302.12911 - CMB Constraints on the Early Universe Independent of Late-Tim
 """
 Flat ΛCDM w(z) = -1
 H0: 68.53 +0.41 -0.41 km/s/Mpc
-ωb: 0.02262 +0.00028 -0.00028
-Ωm: 0.2995 +0.0045 -0.0044
+ωb: 0.02262 +0.00028 -0.00027
+Ωm: 0.2994 +0.0045 -0.0044
 w0: -1
 wa: 0
 r_d: 147.51 Mpc
-100 θ*: 1.04109
+100 θ*: 1.04108
 Chi squared: 1661.9
 Log evidence: -849.3
 Degrees of freedom: 1745
@@ -244,12 +244,12 @@ Degrees of freedom: 1745
 """
 Flat wCDM w(z) = w0
 H0: 67.19 +0.54 -0.54 km/s/Mpc
-ωb: 0.02344 +0.00038 -0.00037
-Ωm: 0.3064 +0.0049 -0.0048
-w0: -0.903 +0.026 -0.027 (prior width 1.5: -1.5 to 0.0)
+ωb: 0.02346 +0.00038 -0.00037
+Ωm: 0.3063 +0.0049 -0.0048
+w0: -0.903 +0.027 -0.027 (prior width 1.5: -1.5 to 0.0)
 wa: 0
 r_d: 147.44 Mpc
-100 θ*: 1.04098
+100 θ*: 1.04097
 Chi squared: 1649.5
 Log evidence: -846.2 (Δ logZ = 3.1 against ΛCDM)
 Degrees of freedom: 1744
@@ -259,12 +259,12 @@ Degrees of freedom: 1744
 """
 Flat w(z) = -1 + 4 * (1 + w0) / (1 + 3 * (1 + z)**3)
 H0: 66.88 +0.57 -0.57 km/s/Mpc
-ωb: 0.02310 +0.00030 -0.00030
-Ωm: 0.3116 +0.0056 -0.0055
+ωb: 0.02311 +0.00031 -0.00030
+Ωm: 0.3115 +0.0056 -0.0055
 w0: -0.831 +0.042 -0.042 (prior width 1.5: -1.5 to 0.0)
 wa: d w(z)/dz at z=0 = -(9/4) * (1 + w0)
 r_d: 147.44 Mpc
-100 θ*: 1.04102
+100 θ*: 1.04101
 Chi squared: 1646.6
 Log evidence: -844.3 (Δ logZ = 5.0 against ΛCDM)
 Degrees of freedom: 1744
