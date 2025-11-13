e20cceb3641ed3006fc0492dc6da6ecd88333eec
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-11-13 14:11:38 +0100

    cmb

--- bao/desi_des5y_obh2_theta_star.py
+++ bao/desi_des5y_obh2_theta_star.py
@@ -6,7 +6,8 @@ from y2024DES.data import get_data, effective_sample_size as sn_size
 from y2025BAO.data import get_data as get_bao_data
 
 c = cmb.c  # Speed of light in km/s
-Orh2 = cmb.Omega_r_h2()
+Orh2 = cmb.Omega_r_h2(2.044)
+Omnu_h2 = cmb.Omnu_h2
 
 sn_legend, z_cmb, z_hel, mu_values, cov_matrix_sn = get_data()
 bao_legend, bao_data, cov_matrix_bao = get_bao_data()
@@ -16,7 +17,6 @@ cho_bao = cho_factor(cov_matrix_bao, lower=True)[0]
 
 """
 Planck compressed priors for π/θ* and ωb, without the shift parameter R (arXiv:1808.05724v1)
-The geometry is constrained by θ* and the sound horizon by ωb.
 """
 cho_cmb = cho_factor(cmb.covariance[1:, 1:], lower=True)[0]
 
@@ -26,26 +26,27 @@ dx = np.diff(z_grid)
 
 
 @njit
-def Ez(z, theta):
-    h, Om, w0 = theta[0] / 100, theta[1], theta[3]
-    z_plus_1 = 1 + z
-    Or = Orh2 / h**2
-    Ode = 1 - Om - Or
-    cubed = z_plus_1**3
-    rho_de = (4 * cubed / (1 + 3 * cubed)) ** (4 * (1 + w0))
-
-    return np.sqrt(Or * z_plus_1**4 + Om * cubed + Ode * rho_de)
+def Ez(z, Obc, Or, w0=-1, wa=0):
+    Ol = 1 - Obc - Or
+    inv_a = 1 + z
+    cubic = inv_a**3
+    rho_de = (4 * cubic / (1 + 3 * cubic)) ** (4 * (1 + w0))
+    return np.sqrt(Or * inv_a**4 + Obc * cubic + Ol * rho_de)
 
 
 @njit
 def theory_mu(theta):
     dL = (1 + z_hel) * DM_z(z_cmb, theta)
-    return theta[-1] + 25 + 5 * np.log10(dL)
+    return theta[0] + 25 + 5 * np.log10(dL)
 
 
 @njit
 def H_z(z, theta):
-    return theta[0] * Ez(z, theta)
+    H0, Obh2, Och2, w0 = theta[1:]
+    h = H0 / 100
+    Obc = (Obh2 + Och2 + Omnu_h2) / h**2
+    Or = Orh2 / h**2
+    return H0 * Ez(z, Obc, Or, w0)
 
 
 @njit
@@ -74,9 +75,8 @@ quantities = np.array([qty_map[q] for q in bao_data["quantity"]], dtype=np.int64
 
 
 def bao_theory(z, qty, theta):
-    H0, Om, Obh2 = theta[0], theta[1], theta[2]
-    zd = cmb.z_drag(wb=Obh2, wm=Om * (H0 / 100) ** 2)
-    rd = cmb.rs_z(Ez, zd, theta, H0, Obh2)
+    Obh2, Och2 = theta[2], theta[3]
+    rd = cmb.r_drag(wb=Obh2, wm=Obh2 + Och2 + Omnu_h2)
 
     DV_mask = qty == 0
     DM_mask = qty == 1
@@ -94,9 +94,7 @@ def solve_triang(cho_L, delta):
 
 
 def chi_squared(theta):
-    H0, Om, Obh2 = theta[0], theta[1], theta[2]
-
-    delta_cmb = cmb.DISTANCE_PRIORS - cmb.cmb_distances(Ez, theta, H0, Om, Obh2)
+    delta_cmb = cmb.DISTANCE_PRIORS - cmb.cmb_distances(Ez, *theta[1:])
     chi2_cmb = solve_triang(cho_cmb, delta_cmb[1:])
 
     delta_sn = mu_values - theory_mu(theta)
@@ -110,11 +108,11 @@ def chi_squared(theta):
 
 bounds = np.array(
     [
-        (50.0, 90.0),  # H0
-        (0.1, 0.7),  # Ωm
-        (0.020, 0.024),  # ωb = Ωb * h^2
-        (-1.5, 0.0),  # w0
         (-0.4, 0.4),  # ΔM nuisance magnitude offset
+        (50.0, 90.0),  # H0
+        (0.010, 0.030),  # ωb = Ωb * h^2
+        (0.05, 0.3),  # ωc = Ωc * h^2
+        (-1.5, 0.0),  # w0
     ],
     dtype=np.float64,
 )
@@ -178,39 +176,37 @@ def main():
     log_evd = log_evidence(samples, log_probs, log_probability, bounds)
 
     [
-        (H0_16, H0_50, H0_84),
-        (Om_16, Om_50, Om_84),
-        (Obh2_16, Obh2_50, Obh2_84),
-        (w0_16, w0_50, w0_84),
         (dM_16, dM_50, dM_84),
+        (H0_16, H0_50, H0_84),
+        (Obh2_16, Obh2_50, Obh2_84),
+        (Och2_16, Och2_50, Och2_84),
+        (w0_16, w0_50, w0_84),
     ] = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
 
     best_fit = np.percentile(samples, 50, axis=0)
-    Omh2_samples = samples[:, 1] * (samples[:, 0] / 100) ** 2
+    Omh2_samples = samples[:, 2] + samples[:, 3] + Omnu_h2
+    Om_samples = Omh2_samples / (samples[:, 1] / 100) ** 2
     zd_samples = cmb.z_drag(wb=samples[:, 2], wm=Omh2_samples)
     z_st_samples = cmb.z_star(wb=samples[:, 2], wm=Omh2_samples)
     Omh2_16, Omh2_50, Omh2_84 = np.percentile(Omh2_samples, [15.9, 50, 84.1])
+    Om_16, Om_50, Om_84 = np.percentile(Om_samples, [15.9, 50, 84.1])
     zd_16, zd_50, zd_84 = np.percentile(zd_samples, [15.9, 50, 84.1])
     z_st_16, z_st_50, z_st_84 = np.percentile(z_st_samples, [15.9, 50, 84.1])
-    R = (
-        100
-        * np.sqrt(Omh2_50)
-        * (1 + z_st_50)
-        * cmb.DA_z(Ez, z_st_50, best_fit, H0_50)
-        / c
-    )
+    R, lA = cmb.cmb_distances(Ez, *best_fit[1:])[:2]
 
     print(f"ΔM: {dM_50:.3f} +{(dM_84 - dM_50):.3f} -{(dM_50 - dM_16):.3f} mag")
     print(f"H0: {H0_50:.2f} +{(H0_84 - H0_50):.2f} -{(H0_50 - H0_16):.2f} km/s/Mpc")
     print(f"Ωm: {Om_50:.3f} +{(Om_84 - Om_50):.3f} -{(Om_50 - Om_16):.3f}")
-    print(f"ωm: {Omh2_50:.4f} +{(Omh2_84 - Omh2_50):.4f} -{(Omh2_50 - Omh2_16):.4f}")
     print(f"ωb: {Obh2_50:.5f} +{(Obh2_84 - Obh2_50):.5f} -{(Obh2_50 - Obh2_16):.5f}")
+    print(f"ωc: {Och2_50:.4f} +{(Och2_84 - Och2_50):.4f} -{(Och2_50 - Och2_16):.4f}")
+    print(f"ωm: {Omh2_50:.4f} +{(Omh2_84 - Omh2_50):.4f} -{(Omh2_50 - Omh2_16):.4f}")
     print(f"w0: {w0_50:.3f} +{(w0_84 - w0_50):.3f} -{(w0_50 - w0_16):.3f}")
     print(f"z_d: {zd_50:.2f} +{(zd_84 - zd_50):.2f} -{(zd_50 - zd_16):.2f}")
-    print(f"r_d: {cmb.rs_z(Ez, zd_50, best_fit, H0_50, Obh2_50):.2f} Mpc")
+    print(f"r_d: {cmb.rs_z(Ez, zd_50, *best_fit[1:]):.2f} Mpc")
     print(f"z*: {z_st_50:.2f} +{(z_st_84 - z_st_50):.2f} -{(z_st_50 - z_st_16):.2f}")
-    print(f"r*: {cmb.rs_z(Ez, z_st_50, best_fit, H0_50, Obh2_50):.2f} Mpc")
-    print(f"shift parameter R: {R:.3f}")
+    print(f"r*: {cmb.rs_z(Ez, z_st_50, *best_fit[1:]):.2f} Mpc")
+    print(f"shift R: {R:.3f}")
+    print(f"100 θ*: {100 * np.pi / lA:.5f}")
     print(f"Chi squared: {chi_squared(best_fit):.2f}")
     print(f"Log Evidence: {log_evd:.2f}")
     print(f"Degrees of freedom: {2 + bao_data['value'].size + sn_size - len(best_fit)}")
@@ -231,7 +227,7 @@ def main():
         x_scale="log",
     )
     plot_corner_and_chains(
-        labels=["$H_0$", "$Ω_m$", "$ω_b$", "$w_0$", "$Δ_M$"],
+        labels=["$Δ_M$", "$H_0$", "$ω_b$", "$ω_c$", "$w_0$"],
         flat_samples=samples,
         samples=chains_samples,
     )
@@ -250,50 +246,22 @@ DESI DR2 + DES5Y + (π/θ*, ωb)CMB
 """
 Flat ΛCDM  w(z) = -1
 
-** Chen's compressed priors for ΛCDM **
-H0: 68.52 +0.29 -0.29 km/s/Mpc
-Ωm: 0.300 +0.004 -0.004
-ωm: 0.1408 +0.0008 -0.0008
-ωb: 0.02235 +0.00014 -0.00014
-z_d: 1059.69 +0.35 -0.35
-r_d: 147.55 Mpc (1.0179 x r*)
-z*: 1088.71 +0.15 -0.15
-r*: 144.96 Mpc
-R: 1.741
-Chi squared: 1661.47
-Log Evidence: -847.53
-Degrees of freedom: 1746
-
 ** Planck + ATC DR6 compression **
-H0: 68.44 +0.28 -0.28 km/s/Mpc
+H0: 68.45 +0.28 -0.28 km/s/Mpc
 Ωm: 0.299 +0.004 -0.004
-ωm: 0.1401 +0.0008 -0.0008
 ωb: 0.02249 +0.00011 -0.00011
+ωc: 0.1170 +0.0008 -0.0008
+ωm: 0.1401 +0.0008 -0.0008
 w0: -1
 wa: 0
-z_d: 1058.24 +0.26 -0.26
+z_d: 1059.99 +0.26 -0.26
 r_d: 147.77 Mpc
-z*: 1088.19 +0.15 -0.15
-r*: 145.09 Mpc
-R: 1.740
+z*: 1089.49 +0.15 -0.15
+r*: 145.14 Mpc
+shift R: 1.741
+100 θ*: 1.04096
 Chi squared: 1661.81
-Log Evidence: -848.13
-Degrees of freedom: 1746
-
-** Early ΛCDM **
-H0: 68.28 +0.29 -0.29 km/s/Mpc
-Ωm: 0.300 +0.004 -0.004
-ωm: 0.1399 +0.0008 -0.0008
-ωb: 0.02222 +0.00014 -0.00014
-w0: -1
-wa: 0
-z_d: 1057.75 +0.34 -0.35
-r_d: 148.05 Mpc
-z*: 1088.80 +0.15 -0.15
-r*: 145.26 Mpc
-R: 1.741
-Chi squared: 1661.33
-Log Evidence: -847.67
+Log Evidence: -849.63
 Degrees of freedom: 1746
 """
 
@@ -301,50 +269,22 @@ Degrees of freedom: 1746
 """
 Flat wCDM w(z) = w0
 
-** Chen's compressed priors for wCDM **
-H0: 66.82 +0.58 -0.58 km/s/Mpc
-Ωm: 0.309 +0.005 -0.005
-ωm: 0.1378 +0.0013 -0.0013
-ωb: 0.02240 +0.00015 -0.00015
-w0: -0.913 +0.025 -0.025 (prior width 1.5: -1.5 to 0.0)
-z_d: 1059.57 +0.36 -0.36
-r_d: 148.33 Mpc (1.0178 x r*)
-z*: 1088.46 +0.17 -0.17
-r*: 145.73 Mpc
-R: 1.731
-Chi squared: 1650.22
-Log Evidence: -845.06 (Δ logZ = 2.47 against ΛCDM)
-Degrees of freedom: 1745
-
 ** Planck + ATC DR6 compression **
-H0: 66.69 +0.58 -0.57 km/s/Mpc
+H0: 66.70 +0.58 -0.57 km/s/Mpc
 Ωm: 0.308 +0.005 -0.005
-ωm: 0.1370 +0.0013 -0.0012
 ωb: 0.02250 +0.00011 -0.00011
+ωc: 0.1138 +0.0013 -0.0012
+ωm: 0.1370 +0.0013 -0.0013
 w0: -0.911 +0.025 -0.025 (prior width 1.5: -1.5 to 0.0)
 wa: 0
-z_d: 1058.03 +0.27 -0.27
+z_d: 1059.78 +0.27 -0.27
 r_d: 148.62 Mpc
-z*: 1087.90 +0.18 -0.18
-r*: 145.92 Mpc
-R: 1.731
-Chi squared: 1650.02
-Log Evidence: -845.40 (Δ logZ = 2.73 against ΛCDM)
-Degrees of freedom: 1745
-
-** Early ΛCDM **
-H0: 66.59 +0.57 -0.57 km/s/Mpc
-Ωm: 0.309 +0.005 -0.005
-ωm: 0.1369 +0.0012 -0.0013
-ωb: 0.02224 +0.00014 -0.00014
-w0: -0.913 +0.025 -0.025
-z_d: 1057.56 +0.35 -0.35
-r_d: 148.84 Mpc
-z*: 1088.57 +0.16 -0.16
-r*: 146.04 Mpc
-R: 1.731
-Chi squared: 1650.30
-Log Evidence: -845.32 (Δ logZ = 2.35 against ΛCDM)
+z*: 1089.20 +0.17 -0.18
+r*: 145.97 Mpc
+shift R: 1.731
+100 θ*: 1.04091
+Chi squared: 1650.01
+Log Evidence: -846.93 (Δ logZ = 2.70 against ΛCDM)
 Degrees of freedom: 1745
 """
 
@@ -352,52 +292,22 @@ Degrees of freedom: 1745
 """
 Flat w(z) = -1 + 4 * (1 + w0) / (1 + 3 * (1 + z)^3)
 
-** Chen's compressed priors for wCDM **
-H0: 66.65 +0.56 -0.55 km/s/Mpc
-Ωm: 0.313 +0.005 -0.005
-ωm: 0.1390 +0.0010 -0.0010
-ωb: 0.02239 +0.00015 -0.00015
-w0: -0.839 +0.041 -0.041 (prior width 1.5: -1.5 to 0.0)
-wa: d w(z)/dz at z=0 = -(9/4) * (1 + w0)
-z_d: 1059.66 +0.36 -0.36
-r_d: 147.98 Mpc
-z*: 1088.55 +0.15 -0.15
-r*: 145.39 Mpc
-R: 1.735
-Chi squared: 1646.94
-Log Evidence: -842.93 (Δ logZ = 4.60 against ΛCDM)
-Degrees of freedom: 1745
-
 ** Planck + ATC DR6 compression **
-H0: 66.52 +0.55 -0.55 km/s/Mpc
-Ωm: 0.313 +0.005 -0.005
-ωm: 0.1383 +0.0009 -0.0009
+H0: 66.54 +0.55 -0.55 km/s/Mpc
+Ωm: 0.312 +0.005 -0.005
 ωb: 0.02250 +0.00011 -0.00011
-w0: -0.836 +0.041 -0.041 (prior width 1.5: -1.5 to 0.0)
+ωc: 0.1152 +0.0009 -0.0009
+ωm: 0.1383 +0.0009 -0.0009
+w0: -0.837 +0.041 -0.041 (prior width 1.5: -1.5 to 0.0)
 wa: d w(z)/dz at z=0 = -(9/4) * (1 + w0)
-z_d: 1058.13 +0.26 -0.26
+z_d: 1059.88 +0.26 -0.26
 r_d: 148.26 Mpc
-z*: 1088.02 +0.16 -0.16
-r*: 145.57 Mpc
-R: 1.735
-Chi squared: 1646.80
-Log Evidence: -843.31 (Δ logZ = 4.82 against ΛCDM)
-Degrees of freedom: 1745
-
-** Early ΛCDM **
-H0: 66.41 +0.56 -0.55 km/s/Mpc
-Ωm: 0.313 +0.005 -0.005
-ωm: 0.1382 +0.0009 -0.0009
-ωb: 0.02223 +0.00014 -0.00014
-w0: -0.840 +0.041 -0.041 (prior width 1.5: -1.5 to 0.0)
-wa: d w(z)/dz at z=0 = -(9/4) * (1 + w0)
-z_d: 1057.65 +0.35 -0.34
-r_d: 148.51 Mpc
-z*: 1088.66 +0.15 -0.15
-r*: 145.71 Mpc
-R: 1.735
-Chi squared: 1647.01
-Log Evidence: -843.18 (Δ logZ = 4.49 against ΛCDM)
+z*: 1089.32 +0.16 -0.16
+r*: 145.62 Mpc
+shift R: 1.735
+100 θ*: 1.04094
+Chi squared: 1646.79
+Log Evidence: -844.84 (Δ logZ = 4.79 against ΛCDM)
 Degrees of freedom: 1745
 """
 
@@ -405,51 +315,8 @@ Degrees of freedom: 1745
 """
 Flat w0waCDM w(z) = w0 + wa * z / (1 + z)
 
-** Chen's compressed priors for ΛCDM **
-H0: 66.86 +0.56 -0.56 km/s/Mpc
-Ωm: 0.316 +0.006 -0.006
-ωm: 0.1414 +0.0017 -0.0018
-ωb: 0.02236 +0.00014 -0.00014
-w0: -0.790 +0.061 -0.060 (prior width 1.5: -1.5 to 0.0)
-wa: -0.615 +0.271 -0.282 (prior width 3.5: -2.5 to 1.0)
-z_d: 1059.75 +0.36 -0.37
-r_d: 147.38 Mpc (1.0178 x r*)
-z*: 1088.75 +0.18 -0.18
-r*: 144.80 Mpc
-R: 1.743
-Chi squared: 1645.79
-Log Evidence: -844.21 (Δ logZ = 3.32 against ΛCDM)
-Degrees of freedom: 1744
-
 ** Planck + ATC DR6 compression **
-H0: 66.74 +0.56 -0.55 km/s/Mpc
-Ωm: 0.316 +0.006 -0.006
-ωm: 0.1407 +0.0017 -0.0018
-ωb: 0.02249 +0.00011 -0.00011
-w0: -0.791 +0.061 -0.060 (prior width 1.5: -1.5 to 0.0)
-wa: -0.594 +0.269 -0.282 (prior width 3.5: -2.5 to 1.0)
-z_d: 1058.29 +0.28 -0.28
-r_d: 147.63 Mpc
-z*: 1088.23 +0.20 -0.21
-r*: 144.96 Mpc
-shift parameter R: 1.742
-Chi squared: 1645.78
-Log Evidence: -844.65 (Δ logZ = 3.48 against ΛCDM)
-Degrees of freedom: 1744
-
-** Early ΛCDM **
-H0: 66.64 +0.54 -0.55 km/s/Mpc
-Ωm: 0.317 +0.006 -0.006
-ωm: 0.1407 +0.0016 -0.0018
-ωb: 0.02223 +0.00014 -0.00014
-w0: -0.790 +0.061 -0.059 (prior width 1.5: -1.5 to 0.0)
-wa: -0.617 +0.268 -0.284 (prior width 3.5: -2.5 to 1.0)
-z_d: 1057.81 +0.36 -0.36
-r_d: 147.86 Mpc
-z*: 1088.84 +0.18 -0.18
-r*: 145.09 Mpc
-R: 1.744
-Chi squared: 1645.79
-Log Evidence: -844.39 (Δ logZ = 3.28 against ΛCDM)
-Degrees of freedom: 1744
+TODO
+w0: (prior width 1.5: -1.5 to 0.0)
+wa: (prior width 3.5: -2.5 to 1.0)
 """
