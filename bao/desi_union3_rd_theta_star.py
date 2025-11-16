80ba709ab0beeee1d2748fd38424ddce52b6c1b4
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-11-16 12:24:31 +0100

    desi dr2+union3+rd+theta*

--- bao/desi_union3_rd_theta_star.py
+++ bao/desi_union3_rd_theta_star.py
@@ -31,12 +31,6 @@ def Ez(z, Obc, Or, w0=-1, wa=0):
 
 
 @njit
-def theory_mu(params):
-    dL = (1 + z_cmb) * DM_z(z_cmb, params)
-    return params[0] + 25 + 5 * np.log10(dL)
-
-
-@njit
 def H_z(z, params):
     H0, Obh2, Och2, w0 = params[1:]
     h = H0 / 100
@@ -82,6 +76,12 @@ def bao_theory(z, qty, rd, params):
     return results / rd
 
 
+@njit
+def theory_mu(params):
+    dL = (1 + z_cmb) * DM_z(z_cmb, params)
+    return params[0] + 25 + 5 * np.log10(dL)
+
+
 def solve_triang(cho_L, delta):
     y = solve_triangular(cho_L, delta, lower=True, check_finite=False)
     return np.dot(y, y)
@@ -105,10 +105,10 @@ def chi_squared(params):
 
 bounds = np.array(
     [
-        (-0.8, 0.8),  # ΔM
+        (-1.0, 1.0),  # ΔM
         (50.0, 90.0),  # H0
         (0.010, 0.030),  # Ob * h^2
-        (0.05, 0.3),  # Ωm * h^2
+        (0.05, 0.30),  # Ωc * h^2
         (-1.5, 0.0),  # w0
     ],
     dtype=np.float64,
@@ -147,8 +147,8 @@ def main():
     np.random.seed(42)
     ndim = len(bounds)
     nwalkers = 150
-    burn_in = 300
-    nsteps = 2000 + burn_in
+    burn_in = 500
+    nsteps = 2500 + burn_in
     np.random.seed(42)
     initial_pos = np.random.uniform(bounds[:, 0], bounds[:, 1], size=(nwalkers, ndim))
     moves = [
@@ -174,7 +174,8 @@ def main():
     log_evd = log_evidence(samples, log_probs, log_probability, bounds)
     print("Gelman-Rubin:", gelman_rubin(chains_samples))
 
-    pct = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
+    one_sigma_ci = [15.9, 50, 84.1]
+    pct = np.percentile(samples, one_sigma_ci, axis=0).T
     [
         (dM_16, dM_50, dM_84),
         (H0_16, H0_50, H0_84),
@@ -184,12 +185,18 @@ def main():
     ] = pct
 
     best_fit = np.percentile(samples, 50, axis=0)
-    theta_100, rd = cmb.cmb_distances(Ez, *best_fit[1:])
+    theta_100, _ = cmb.cmb_distances(Ez, *best_fit[1:])
 
     omh2_samples = samples[:, 2] + samples[:, 3] + Omnu_h2
     Om_samples = omh2_samples / (samples[:, 1] / 100) ** 2
-    omh2_16, omh2_50, omh2_84 = np.percentile(omh2_samples, [15.9, 50, 84.1])
-    Om_16, Om_50, Om_84 = np.percentile(Om_samples, [15.9, 50, 84.1])
+    rd_samples = cmb.r_drag(samples[:, 2], omh2_samples)
+    zd_samples = cmb.z_drag(samples[:, 2], omh2_samples)
+    zst_samples = cmb.z_star(samples[:, 2], omh2_samples)
+    omh2_16, omh2_50, omh2_84 = np.percentile(omh2_samples, one_sigma_ci)
+    Om_16, Om_50, Om_84 = np.percentile(Om_samples, one_sigma_ci)
+    rd_16, rd_50, rd_84 = np.percentile(rd_samples, one_sigma_ci)
+    zd_16, zd_50, zd_84 = np.percentile(zd_samples, one_sigma_ci)
+    zst_16, zst_50, zst_84 = np.percentile(zst_samples, one_sigma_ci)
 
     print(f"ΔM: {dM_50:.3f} +{(dM_84 - dM_50):.3f} -{(dM_50 - dM_16):.3f} mag")
     print(f"H0: {H0_50:.2f} +{(H0_84 - H0_50):.2f} -{(H0_50 - H0_16):.2f} km/s/Mpc")
@@ -198,14 +205,17 @@ def main():
     print(f"ωm: {omh2_50:.4f} +{(omh2_84 - omh2_50):.4f} -{(omh2_50 - omh2_16):.4f}")
     print(f"Ωm: {Om_50:.4f} +{(Om_84 - Om_50):.4f} -{(Om_50 - Om_16):.4f}")
     print(f"w0: {w0_50:.3f} +{(w0_84 - w0_50):.3f} -{(w0_50 - w0_16):.3f}")
-    print(f"r_d: {rd:.2f} Mpc")
+    print(f"r_d: {rd_50:.2f} +{(rd_84 - rd_50):.2f} -{(rd_50 - rd_16):.2f} Mpc")
+    print(f"z_d: {zd_50:.2f} +{(zd_84 - zd_50):.2f} -{(zd_50 - zd_16):.2f}")
+    print(f"z*: {zst_50:.2f} +{(zst_84 - zst_50):.2f} -{(zst_50 - zst_16):.2f}")
+    print(f"r*: {cmb.rs_z(Ez, zst_50, *best_fit[1:]):.2f} Mpc")
     print(f"100 θ*: {theta_100:.5f}")
     print(f"Chi squared: {chi_squared(best_fit):.1f}")
     print(f"Log evidence: {log_evd:.1f}")
     print(f"Degrees of freedom: {1 + len(bao_data['z']) + len(z_cmb) - len(best_fit)}")
 
     plot_bao_predictions(
-        theory_predictions=lambda z, qty: bao_theory(z, qty, rd, best_fit),
+        theory_predictions=lambda z, qty: bao_theory(z, qty, rd_50, best_fit),
         data=bao_data,
         errors=np.sqrt(np.diag(cov_matrix_bao)),
         title=bao_legend,
@@ -253,30 +263,36 @@ Degrees of freedom: 32
 
 -- ATC DR6 --
 H0: 69.59 +0.52 -0.52 km/s/Mpc
-ωb: 0.02399 +0.00051 -0.00051
+ωb: 0.02398 +0.00052 -0.00051
 ωc: 0.1173 +0.0008 -0.0008
-ωm: 0.1419 +0.0009 -0.0008
-Ωm: 0.2930 +0.0048 -0.0047
+ωm: 0.1419 +0.0008 -0.0008
+Ωm: 0.2930 +0.0049 -0.0048
 w0: -1
 wa: 0
-r_d: 146.05 Mpc
-100 θ*: 1.04082
+r_d: 146.05 +0.55 -0.56 Mpc
+z_d: 1063.38 +1.12 -1.12
+z*: 1087.65 +0.64 -0.62
+r*: 143.93 Mpc
+100 θ*: 1.04083
 Chi squared: 41.3
-Log evidence: -35.4
+Log evidence: -35.6
 Degrees of freedom: 32
 
 -- ATC DR6 + Planck --
 H0: 68.95 +0.44 -0.44 km/s/Mpc
-ωb: 0.02304 +0.00030 -0.00030
+ωb: 0.02304 +0.00031 -0.00030
 ωc: 0.1169 +0.0007 -0.0007
 ωm: 0.1406 +0.0007 -0.0006
-Ωm: 0.2958 +0.0047 -0.0046
+Ωm: 0.2957 +0.0048 -0.0047
 w0: -1
 wa: 0
-r_d: 147.18 Mpc
+r_d: 147.18 +0.29 -0.29 Mpc
+z_d: 1061.24 +0.66 -0.67
+z*: 1088.77 +0.43 -0.42
+r*: 144.74 Mpc
 100 θ*: 1.04097
 Chi squared: 40.2
-Log evidence: -35.6
+Log evidence: -35.9
 Degrees of freedom: 32
 """
 
@@ -299,17 +315,20 @@ Log evidence: -35.2 (Δ logZ = 0.4 against ΛCDM)
 Degrees of freedom: 31
 
 -- ATC DR6 --
-H0: 67.93 +0.75 -0.73 km/s/Mpc
-ωb: 0.02502 +0.00065 -0.00063
+H0: 67.94 +0.74 -0.73 km/s/Mpc
+ωb: 0.02502 +0.00065 -0.00064
 ωc: 0.1140 +0.0014 -0.0015
 ωm: 0.1396 +0.0012 -0.0012
 Ωm: 0.3025 +0.0057 -0.0057
-w0: -0.892 +0.036 -0.036 (prior width 1.5: -1.5 to 0.0)
+w0: -0.893 +0.036 -0.036 (prior width 1.5: -1.5 to 0.0)
 wa: 0
-r_d: 145.81 Mpc
-100 θ*: 1.04069
+r_d: 145.81 +0.56 -0.55 Mpc
+z_d: 1065.39 +1.31 -1.32
+z*: 1086.20 +0.79 -0.77
+r*: 144.00 Mpc
+100 θ*: 1.04071
 Chi squared: 32.7
-Log evidence: -34.0 (Δ logZ = 1.4 against ΛCDM)
+Log evidence: -34.2 (Δ logZ = 1.4 against ΛCDM)
 Degrees of freedom: 31
 
 -- ATC DR6 + Planck --
@@ -317,13 +336,16 @@ H0: 67.41 +0.72 -0.71 km/s/Mpc
 ωb: 0.02380 +0.00045 -0.00044
 ωc: 0.1140 +0.0014 -0.0015
 ωm: 0.1384 +0.0011 -0.0011
-Ωm: 0.3046 +0.0058 -0.0057
+Ωm: 0.3046 +0.0059 -0.0057
 w0: -0.904 +0.036 -0.037 (prior width 1.5: -1.5 to 0.0)
 wa: 0
-r_d: 147.12 Mpc
+r_d: 147.12 +0.30 -0.29 Mpc
+z_d: 1062.75 +0.92 -0.89
+z*: 1087.57 +0.63 -0.63
+r*: 144.93 Mpc
 100 θ*: 1.04091
 Chi squared: 33.2
-Log evidence: -35.0 (Δ logZ = 0.6 against ΛCDM)
+Log evidence: -35.2 (Δ logZ = 0.7 against ΛCDM)
 Degrees of freedom: 31
 """
 
@@ -347,31 +369,37 @@ Log evidence: -33.2 (Δ logZ = 2.4 against ΛCDM)
 Degrees of freedom: 31
 
 -- ATC DR6 --
-H0: 67.17 +0.87 -0.84 km/s/Mpc
-ωb: 0.02461 +0.00054 -0.00054
+H0: 67.16 +0.87 -0.85 km/s/Mpc
+ωb: 0.02461 +0.00055 -0.00055
 ωc: 0.1155 +0.0009 -0.0009
 ωm: 0.1408 +0.0009 -0.0009
-Ωm: 0.3120 +0.0076 -0.0076
-w0: -0.783 +0.065 -0.065 (prior width 1.5: -1.5 to 0.0)
+Ωm: 0.3120 +0.0077 -0.0076
+w0: -0.782 +0.065 -0.065 (prior width 1.5: -1.5 to 0.0)
 wa: d w(z)/d z at z=0 = (9/4) * (1 + w0)
-r_d: 145.84 Mpc
-100 θ*: 1.04071
+r_d: 145.84 +0.56 -0.55 Mpc
+z_d: 1064.63 +1.15 -1.17
+z*: 1086.78 +0.66 -0.64
+r*: 143.91 Mpc
+100 θ*: 1.04073
 Chi squared: 30.1
-Log evidence: -32.1 (Δ logZ = 3.3 against ΛCDM)
+Log evidence: -32.3 (Δ logZ = 3.3 against ΛCDM)
 Degrees of freedom: 31
 
 -- ATC DR6 + Planck --
-H0: 66.66 +0.85 -0.82 km/s/Mpc
-ωb: 0.02350 +0.00034 -0.00034
+H0: 66.65 +0.84 -0.83 km/s/Mpc
+ωb: 0.02349 +0.00034 -0.00034
 ωc: 0.1152 +0.0009 -0.0009
 ωm: 0.1394 +0.0008 -0.0008
-Ωm: 0.3137 +0.0077 -0.0077
-w0: -0.796 +0.064 -0.066 (prior width 1.5: -1.5 to 0.0)
+Ωm: 0.3138 +0.0077 -0.0076
+w0: -0.796 +0.065 -0.065 (prior width 1.5: -1.5 to 0.0)
 wa: d w(z)/d z at z=0 = (9/4) * (1 + w0)
-r_d: 147.12 Mpc
-100 θ*: 1.04095
+r_d: 147.13 +0.29 -0.29 Mpc
+z_d: 1062.14 +0.73 -0.73
+z*: 1088.06 +0.48 -0.47
+r*: 144.84 Mpc
+100 θ*: 1.04092
 Chi squared: 30.4
-Log evidence: -33.1 (Δ logZ = 2.5 against ΛCDM)
+Log evidence: -33.3 (Δ logZ = 2.6 against ΛCDM)
 Degrees of freedom: 31
 """
 
@@ -394,30 +422,36 @@ Log evidence: -34.3 (Δ logZ = 1.3 against ΛCDM)
 Degrees of freedom: 30
 
 -- ATC DR6 --
-H0: 66.94 +0.90 -0.91 km/s/Mpc
-ωb: 0.02396 +0.00078 -0.00074
-ωc: 0.1163 +0.0019 -0.0023
-ωm: 0.1408 +0.0015 -0.0018
-Ωm: 0.3142 +0.0102 -0.0102
-w0: -0.747 +0.095 -0.092 (prior width 1.5: -1.5 to 0.0)
-wa: -0.584 +0.366 -0.385 (prior width 4.5: -3.0 to 1.5)
-r_d: 145.92 Mpc
-100 θ*: 1.04079
-Chi squared: 30.1
-Log evidence: -34.0 (Δ logZ = 2.2 against ΛCDM)
+H0: 66.89 +0.94 -0.94 km/s/Mpc
+ωb: 0.02400 +0.00079 -0.00073
+ωc: 0.1178 +0.0019 -0.0023
+ωm: 0.1423 +0.0015 -0.0017
+Ωm: 0.3181 +0.0106 -0.0105
+w0: -0.734 +0.101 -0.098 (prior width 1.5: -1.5 to 0.0)
+wa: -0.685 +0.393 -0.411 (prior width 4.5: -3.0 to 1.5)
+r_d: 145.93 +0.56 -0.56 Mpc
+z_d: 1063.46 +1.61 -1.54
+z*: 1087.67 +1.01 -1.06
+r*: 143.80 Mpc
+100 θ*: 1.04070
+Chi squared: 29.6
+Log evidence: -34.0 (Δ logZ = 1.6 against ΛCDM)
 Degrees of freedom: 30
 
 -- ATC DR6 + Planck --
-H0: 66.28 +0.87 -0.88 km/s/Mpc
-ωb: 0.02281 +0.00054 -0.00050
-ωc: 0.1180 +0.0017 -0.0019
-ωm: 0.1414 +0.0013 -0.0015
-Ωm: 0.3218 +0.0101 -0.0098
-w0: -0.722 +0.099 -0.094 (prior width 1.5: -1.5 to 0.0)
-wa: -0.787 +0.376 -0.398 (prior width 4.5: -3.0 to 1.5)
-r_d: 147.15 Mpc
-100 θ*: 1.04090
+H0: 66.26 +0.91 -0.89 km/s/Mpc
+ωb: 0.02280 +0.00058 -0.00050
+ωc: 0.1180 +0.0018 -0.0020
+ωm: 0.1414 +0.0014 -0.0016
+Ωm: 0.3220 +0.0103 -0.0103
+w0: -0.721 +0.103 -0.099 (prior width 1.5: -1.5 to 0.0)
+wa: -0.785 +0.394 -0.420 (prior width 4.5: -3.0 to 1.5)
+r_d: 147.16 +0.29 -0.30 Mpc
+z_d: 1060.78 +1.18 -1.05
+z*: 1089.16 +0.80 -0.89
+r*: 144.64 Mpc
+100 θ*: 1.04096
 Chi squared: 29.2
-Log evidence: -34.3 (Δ logZ = 1.3 against ΛCDM)
+Log evidence: -34.6 (Δ logZ = 1.3 against ΛCDM)
 Degrees of freedom: 30
 """
