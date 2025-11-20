1f69f1ab3b4df42a0ecc5a2b47e144062ef58a13
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-11-20 02:16:18 +0100

    cc+cmb

--- cosmic_chronometers/cc_cmb.py
+++ cosmic_chronometers/cc_cmb.py
@@ -1,12 +1,14 @@
 from numba import njit
 import numpy as np
 from scipy.linalg import cho_factor, cho_solve
-import cmb.data_chen_compression as cmb
+import cmb.data_planck_act_compression as cmb
 from y2005cc.data import get_data
 from .plot_predictions import plot_cc_predictions
 
 c = cmb.c  # Speed of light in km/s
-Or_h2 = cmb.Omega_r_h2()
+Orh2 = cmb.Omega_r_h2(2.044)
+Onuh2 = cmb.Omnu_h2
+z_nr = cmb.z_nr
 
 legend, z_values, H_values, cov_matrix_cc = get_data()
 cho_cc = cho_factor(cov_matrix_cc)
@@ -14,46 +16,68 @@ logdet = np.linalg.slogdet(cov_matrix_cc)[1]
 
 
 @njit
-def H_z(z, params):
-    H0, Om = params[0], params[1]
-    h = H0 / 100
-    Or = Or_h2 / h**2
-    cubed = (1 + z) ** 3
-    rho_de = 1
-    return H0 * np.sqrt(Or * (1 + z) ** 4 + Om * cubed + (1 - Om - Or) * rho_de)
+def Omnu_z(z):
+    """
+    Computes the appox. evolution of one massive
+    neutrino species energy density with redshift
+    """
+    return (
+        (1 + z) ** 4
+        * (1 + ((1 + z_nr) / (1 + z)) ** 2) ** 0.5
+        * (1 + (1 + z_nr) ** 2) ** -0.5
+    )
 
 
 @njit
-def Ez(z, params):
-    return H_z(z, params) / params[0]
+def Ez(z, H0, Obh2, Och2, w0=-1, wa=0):
+    h = H0 / 100
+    Obc = (Obh2 + Och2) / h**2
+    Onu = Onuh2 / h**2
+    Or = Orh2 / h**2
+    Ode = 1.0 - Obc - Or - Onu
+
+    zp1 = 1 + z
+
+    radiation_term = Or * zp1**4
+    matter_term = Obc * zp1**3
+    neutrino_term = Onu * Omnu_z(z)
+    dark_energy_term = Ode
+
+    return np.sqrt(radiation_term + matter_term + dark_energy_term + neutrino_term)
+
+
+@njit
+def H_z(z, params):
+    H0, Obh2, Och2 = params[1:]
+    return H0 * Ez(z, H0, Obh2, Och2)
 
 
 bounds = np.array(
     [
-        (50, 85),  # H0
-        (0.1, 0.45),  # Om
-        (0.0210, 0.0235),  # Ωb * h^2
         (0.1, 1.5),  # f_cc
+        (50, 85),  # H0
+        (0.0210, 0.0235),  # Ωb * h^2
+        (0.05, 0.30),  # Ωc * h^2
     ],
     dtype=np.float64,
 )
 
 
 def chi_squared(params):
-    H0, Om, Ob_h2, f_cc = params
+    f_cc, H0, Ob_h2, Oc_h2 = params
     delta_cc = H_values - H_z(z_values, params)
     chi2_cc = f_cc**-2 * np.dot(
         delta_cc, cho_solve(cho_cc, delta_cc, check_finite=False)
     )
 
-    delta_cm = cmb.DISTANCE_PRIORS - cmb.cmb_distances(Ez, params, H0, Om, Ob_h2)
+    delta_cm = cmb.DISTANCE_PRIORS - cmb.cmb_distances(Ez, H0, Ob_h2, Oc_h2)
     chi2_cmb = np.dot(delta_cm, np.dot(cmb.inv_cov_mat, delta_cm))
     return chi2_cc + chi2_cmb
 
 
 def log_likelihood(params):
     N = len(z_values)
-    normalization = N * np.log(2 * np.pi) + logdet + 2 * N * np.log(params[-1])
+    normalization = N * np.log(2 * np.pi) + logdet + 2 * N * np.log(params[0])
     return -0.5 * (chi_squared(params) + normalization)
 
 
@@ -78,12 +102,9 @@ def main():
 
     ndim = len(bounds)
     nwalkers = 150
-    burn_in = 200
-    nsteps = 2000 + burn_in
-    initial_pos = np.zeros((nwalkers, ndim))
-
-    for dim, (lower, upper) in enumerate(bounds):
-        initial_pos[:, dim] = np.random.uniform(lower, upper, nwalkers)
+    burn_in = 250
+    nsteps = 2500 + burn_in
+    initial_pos = np.random.uniform(bounds[:, 0], bounds[:, 1], size=(nwalkers, ndim))
 
     with Pool(5) as pool:
         sampler = emcee.EnsembleSampler(
@@ -93,8 +114,7 @@ def main():
             pool=pool,
             moves=[
                 (emcee.moves.KDEMove(), 0.30),
-                (emcee.moves.DEMove(), 0.56),
-                (emcee.moves.DESnookerMove(), 0.14),
+                (emcee.moves.DEMove(), 0.70),
             ],
         )
         sampler.run_mcmc(initial_pos, nsteps, progress=True)
@@ -108,23 +128,23 @@ def main():
         print("Autocorrelation time could not be computed", e)
 
     samples = sampler.get_chain(discard=burn_in, flat=True)
-    print("correlation matrix:")
-    print(np.array2string(np.corrcoef(samples, rowvar=False), precision=5))
 
     [
-        [H0_16, H0_50, H0_84],
-        [Om_16, Om_50, Om_84],
-        [Obh2_16, Obh2_50, Obh2_84],
         [f_16, f_50, f_84],
+        [H0_16, H0_50, H0_84],
+        [Obh2_16, Obh2_50, Obh2_84],
+        [Och2_16, Och2_50, Och2_84],
     ] = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
 
-    best_fit = np.array([H0_50, Om_50, Obh2_50, f_50], dtype=np.float64)
+    best_fit = np.percentile(samples, 50, axis=0)
+
+    Om_samples = (samples[:, 2] + samples[:, 3] + Onuh2) / (samples[:, 1] / 100) ** 2
+    Om_16, Om_50, Om_84 = np.percentile(Om_samples, [15.9, 50, 84.1], axis=0)
 
     print(f"H0: {H0_50:.2f} +{(H0_84 - H0_50):.2f} -{(H0_50 - H0_16):.2f}")
     print(f"Ωm: {Om_50:.4f} +{(Om_84 - Om_50):.4f} -{(Om_50 - Om_16):.4f}")
-    print(
-        f"Ωb x h^2: {Obh2_50:.5f} +{(Obh2_84 - Obh2_50):.5f} -{(Obh2_50 - Obh2_16):.5f}"
-    )
+    print(f"ωb: {Obh2_50:.5f} +{(Obh2_84 - Obh2_50):.5f} -{(Obh2_50 - Obh2_16):.5f}")
+    print(f"ωc: {Och2_50:.5f} +{(Och2_84 - Och2_50):.5f} -{(Och2_50 - Och2_16):.5f}")
     print(f"f: {f_50:.2f} +{(f_84 - f_50):.2f} -{(f_50 - f_16):.2f}")
     print(f"Chi squared: {chi_squared(best_fit):.2f}")
     print(f"Log likelihood: {log_likelihood(best_fit):.2f}")
@@ -137,7 +157,7 @@ def main():
         H_err=np.sqrt(np.diag(cov_matrix_cc)) * f_50,
         label=f"{legend} $H_0$: {H0_50:.2f} ± {(H0_84 - H0_50):.2f} km/s/Mpc",
     )
-    labels = ["$H_0$", "$Ω_m$", "$Ω_b h^2$", "f"]
+    labels = ["f", "$H_0$", "$ω_b$", "$ω_c$"]
     corner.corner(
         samples,
         labels=labels,
@@ -176,17 +196,12 @@ https://arxiv.org/pdf/2506.03836
 *******************************
 
 Flat ΛCDM
-H0: 67.39 +0.59 -0.60 km/s/Mpc
-Ωm: 0.3168 +0.0084 -0.0080
-ωb: 0.02236 +0.00014 -0.00014
-f: 0.69 +0.09 -0.08
-Chi squared: 31.31
-Log likelihood: -130.56
+H0: 67.61 +0.49 -0.49
+Ωm: 0.3118 +0.0070 -0.0069
+ωb: 0.02249 +0.00011 -0.00011
+ωc: 0.11936 +0.00119 -0.00119
+f: 0.69 +0.10 -0.08
+Chi squared: 31.30
+Log likelihood: -130.60
 Degs of freedom: 32
-
-correlation matrix:
-[[ 1.      -0.99339  0.724    0.00941]
- [-0.99339  1.      -0.65643 -0.00887]
- [ 0.724   -0.65643  1.       0.00875]
- [ 0.00941 -0.00887  0.00875  1.     ]]
 """
