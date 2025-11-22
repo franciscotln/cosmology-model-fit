d8d962f45ed58c36201e2cd5144eda8f50c7ed22
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-11-22 12:56:28 +0100

    desi union3 Omh2 cmb

--- bao/desi_union3_omh2.py
+++ bao/desi_union3_omh2.py
@@ -14,15 +14,15 @@ cho_bao = cho_factor(bao_cov_matrix, lower=True)[0]
 c = c0 / 1000  # Speed of light in km/s
 
 z_max = max(np.max(z_sn_vals), np.max(bao_data["z"])) + 0.1
-z_grid = np.linspace(0, z_max, num=1200)
+z_grid = np.linspace(0, z_max, num=2000)
 dx = np.diff(z_grid)
 
 
 @njit
 def Ez(z, params):
     Om, w0 = params[2], params[3]
-    one_plus_z = 1 + z
-    cubic = one_plus_z**3
+    zp1 = 1 + z
+    cubic = zp1**3
     rho_de = (4 * cubic / (1 + 3 * cubic)) ** (4 * (1 + w0))
     return np.sqrt(Om * cubic + (1 - Om) * rho_de)
 
@@ -75,6 +75,11 @@ def bao_theory(z, qty, params):
     return results / params[0]
 
 
+def solve_triang(cho_L, delta):
+    y = solve_triangular(cho_L, delta, lower=True, check_finite=False)
+    return y @ y
+
+
 """
 Planck prior on Ωm * h^2
 Fit rs(drag) directly as a free parameter without early universe constraints
@@ -83,13 +88,8 @@ Omh2_planck = 0.1430
 Omh2_planck_sigma = 0.0011
 
 
-def solve_triang(cho_L, delta):
-    y = solve_triangular(cho_L, delta, lower=True, check_finite=False)
-    return np.dot(y, y)
-
-
 def chi_squared(params):
-    Omh2 = params[2] * params[1] ** 2 / 100**2
+    Omh2 = params[2] * (params[1] / 100) ** 2
     chi2_prior = ((Omh2_planck - Omh2) / Omh2_planck_sigma) ** 2
 
     delta_sn = mu_vals - mu_theory(params)
@@ -103,10 +103,10 @@ def chi_squared(params):
 bounds = np.array(
     [
         (120, 160),  # rd
-        (60, 75),  # H0
-        (0.1, 0.6),  # Ωm
+        (50, 80),  # H0
+        (0.2, 0.5),  # Ωm
         (-1.5, 0.0),  # w0
-        (-0.7, 0.7),  # ΔM
+        (-1.0, 1.0),  # ΔM
     ],
     dtype=np.float64,
 )
@@ -142,20 +142,17 @@ def main():
 
     ndim = len(bounds)
     nwalkers = 150
-    burn_in = 200
-    nsteps = 2000 + burn_in
+    burn_in = 300
+    nsteps = 3000 + burn_in
     np.random.seed(42)
     initial_pos = np.random.uniform(bounds[:, 0], bounds[:, 1], size=(nwalkers, ndim))
     moves = [
         (emcee.moves.KDEMove(), 0.30),
-        (emcee.moves.DEMove(), 0.56),
-        (emcee.moves.DESnookerMove(), 0.14),
+        (emcee.moves.DEMove(), 0.70),
     ]
 
     with Pool(5) as pool:
-        sampler = emcee.EnsembleSampler(
-            nwalkers, ndim, log_probability, pool=pool, moves=moves
-        )
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, pool, moves)
         sampler.run_mcmc(initial_pos, nsteps, progress=True)
 
     try:
@@ -221,48 +218,49 @@ if __name__ == "__main__":
 DESI BAO DR2 2025
 *******************************
 
-Flat ΛCDM
-rd: 147.30 +1.28 -1.27 Mpc
-H0: 68.59 +0.97 -0.97 km/s/Mpc
+Flat ΛCDM: w(z) = -1
+rd: 147.30 +1.29 -1.28 Mpc
+H0: 68.59 +0.99 -0.97 km/s/Mpc
 Ωm: 0.304 +0.008 -0.008
 w0: -1
+wa: 0
 Chi squared: 38.8
-Log evidence: -31.2
+Log evidence: -31.7
 Degs of freedom: 32
 
 ===============================
 
-Flat wCDM
-rd: 142.52 +2.40 -2.59 Mpc
+Flat wCDM: w(z) = w0
+rd: 142.52 +2.42 -2.56 Mpc
 H0: 69.32 +1.11 -1.07 km/s/Mpc
 Ωm: 0.298 +0.009 -0.009
-w0: -0.865 +0.051 -0.051 (prior width 1.5: -1.5 to 0.0)
+w0: -0.865 +0.051 -0.052 (prior width 1.5: -1.5 to 0.0)
 wa: 0
-Chi squared: 32.2
-Log evidence: -30.3 (Δ logZ = 0.9 over ΛCDM)
+Chi squared: 32.1
+Log evidence: -30.9 (Δ logZ = 0.8 against ΛCDM)
 Degs of freedom: 31
 
 ===============================
 
-Flat w(z) = -1 + 4 * (1 + w0) / (1 + 3 * (1 + z)**3)
-rd: 144.48 +1.60 -1.60 Mpc
-H0: 67.75 +1.00 -1.00 km/s/Mpc
-Ωm: 0.311 +0.009 -0.009
-w0: -0.774 +0.073 -0.074
-wa: -(9/4) * (1 + w0)
+Flat wzCDM: w(z) = -1 + 4 * (1 + w0) / (1 + 3 * (1 + z)**3)
+rd: 144.47 +1.63 -1.60 Mpc
+H0: 67.75 +1.01 -1.00 km/s/Mpc
+Ωm: 0.312 +0.009 -0.009
+w0: -0.774 +0.074 -0.076 (prior width 1.5: -1.5 to 0.0)
+wa: d w(z)/dz at z=0 = -(9/4) * (1 + w0)
 Chi squared: 30.1
-Log evidence: -28.9 (Δ logZ = 2.3 over ΛCDM)
+Log evidence: -29.5 (Δ logZ = 2.2 against ΛCDM)
 Degs of freedom: 31
 
 ===============================
 
-Flat w0waCDM
-rd: 148.14 +2.38 -3.00 Mpc
-H0: 65.74 +1.82 -1.51 km/s/Mpc
-Ωm: 0.331 +0.015 -0.017
-w0: -0.696 +0.113 -0.108 (prior width 1.5: -1.5 to 0.0)
-wa: -1.011 +0.546 -0.559 (prior width 4.0: -3.0 to 1.0)
+Flat w0waCDM: w(z) = w0 + wa * z / (1 + z)
+rd: 148.14 +2.41 -3.07 Mpc
+H0: 65.75 +1.86 -1.52 km/s/Mpc
+Ωm: 0.331 +0.016 -0.018
+w0: -0.697 +0.114 -0.110 (prior width 1.5: -1.5 to 0.0)
+wa: -1.01 +0.56 -0.56 (prior width 7.0: -4.5 to 2.5)
 Chi squared: 28.8
-Log evidence: -29.8 (Δ logZ = 1.4 over ΛCDM)
+Log evidence: -30.6 (Δ logZ = 1.1 against ΛCDM)
 Degs of freedom: 30
 """
