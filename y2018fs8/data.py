c7f64e8f446c3676ccf317bf68f890b7112467b9
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2025-12-13 01:29:19 +0100

    fs8

--- y2018fs8/data.py
+++ y2018fs8/data.py
@@ -20,3 +20,6 @@ cov_mat = np.loadtxt(pathname + "fs8_cov.dat", dtype=np.float64)
 # arXiv:1803.01337v4
 # arXiv:2110.08498v2
 # arXiv:2007.08999v2
+
+# covariance for the data points z = 0.3, 0.4, 0.5, 0.6 respectively
+# Taken from arXiv:1203.6565v2, section 5.1
