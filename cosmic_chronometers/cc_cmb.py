ca88d404d9f9033872e2974279a484c0ce21dd0e
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-02-14 22:41:58 +0100

    re-run with new datapoints

--- cosmic_chronometers/cc_cmb.py
+++ cosmic_chronometers/cc_cmb.py
@@ -32,7 +32,7 @@ def Ez(z, H0, Obh2, Och2):
 
 @njit
 def H_z(z, params):
-    H0, Obh2, Och2 = params[1:]
+    H0, Obh2, Och2 = params[0:3]
     return H0 * Ez(z, H0, Obh2, Och2)
 
 
@@ -41,26 +41,26 @@ cmb.set_HZ(H_z)
 
 bounds = np.array(
     [
-        (0.30, 2.75),  # f_cc
         (50.0, 85.0),  # H0
         (0.0210, 0.0235),  # Ωb * h^2
         (0.05, 0.30),  # Ωc * h^2
+        (0.30, 2.75),  # f_cc
     ]
 )
 
 
 def chi_squared(params):
     delta_cc = H_values - H_z(z_values, params)
-    chi2_cc = params[0] ** 2 * delta_cc @ inv_cov_cc @ delta_cc
+    chi2_cc = params[3] ** 2 * delta_cc @ inv_cov_cc @ delta_cc
 
-    delta_cm = cmb.DISTANCE_PRIORS - cmb.cmb_distances(params[2], params[3], params)
+    delta_cm = cmb.DISTANCE_PRIORS - cmb.cmb_distances(params[1], params[2], params)
     chi2_cmb = delta_cm @ cmb.inv_cov_mat @ delta_cm
     return chi2_cc + chi2_cmb
 
 
 def log_likelihood(params):
     N = len(z_values)
-    normalization = N * np.log(2 * np.pi) + logdet - 2 * N * np.log(params[0])
+    normalization = N * np.log(2 * np.pi) + logdet - 2 * N * np.log(params[3])
     return -0.5 * (chi_squared(params) + normalization)
 
 
@@ -115,39 +115,36 @@ def main():
     log_evd = log_evidence(samples, log_probs, log_probability, bounds)
 
     [
-        [f_16, f_50, f_84],
         [H0_16, H0_50, H0_84],
         [Obh2_16, Obh2_50, Obh2_84],
         [Och2_16, Och2_50, Och2_84],
+        [f_16, f_50, f_84],
     ] = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
 
     best_fit = np.percentile(samples, 50, axis=0)
     degs_of_freedom = len(cmb.DISTANCE_PRIORS) + len(z_values) - len(best_fit)
 
-    Om_samples = (samples[:, 2] + samples[:, 3] + Onuh2) / (samples[:, 1] / 100) ** 2
+    Om_samples = (samples[:, 1] + samples[:, 2] + Onuh2) / (samples[:, 0] / 100) ** 2
     Om_16, Om_50, Om_84 = np.percentile(Om_samples, [15.9, 50, 84.1], axis=0)
 
-    print(f"H0: {H0_50:.2f} +{(H0_84 - H0_50):.2f} -{(H0_50 - H0_16):.2f}")
+    print(f"H0: {H0_50:.2f} +{(H0_84 - H0_50):.2f} -{(H0_50 - H0_16):.2f} km/s/Mpc")
     print(f"Ωm: {Om_50:.4f} +{(Om_84 - Om_50):.4f} -{(Om_50 - Om_16):.4f}")
     print(f"ωb: {Obh2_50:.5f} +{(Obh2_84 - Obh2_50):.5f} -{(Obh2_50 - Obh2_16):.5f}")
     print(f"ωc: {Och2_50:.5f} +{(Och2_84 - Och2_50):.5f} -{(Och2_50 - Och2_16):.5f}")
-    print(f"f: {f_50:.2f} +{(f_84 - f_50):.2f} -{(f_50 - f_16):.2f}")
+    print(f"fcc: {f_50:.2f} +{(f_84 - f_50):.2f} -{(f_50 - f_16):.2f}")
     print(f"Chi squared: {chi_squared(best_fit):.2f}")
     print(f"Log likelihood: {log_likelihood(best_fit):.2f}")
     print(f"Log evidence: {log_evd:.2f}")
     print(f"Degs of freedom: {degs_of_freedom}")
 
+    labels = ["$H_0$", "$ω_b$", "$ω_c$", "$f_{CC}$"]
+    plot_corner_and_chains(labels=labels, flat_samples=samples, samples=chains_samples)
     plot_cc_predictions(
         H_z=lambda z: H_z(z, best_fit),
         z=z_values,
         H=H_values,
         H_err=np.sqrt(np.diag(cov_matrix_cc)) / f_50,
         label=f"{legend} $H_0$: {H0_50:.2f} ± {(H0_84 - H0_50):.2f} km/s/Mpc",
-    )
-    plot_corner_and_chains(
-        labels=["f", "$H_0$", "$ω_b$", "$ω_c$"],
-        flat_samples=samples,
-        samples=chains_samples,
     )
 
 
@@ -158,34 +155,37 @@ if __name__ == "__main__":
 *******************************
 Results for data from
 https://arxiv.org/pdf/2307.09501
-and one data point from
+and 4 data points from
 https://arxiv.org/pdf/2506.03836
+https://arxiv.org/pdf/2511.02730v1
 *******************************
+"""
 
+"""
 Flat ΛCDM
 
 -------------------------------
 
 f: 1.50 +0.19 -0.18
-H0: 67.60 +0.50 -0.49
-Ωm: 0.3118 +0.0071 -0.0070
+H0: 67.59 +0.49 -0.49
+Ωm: 0.3120 +0.0071 -0.0069
 ωb: 0.02249 +0.00011 -0.00011
-ωc: 0.11936 +0.00121 -0.00119
-f: 1.50 +0.19 -0.18
-Chi squared: 33.33
-Log likelihood: -130.57
-Log evidence: -147.01 (Δ logZ = 3.32 compared to fixed f)
-Degs of freedom: 32
+ωc: 0.11939 +0.00121 -0.00120
+f: 1.51 +0.18 -0.17
+Chi squared: 36.36
+Log likelihood: -141.72
+Log evidence: -158.19 (Δ logZ = 3.79 compared to fixed f)
+Degs of freedom: 35
 
 -------------------------------
 
 f: 1.0 (fixed - assuming no overestimaded errors in CCH sample)
-H0: 67.62 +0.50 -0.50
+H0: 67.61 +0.50 -0.50
 Ωm: 0.3117 +0.0072 -0.0070
 ωb: 0.02250 +0.00011 -0.00011
-ωc: 0.11935 +0.00122 -0.00121
-Chi squared: 14.86
-Log likelihood: -134.67
-Log evidence: -150.33
-Degs of freedom: 33
+ωc: 0.11934 +0.00122 -0.00120
+Chi squared: 16.00
+Log likelihood: -146.32
+Log evidence: -161.98
+Degs of freedom: 36
 """
