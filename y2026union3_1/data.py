13d3f1f586767a7b2c8a498488e32b93cc2e9f16
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-02-15 09:23:57 +0100

    new data union3.1

---
+++ y2026union3_1/data.py
@@ -0,0 +1,23 @@
+# arXiv:2311.12098
+import pandas as pd
+import numpy as np
+
+data_frame = pd.read_csv("y2026union3_1/raw-data/bins_union_3_1.csv")
+cov_data = np.genfromtxt("y2026union3_1/raw-data/covariance.txt", dtype=np.float64)
+
+n = data_frame["zcmb"].size
+covariance_matrix = cov_data.reshape((n, n))
+z_cmb = data_frame["zcmb"].to_numpy()
+z_hel = data_frame["zhel"].to_numpy()
+mu_values = data_frame["mu"].to_numpy()
+sort_indices = np.argsort(z_cmb)
+
+
+def get_data():
+    return (
+        "Union3.1 - 22 Bins",
+        z_cmb[sort_indices],
+        z_hel[sort_indices],
+        mu_values[sort_indices],
+        covariance_matrix[sort_indices, :][:, sort_indices],
+    )
