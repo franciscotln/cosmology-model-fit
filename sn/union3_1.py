4a3675f0dfcac836a9b3c695819cbe0ef368907b
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-02-15 09:57:00 +0100

    union3.1 alone

--- sn/union3_1.py
+++ sn/union3_1.py
@@ -2,9 +2,9 @@ from numba import njit
 import numpy as np
 from scipy.constants import c as c0
 from interpolator import interp_hermite
-from y2023union3.data import get_data
+from y2026union3_1.data import get_data
 
-legend, z_values, mu_vals, cov_matrix = get_data()
+legend, z_cmb, z_hel, mu_vals, cov_matrix = get_data()
 
 inv_cov = np.linalg.inv(cov_matrix)
 
@@ -14,20 +14,17 @@ H0 = 70.0  # Hubble constant (km/s/Mpc)
 # params indices
 OFFSET = 0
 OM = 1
-W0 = 2
 
-bounds = np.array([(-0.6, 0.6), (0.0, 1.0), (-1.0, 0.0)])  # ΔM, Ωm, w0
+bounds = np.array([(-0.6, 0.6), (0.0, 1.0)])  # ΔM, Ωm
 
-z_grid = np.linspace(0, np.max(z_values) + 0.1, num=2000)
+z_grid = np.linspace(0, np.max(z_cmb) + 0.1, num=3000)
 dx = np.diff(z_grid)
 
 
 @njit
 def Ez(z, params):
-    Om, w0 = params[OM], params[W0]
-    cubed = (1.0 + z) ** 3
-    rho_de = (2 * cubed / (1.0 + w0 + (1.0 - w0) * cubed)) ** 2
-    return np.sqrt(Om * cubed + (1 - Om) * rho_de)
+    Om = params[OM]
+    return np.sqrt(Om * (1.0 + z) ** 3 + (1.0 - Om))
 
 
 @njit
@@ -41,7 +38,7 @@ def DM_z(z, params):
 
 @njit
 def mu_theory(params):
-    dL = (1.0 + z_values) * DM_z(z_values, params)
+    dL = (1.0 + z_hel) * DM_z(z_cmb, params)
     return params[OFFSET] + 25.0 + 5 * np.log10(dL)
 
 
@@ -83,9 +80,9 @@ def main():
     from sn.plotting import plot_predictions, print_color, plot_residuals
 
     n_dim = len(bounds)
-    n_walkers = 150
+    n_walkers = 200
     burn_in = 500
-    n_steps = burn_in + 2500
+    n_steps = burn_in + 2000
     np.random.seed(42)
     initial_pos = np.random.uniform(bounds[:, 0], bounds[:, 1], size=(n_walkers, n_dim))
     moves = [
@@ -93,7 +90,7 @@ def main():
         (emcee.moves.DEMove(), 0.75),
     ]
 
-    with Pool(5) as pool:
+    with Pool(6) as pool:
         sampler = emcee.EnsembleSampler(n_walkers, n_dim, log_probability, pool, moves)
         sampler.run_mcmc(
             initial_pos, n_steps, progress=True, progress_kwargs={"colour": "#ff5a00"}
@@ -118,11 +115,10 @@ def main():
     one_sigma_ci = [15.9, 50, 84.1]
     dM_16, dM_50, dM_84 = np.percentile(samples[:, OFFSET], one_sigma_ci)
     Om_16, Om_50, Om_84 = np.percentile(samples[:, OM], one_sigma_ci)
-    w0_16, w0_50, w0_84 = np.percentile(samples[:, W0], one_sigma_ci)
 
-    best_fit_params = np.array([dM_50, Om_50, w0_50], dtype=np.float64)
+    best_fit = np.percentile(samples, 50, axis=0)
 
-    predicted_distances = mu_theory(best_fit_params)
+    predicted_distances = mu_theory(best_fit)
     residuals = mu_vals - predicted_distances
 
     # Calculate R-squared
@@ -134,35 +130,33 @@ def main():
 
     dM_label = f"{dM_50:.3f} +{dM_84-dM_50:.3f}/-{dM_50-dM_16:.3f}"
     Om_label = f"{Om_50:.3f} +{Om_84-Om_50:.3f}/-{Om_50-Om_16:.3f}"
-    w0_label = f"{w0_50:.3f} +{w0_84-w0_50:.3f}/-{w0_50-w0_16:.3f}"
 
     print_color("Dataset", legend)
-    print_color("z range", f"{z_values[0]:.3f} - {z_values[-1]:.3f}")
-    print_color("Sample size", len(z_values))
+    print_color("z range", f"{z_cmb[0]:.3f} - {z_cmb[-1]:.3f}")
+    print_color("Sample size", len(z_cmb))
     print_color("ΔM", dM_label)
     print_color("Ωm", Om_label)
-    print_color("w0", w0_label)
     print_color("R-squared (%)", f"{100 * r2:.2f}")
     print_color("RMSD (mag)", f"{rmsd:.3f}")
     print_color("Skewness of residuals", f"{skew(residuals):.3f}")
-    print_color("Chi squared", f"{chi_squared(best_fit_params):.1f}")
+    print_color("Chi squared", f"{chi_squared(best_fit):.1f}")
     print_color("Log evidence", f"{log_evd:.1f}")
-    print_color("Degs of freedom", len(z_values) - len(best_fit_params))
+    print_color("Degs of freedom", len(z_cmb) - len(best_fit))
 
     sigma_mu = np.sqrt(np.diag(cov_matrix))
 
-    labels = ["$ΔM$", "$Ω_m$", "$w_0$"]
+    labels = ["$ΔM$", "$Ω_m$"]
     plot_corner_and_chains(labels=labels, flat_samples=samples, samples=chain_samples)
     plot_predictions(
         legend=legend,
-        x=z_values,
+        x=z_cmb,
         y=mu_vals,
         y_err=sigma_mu,
         y_model=predicted_distances,
         label=f"$Ω_m$={Om_50:.4f}",
         x_scale="log",
     )
-    plot_residuals(z_values=z_values, residuals=residuals, y_err=sigma_mu, bins=40)
+    plot_residuals(z_values=z_cmb, residuals=residuals, y_err=sigma_mu, bins=40)
 
 
 if __name__ == "__main__":
@@ -177,54 +171,60 @@ Sample size: 22
 
 Flat ΛCDM: w(z) = -1
 
-Ωm: 0.357 +0.027/-0.026
-w0: -1
-wa: 0
-R-squared (%): 99.95
-RMSD (mag): 0.050
-Skewness of residuals: 0.590
-Chi squared: 24.0
-Log evidence: -16.4
+ΔM: 0.027 +0.020/-0.020
+Ωm: 0.335 +0.025/-0.024
+R-squared (%): 99.96
+RMSD (mag): 0.044
+Skewness of residuals: 0.088
+Chi squared: 28.8
+Log evidence: -22.0
 Degs of freedom: 20
+
+===============================
+
+Flat ΛCDM: w(z) = -1, varying absolute magnitude
+M(z) = a + np.tanh(1 - z_cmb ** (0.1 * b))
+
+ΔM: -0.078 +0.062/-0.063
+Ωm: 0.247 +0.052/-0.048
+w0: 0.408 +0.267/-0.238
+R-squared (%): 99.96
+RMSD (mag): 0.046
+Skewness of residuals: -1.056
+Chi squared: 25.6
+Log evidence: -22.1
+Degs of freedom: 19
 
 ===============================
 
 Flat wCDM: w(z) = w0
 
-Ωm: 0.252 +0.086/-0.108
-w0: -0.748 +0.153/-0.183
-wa: 0
-R-squared (%): 99.94
-RMSD (mag): 0.055
-Skewness of residuals: -1.266
-Chi squared: 22.1
-Log evidence: -16.5
+ΔM: 0.034 +0.020/-0.020
+Ωm: 0.246 +0.081/-0.100
+w0: -0.786 +0.152/-0.179
+R-squared (%): 99.95
+RMSD (mag): 0.050
+Skewness of residuals: -1.534
+Chi squared: 27.2
+Log evidence: -22.5
 Degs of freedom: 19
 
 ===============================
 
 Flat wzCDM: w(z) = -1 + 2 * (1 + w0) / (1 + w0 + (1 - w0) * (1 + z)**3)
 
-Ωm: 0.297 +0.044/-0.048
-w0: -0.713 +0.142/-0.152 (prior width 1.0: -1.0 to 0.0)
-wa: -1.5 * (1 - w0^2)
-R-squared (%): 99.94
-RMSD (mag): 0.054
-Skewness of residuals: -1.156
-Chi squared: 21.5
-Log evidence: -16.0
+ΔM: 0.042 +0.021/-0.021
+Ωm: 0.280 +0.040/-0.045
+w0: -0.745 +0.131/-0.137 (prior width 1.0: -1.0 to 0.0)
+R-squared (%): 99.95
+RMSD (mag): 0.049
+Skewness of residuals: -1.495
+Chi squared: 26.6
+Log evidence: -21.8
 Degs of freedom: 19
 
 ===============================
 
 Flat w0waCDM: w(z) = w0 + wa * z / (1 + z)
-
-Ωm: 0.4406 +0.0555/-0.0923
-w0: -0.5574 +0.2799/-0.2343
-wa: -4.1187 +2.8976/-3.2694
-R-squared (%): 99.96
-RMSD (mag): 0.043
-Skewness of residuals: 0.669
-Chi squared: 20.6
-Degs of freedom: 18
+TODO
 """
