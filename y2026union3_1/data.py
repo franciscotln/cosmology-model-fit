d90994298c25330f18012ae9f1604e62ef9f7cae
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-02-18 12:08:10 +0100

    union3.1 data

--- y2026union3_1/data.py
+++ y2026union3_1/data.py
@@ -10,14 +10,13 @@ covariance_matrix = cov_data.reshape((n, n))
 z_cmb = data_frame["zcmb"].to_numpy()
 z_hel = data_frame["zhel"].to_numpy()
 mu_values = data_frame["mb"].to_numpy()
-sort_indices = np.argsort(z_cmb)
 
 
 def get_data():
     return (
         "Union3.1 - 22 Bins",
-        z_cmb[sort_indices],
-        z_hel[sort_indices],
-        mu_values[sort_indices],
-        covariance_matrix[sort_indices, :][:, sort_indices],
+        z_cmb,
+        z_hel,
+        mu_values,
+        covariance_matrix,
     )
