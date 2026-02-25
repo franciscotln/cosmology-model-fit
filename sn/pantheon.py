ff45d346a7cd86f86cc55ce89ee74cdf48a0b19c
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-02-25 13:34:00 +0100

    pantheon

--- sn/pantheon.py
+++ sn/pantheon.py
@@ -9,7 +9,6 @@ from y2022pantheonSHOES.data import get_data
 legend, z_cmb, z_hel, apparent_mag_vals, cov_matrix = get_data()
 
 c = c0 / 1000  # Speed of light (km/s)
-H0 = 70.0  # Hubble constant (km/s/Mpc)
 
 cho = cho_factor(cov_matrix, lower=True)[0]
 
@@ -27,13 +26,13 @@ def Ode_z(w0):
 
 @njit
 def Ez(params):
-    Om = params[1]
+    Om = params[2]
     return np.sqrt(Om * cubed + (1.0 - Om))
 
 
 @njit
 def DM_z(params):
-    dh_grid = (c / H0) / Ez(params)
+    dh_grid = (c / params[1]) / Ez(params)
     dh = (dh_grid[:-1] + dh_grid[1:]) / 2
     cum_dm = np.zeros(z_grid.size, dtype=np.float64)
     cum_dm[1:] = np.cumsum(dh * dz)
@@ -41,19 +40,19 @@ def DM_z(params):
 
 
 @njit
-def outflow_correction(params):
+def outflow_correction(params, z):
     # up to second order in z
-    Om, v_100 = params[1], params[2]
+    Om, v_100 = params[2], params[3]
     q0 = 1.5 * Om - 1.0
-    q_term = (1.0 - q0) * z_cmb
+    q_term = (1.0 - q0) * z
     q_corr = (1.0 + q_term) / (1.0 + 0.5 * q_term)
-    v_ratio = 100 * v_100 / (c * z_cmb)
+    v_ratio = 100 * v_100 / (c * z)
     return v_ratio * (5 / np.log(10)) * q_corr
 
 
 @njit
 def apparent_mag(params):
-    Mz = params[0] + outflow_correction(params)
+    Mz = params[0] + outflow_correction(params, z_cmb)
     return Mz + 25.0 + 5 * np.log10((1.0 + z_hel) * DM_z(params))
 
 
@@ -74,6 +73,7 @@ def log_likelihood(params):
 bounds = np.array(
     [
         (-20.0, -19.0),  # M
+        (50.0, 90.0),  # H0
         (0.0, 0.7),  # Ωm
         (-1.70, 3.20),  # v_flow [x 100 km/s]
     ]
@@ -86,7 +86,8 @@ normalization = -np.sum(np.log(bounds[:, 1] - bounds[:, 0]))
 def log_prior(params):
     if not np.all((bounds[:, 0] < params) & (params < bounds[:, 1])):
         return -np.inf
-    return normalization
+    # H0 prior from TRGB Freedman et al
+    return normalization - 0.5 * (params[1] - 70.39) ** 2 / 1.80**2
 
 
 def log_probability(params):
@@ -141,6 +142,7 @@ def main():
 
     [
         (M0_16, M0_50, M0_84),
+        (H0_16, H0_50, H0_84),
         (Om_16, Om_50, Om_84),
         (vf_16, vf_50, vf_84),
     ] = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
@@ -153,22 +155,21 @@ def main():
     skewness = stats.skew(residuals)
     kurtosis = stats.kurtosis(residuals)
 
-    # Calculate R-squared
-    average_distance_modulus = np.mean(apparent_mag_vals)
+    mean_app_mag = np.mean(apparent_mag_vals)
     ss_res = np.sum(residuals**2)
-    ss_tot = np.sum((apparent_mag_vals - average_distance_modulus) ** 2)
+    ss_tot = np.sum((apparent_mag_vals - mean_app_mag) ** 2)
     r_squared = 1 - (ss_res / ss_tot)
-
-    # Calculate root mean square deviation
     rmsd = np.sqrt(np.mean(residuals**2))
 
     M_label = f"{M0_50:.3f} +{M0_84-M0_50:.3f}/-{M0_50-M0_16:.3f}"
+    H0_label = f"{H0_50:.2f} +{H0_84-H0_50:.2f}/-{H0_50-H0_16:.2f} km/s/Mpc"
     omega_label = f"{Om_50:.3f} +{Om_84-Om_50:.3f}/-{Om_50-Om_16:.3f}"
     vf_label = f"{vf_50:.3f} +{vf_84-vf_50:.3f}/-{vf_50-vf_16:.3f}"
 
     print_color("Dataset", legend)
     print_color("z range", f"{z_cmb[0]:.4f} - {z_cmb[-1]:.4f}")
     print_color("M", M_label)
+    print_color("H0", H0_label)
     print_color("Ωm", omega_label)
     print_color("v_flow", vf_label)
     print_color("R-squared (%)", f"{100 * r_squared:.2f}")
@@ -179,15 +180,15 @@ def main():
     print_color("Chi squared", f"{chi_squared(best_fit):.2f}")
     print_color("Log Evidence", f"{log_evd:.1f}")
 
-    labels = ["$M_0$", "$Ω_m$", "$v_{flow}$"]
+    labels = ["$M_0$", "$H_0$", "$Ω_m$", "$v_{flow}$"]
     plot_corner_and_chains(labels=labels, flat_samples=samples, samples=chains_samples)
     plot_predictions(
         legend=legend,
         x=z_cmb,
-        y=apparent_mag_vals - M0_50,
+        y=apparent_mag_vals - (M0_50 + outflow_correction(best_fit, z_cmb)),
         y_err=np.sqrt(np.diag(cov_matrix)),
-        y_model=predicted_apparent_mag - M0_50,
-        label=f"$Ω_m$={Om_50:.3f}, $M$={M0_50:.3f}",
+        y_model=predicted_apparent_mag - (M0_50 + outflow_correction(best_fit, z_cmb)),
+        label=f"$Ω_m$={Om_50:.3f}",
         x_scale="log",
     )
     plot_residuals(
@@ -212,65 +213,70 @@ Sample size: 1590
 
 """
 ΛCDM
-M: -19.351 +0.007/-0.007
-Ωm: 0.332 +0.018/-0.018 (agreement with BAO within 1.94 sigma)
+M: -19.339 +0.055/-0.057 mag
+H0: 70.38 +- 1.80 km/s/Mpc
+Ωm: 0.332 +0.018/-0.018
 R-squared (%): 99.74
 RMSD (mag): 0.153
 Skewness of residuals: 0.090
 kurtosis of residuals: 1.582
-Degs of freedom: 1588
+Degs of freedom: 1587
 Chi squared: 1402.92
-Log Evidence: -708.9
+Log Evidence: -711.0
 
 =============================
 
 Flat ΛCDM w(z) = -1
 Void outflow correction to absolute mag of SNe M(z) = M_inf + v_flow_corr
-v_flow_corr = 100 * v_flow * (5 / ln(10)) / (c * z_cmb) * q_term with v_flow in units 100 km/s
+v_flow_corr = 100 * v_flow * q_corr * (5 / ln(10)) / (c * z_cmb) with v_flow in units 100 km/s
 
-M_inf: -19.367 +0.012/-0.012 mag
-v_flow: 76.2 +- 48.9 km/s (prior ~ U(-1.7, 3.2) x 100 km/s)
+M_inf: -19.354 +0.056/-0.058 mag
+v_flow: 76 +- 49 km/s (prior ~ U(-1.7, 3.2) x 100 km/s)
+H0: 70.40 +1.81/-1.80 km/s/Mpc
 Ωm: 0.315 +0.021/-0.020
 R-squared (%): 99.74
 RMSD (mag): 0.153
 Skewness of residuals: 0.055
 kurtosis of residuals: 1.580
-Degs of freedom: 1587
-Chi squared: 1400.44
-Log Evidence: -709.0
+Degs of freedom: 1586
+Chi squared: 1400.45 (1.57 sigma significance)
+Log Evidence: -711.2
 """
 
 """
 wCDM
-M: -19.347 +0.009/-0.009
-Ωm: 0.292 +0.064/-0.078
-w0: -0.901 +0.142/-0.159 (prior width 1: -1.5 to -0.5)
+M: -19.335 +0.055/-0.057
+H0: 70.39 +1.80/-1.79 km/s/Mpc
+Ωm: 0.292 +0.063/-0.076
+w0: -0.901 +0.140/-0.159 (prior width 1: -1.5 to -0.5)
 R-squared (%): 99.74
 RMSD (mag): 0.154
 Skewness of residuals: 0.079
-kurtosis of residuals: 1.589
-Degs of freedom: 1587
+kurtosis of residuals: 1.590
+Degs of freedom: 1586
 Chi squared: 1402.47
-Log Evidence: -709.5
+Log Evidence: -711.7
 """
 
 """
 Flat w(z) = -1 + 2 * (1 + w0) / (1 + w0 + (1 - w0) * (1 + z)**3)
-M: -19.344 +0.008/-0.008
+M: -19.332 +0.055/-0.057 mag
+H0: 70.40 +1.79/-1.81 km/s/Mpc
 Ωm: 0.299 +0.028/-0.034
-w0: -0.873 +0.103/-0.084 (prior width 2/3: -1.0 to -1/3)
+w0: -0.873 +0.102/-0.083 (prior width 2/3: -1.0 to -1/3) truncated posterior
 wa: d w(z)/dz at z=0 = -1.5 * (1 - w0^2)
 R-squared (%): 99.74
 RMSD (mag): 0.154
 Skewness of residuals: 0.074
 kurtosis of residuals: 1.592
-Degs of freedom: 1587
-Chi squared: 1402.68
-Log Evidence: -709.3
+Degs of freedom: 1586
+Chi squared: 1402.70
+Log Evidence: -711.5 (not accurate due to truncation of w0 posterior)
 """
 
 """
 Flat w0waCDM
+TODO: re-run after adding H0 prior
 M0: 19.348 +0.010/-0.010 mag
 Ωm: 0.337 +0.082/-0.148
 w0: -0.919 +0.146/-0.162 (0.53 sigma)
