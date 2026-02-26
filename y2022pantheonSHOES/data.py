750a732aefc7a19250473e375945761163505c58
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-02-26 19:25:56 +0100

    pantheon data

--- y2022pantheonSHOES/data.py
+++ y2022pantheonSHOES/data.py
@@ -4,20 +4,25 @@ import os
 import pandas as pd
 import numpy as np
 
-path_to_data = os.path.dirname(os.path.abspath(__file__)) + '/raw-data/'
-data_frame = pd.read_csv(path_to_data + 'distances.txt', sep = ' ')
-convariances_file = pd.read_csv(path_to_data + 'covariance_stat_sys.txt', sep = ' ')
-selected_columns = data_frame[['zHD', 'zHEL', 'm_b_corr']]
+path_to_data = os.path.dirname(os.path.abspath(__file__)) + "/raw-data/"
+data_frame = pd.read_csv(path_to_data + "distances.txt", sep=" ")
+convariances_file = pd.read_csv(path_to_data + "covariance_stat_sys.txt", sep=" ")
+selected_columns = data_frame[["zHD", "zHEL", "m_b_corr", "RA", "DEC"]]
 
-legend = 'Pantheon+ (2022)'
-z_values = selected_columns['zHD'].to_numpy(dtype=np.float64)
-z_hel_values = selected_columns['zHEL'].to_numpy(dtype=np.float64)
-apparent_magnitudes = selected_columns['m_b_corr'].to_numpy(dtype=np.float64)
+legend = "Pantheon+ (2022)"
+z_values = selected_columns["zHD"].to_numpy(dtype=np.float64)
+z_hel_values = selected_columns["zHEL"].to_numpy(dtype=np.float64)
+apparent_magnitudes = selected_columns["m_b_corr"].to_numpy(dtype=np.float64)
+RA = selected_columns["RA"].to_numpy(dtype=np.float64)
+DEC = selected_columns["DEC"].to_numpy(dtype=np.float64)
 
 n = z_values.size
-covariance_matrix = convariances_file['cov_mu_shoes'].to_numpy(dtype=np.float64).reshape((n, n))
+covariance_matrix = (
+    convariances_file["cov_mu_shoes"].to_numpy(dtype=np.float64).reshape((n, n))
+)
 
 pantheon_plus_range = np.where(z_values > 0.01)[0]
+
 
 def get_data():
     return (
@@ -25,5 +30,17 @@ def get_data():
         z_values[pantheon_plus_range],
         z_hel_values[pantheon_plus_range],
         apparent_magnitudes[pantheon_plus_range],
+        covariance_matrix[np.ix_(pantheon_plus_range, pantheon_plus_range)],
+    )
+
+
+def get_data_with_position():
+    return (
+        legend,
+        z_values[pantheon_plus_range],
+        z_hel_values[pantheon_plus_range],
+        apparent_magnitudes[pantheon_plus_range],
+        RA[pantheon_plus_range],
+        DEC[pantheon_plus_range],
         covariance_matrix[np.ix_(pantheon_plus_range, pantheon_plus_range)],
     )
--- y2022pantheonSHOES/data_shoes.py
+++ y2022pantheonSHOES/data_shoes.py
@@ -6,13 +6,17 @@ import numpy as np
 path_to_data = os.path.dirname(os.path.abspath(__file__)) + "/raw-data/"
 data_frame = pd.read_csv(path_to_data + "distances.txt", sep=" ")
 cov_file = pd.read_csv(path_to_data + "covariance_stat_sys.txt", sep=" ")
-selected_columns = data_frame[["zHD", "zHEL", "m_b_corr", "CEPH_DIST", "IS_CALIBRATOR"]]
+selected_columns = data_frame[
+    ["zHD", "zHEL", "m_b_corr", "CEPH_DIST", "IS_CALIBRATOR", "RA", "DEC"]
+]
 
 legend = "Pantheon+ and SH0ES"
 z_values = selected_columns["zHD"].to_numpy(dtype=np.float64)
 z_hel_values = selected_columns["zHEL"].to_numpy(dtype=np.float64)
 apparent_mag = selected_columns["m_b_corr"].to_numpy(dtype=np.float64)
 cepheid_distances = selected_columns["CEPH_DIST"].to_numpy(dtype=np.float64)
+RA = selected_columns["RA"].to_numpy(dtype=np.float64)
+DEC = selected_columns["DEC"].to_numpy(dtype=np.float64)
 
 n = z_values.size
 covariance_matrix = cov_file["cov_mu_shoes"].to_numpy(dtype=np.float64).reshape((n, n))
@@ -31,5 +35,21 @@ def get_data(z_cut_ceph=0.0):
         z_hel_values[pantheon_SH0ES_range],
         apparent_mag[pantheon_SH0ES_range],
         cepheid_distances[pantheon_SH0ES_range],
+        covariance_matrix[np.ix_(pantheon_SH0ES_range, pantheon_SH0ES_range)],
+    )
+
+
+def get_data_with_positions(z_cut_ceph=0.0):
+    ceph_mask = (z_values >= z_cut_ceph) & (selected_columns["IS_CALIBRATOR"] == 1)
+    pantheon_SH0ES_range = np.where((ceph_mask) | (z_values > 0.01))[0]
+
+    return (
+        legend,
+        z_values[pantheon_SH0ES_range],
+        z_hel_values[pantheon_SH0ES_range],
+        apparent_mag[pantheon_SH0ES_range],
+        cepheid_distances[pantheon_SH0ES_range],
+        RA[pantheon_SH0ES_range],
+        DEC[pantheon_SH0ES_range],
         covariance_matrix[np.ix_(pantheon_SH0ES_range, pantheon_SH0ES_range)],
     )
