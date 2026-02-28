43c516745fbd273c88e151e8216312fbd6991fb3
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-02-28 09:41:32 +0100

    P+SH0ES

--- sn/pantheon_and_sh0es.py
+++ sn/pantheon_and_sh0es.py
@@ -6,25 +6,9 @@ from scipy.linalg import cho_factor, solve_triangular
 from interpolator import interp_hermite
 from y2022pantheonSHOES.data_shoes import get_data
 
-legend, z_cmb, z_hel, apparent_mags, ceph_dists, cov_matrix = get_data()
+legend, z_cmb, z_hel, mB_vals, ceph_dists, cov_matrix = get_data()
 
 ceph_mask = ceph_dists != -9
-z_outflow_cut = 0.0063  # Outflow effects start from here on and decays as ~1/z
-flow_cut_mask = z_cmb < z_outflow_cut
-local_ceph = ceph_mask & flow_cut_mask
-z_cut_arr = np.full_like(z_cmb, z_outflow_cut)
-
-"""
-z_cut  chi2    H0 [km/s/Mpc]
-0.0050 1450.11 71.7
-0.0055 1449.57 71.5
-0.0061 1449.05 71.3
-0.0063 1448.94 71.3
-0.0065 1449.00 71.4
-0.0070 1449.22 71.6
-0.0075 1449.33 71.7
-0.0080 1449.33 71.8
-"""
 
 cho = cho_factor(cov_matrix, lower=True)[0]
 
@@ -45,33 +29,38 @@ def Ode_z(params):
 
 
 @njit
-def Ez(params):
-    O_m = params[2]
-    return np.sqrt(O_m * zp1**3 + (1.0 - O_m))
+def Hz(params):
+    H0, O_m = params[1], params[2]
+    return H0 * np.sqrt(O_m * zp1**3 + (1.0 - O_m))
 
 
 @njit
-def DM_z(theta):
-    dh_grid = (c / theta[1]) / Ez(theta)
+def DM_z(z, theta):
+    dh_grid = c / Hz(theta)
     dh = (dh_grid[:-1] + dh_grid[1:]) / 2
     cum_dm = np.zeros(z_grid.size, dtype=np.float64)
     cum_dm[1:] = np.cumsum(dh * dz)
-    return interp_hermite(z_cmb, z_grid, cum_dm, dh_grid)
+    return interp_hermite(z, z_grid, cum_dm, dh_grid)
+
+
+correction_mask = (z_cmb <= 0.1) & ~ceph_mask
 
 
 @njit
-def model_mu(params):
-    return 25.0 + 5.0 * np.log10(zp1_hel * DM_z(params))
+def mu_corr(params):
+    z_pec = 100 * params[3] / c
+    z_cosmo = -1.0 + (1.0 + z_cmb) / (1.0 + z_pec)
+
+    return np.where(
+        correction_mask,
+        5.0 * np.log10(DM_z(z_cosmo, params) / DM_z(z_cmb, params)),
+        0.0,
+    )
 
 
 @njit
-def v_flow(v_100, z):
-    return 100 * v_100 * (5 / np.log(10)) / (c * z)
-
-
-@njit
-def outflow_correction(theta):
-    return np.where(local_ceph, v_flow(theta[3], z_cut_arr), v_flow(theta[3], z_cmb))
+def mu_theory(params):
+    return 25.0 + 5.0 * np.log10(zp1_hel * DM_z(z_cmb, params))
 
 
 def solve_triang(cho_L, delta):
@@ -80,9 +69,9 @@ def solve_triang(cho_L, delta):
 
 
 def chi_squared(params):
-    mu_theory = np.where(ceph_mask, ceph_dists, model_mu(params))
-    apparent_mag_theory = mu_theory + params[0] + outflow_correction(params)
-    delta = apparent_mags - apparent_mag_theory
+    mu_pred = np.where(ceph_mask, ceph_dists, mu_theory(params))
+    mB_theory = mu_pred + params[0] + mu_corr(params)
+    delta = mB_vals - mB_theory
     return solve_triang(cho, delta)
 
 
@@ -95,7 +84,7 @@ bounds = np.array(
         (-20.0, -18.5),  # M
         (60.0, 85.0),  # H0
         (0.1, 0.6),  # Ωm
-        (-1.6, 3.2),  # vb
+        (-3.5, 2.0),  # v / 100 km/s
     ]
 )
 
@@ -120,13 +109,15 @@ def main():
     import emcee
     from multiprocessing import Pool
     from corner_plot import plot_corner_and_chains
+    from log_evidence import log_evidence
     from sn.plotting import plot_predictions, print_color, plot_residuals
 
-    burn_in = 500
     n_dim = len(bounds)
     n_walkers = 150
+    burn_in = 500
     n_steps = burn_in + 2500
-    initial_pos = np.random.uniform(bounds[:, 0], bounds[:, 1], size=(n_walkers, n_dim))
+    np.random.seed(42)
+    state0 = np.random.uniform(bounds[:, 0], bounds[:, 1], size=(n_walkers, n_dim))
     moves = [
         (emcee.moves.KDEMove(bw_method="silverman"), 0.2),
         (emcee.moves.DEMove(), 0.8),
@@ -135,11 +126,14 @@ def main():
     with Pool(6) as pool:
         sampler = emcee.EnsembleSampler(n_walkers, n_dim, log_probability, pool, moves)
         sampler.run_mcmc(
-            initial_pos, n_steps, progress=True, progress_kwargs={"colour": "#ff5a00"}
+            state0, n_steps, progress=True, progress_kwargs={"colour": "#ff5a00"}
         )
 
     chains_samples = sampler.get_chain(discard=burn_in, flat=False)
     samples = sampler.get_chain(discard=burn_in, flat=True)
+    log_probs = sampler.get_log_prob(discard=burn_in, flat=True)
+    log_evd = log_evidence(samples, log_probs, log_probability, bounds)
+    print_color("Log Evidence", f"{log_evd:.2f}")
 
     try:
         tau = sampler.get_autocorr_time()
@@ -155,58 +149,52 @@ def main():
         (M_16, M_50, M_84),
         (H0_16, H0_50, H0_84),
         (omega_16, omega_50, omega_84),
-        (vb_16, vb_50, vb_84),
+        (v_16, v_50, v_84),
     ] = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
 
     best_fit = np.percentile(samples, 50, axis=0)
 
-    mu_vals_theory = model_mu(best_fit)
-    residuals = (
-        apparent_mags
-        - (M_50 + outflow_correction(best_fit))
-        - np.where(ceph_mask, ceph_dists, mu_vals_theory)
-    )
+    mu_pred = mu_theory(best_fit)
+    mB_corrected = mB_vals - mu_corr(best_fit)
+    residuals = mB_corrected - M_50 - np.where(ceph_mask, ceph_dists, mu_pred)
 
-    # R-squared
     ss_res = np.sum(residuals**2)
-    ss_tot = np.sum((apparent_mags - np.mean(apparent_mags)) ** 2)
+    ss_tot = np.sum((mB_corrected - np.mean(mB_corrected)) ** 2)
     r_squared = 1 - (ss_res / ss_tot)
-
-    # root mean square deviation
     rmsd = np.sqrt(np.mean(residuals**2))
 
-    M_label = f"{M_50:.3f} +{M_84-M_50:.3f}/-{M_50-M_16:.3f}"
-    H0_label = f"{H0_50:.2f} +{H0_84-H0_50:.2f}/-{H0_50-H0_16:.2f}"
+    M_label = f"{M_50:.3f} +{M_84-M_50:.3f}/-{M_50-M_16:.3f} mag"
+    H0_label = f"{H0_50:.2f} +{H0_84-H0_50:.2f}/-{H0_50-H0_16:.2f} km/s/Mpc"
     omega_label = f"{omega_50:.3f} +{omega_84-omega_50:.3f}/-{omega_50-omega_16:.3f}"
-    v_flow_label = f"{vb_50:.3f} +{vb_84-vb_50:.3f}/-{vb_50-vb_16:.3f}"
+    v_flow_label = f"{v_50:.3f} +{v_84-v_50:.3f}/-{v_50-v_16:.3f} x 100 km/s"
 
     print_color("Dataset", legend)
     print_color("z range", f"{z_cmb[0]:.4f} - {z_cmb[-1]:.4f}")
     print_color("Sample size", len(z_cmb))
     print_color("M", M_label)
-    print_color("H0 (km/s/Mpc)", H0_label)
+    print_color("H0", H0_label)
     print_color("Ωm", omega_label)
-    print_color("v_flow (100 km/s)", v_flow_label)
+    print_color("v", v_flow_label)
     print_color("R-squared (%)", f"{100 * r_squared:.2f}")
     print_color("RMSD (mag)", f"{rmsd:.3f}")
     print_color("Skewness of residuals", f"{stats.skew(residuals):.3f}")
     print_color("kurtosis of residuals", f"{stats.kurtosis(residuals):.3f}")
     print_color("Chi squared", f"{chi_squared(best_fit):.2f}")
 
-    sigma_mu = np.sqrt(cov_matrix.diagonal())
+    mu_std = np.sqrt(cov_matrix.diagonal())
 
-    labels = ["M", "$H_0$", "$Ω_m$", "$v_{flow}$"]
+    labels = ["M", "$H_0$", "$Ω_m$", "$v_{100}$"]
     plot_corner_and_chains(labels=labels, flat_samples=samples, samples=chains_samples)
     plot_predictions(
         legend=legend,
         x=z_cmb,
-        y=apparent_mags - (M_50 + outflow_correction(best_fit)),
-        y_err=sigma_mu,
-        y_model=mu_vals_theory,
+        y=mB_corrected - M_50,
+        y_err=mu_std,
+        y_model=mu_pred,
         label=f"H0={H0_50:.2f} km/s/Mpc",
         x_scale="log",
     )
-    plot_residuals(z_values=z_cmb, residuals=residuals, y_err=sigma_mu, bins=65)
+    plot_residuals(z_values=z_cmb, residuals=residuals, y_err=mu_std, bins=65)
 
 
 if __name__ == "__main__":
@@ -233,19 +221,19 @@ Chi squared: 1452.02
 =============================
 
 ΛCDM
-Void outflow correction to magnitude of SNe M(z) = M_inf + v_flow_corr
-v_flow_corr = 100 * v_flow * (5 / ln(10)) / (c * z_cmb) with v_flow in units 100 km/s
+Isotropic velocity SNe observed redshifts (limit to z <= 0.1)
+z_cosmo = -1 + (1 + z) / (1 + v/c)
 
-v_flow: 81 +- 47 km/s
-M_inf: -19.326 +- 0.056 mag
-M0 (computed at z=0.0063): -19.233 mag
-H0: 71.3 +- 1.6 km/s/Mpc
-Ωm: 0.315 +- 0.020
+M: -19.242 +0.029/-0.029 mag
+H0: 74.01 +1.07/-1.06 km/s/Mpc
+Ωm: 0.317 +0.020/-0.020
+v: -0.68 +0.45/-0.45 x 100 km/s
 R-squared (%): 99.78
 RMSD (mag): 0.153
-Skewness of residuals: 0.050
-kurtosis of residuals: 1.541
-Chi squared: 1448.94 (1.75 sigma significance)
+Skewness of residuals: 0.052
+kurtosis of residuals: 1.558
+Chi squared: 1449.61 (1.55 sigma significance)
+Log Evidence: -736.14
 
 =============================
 
