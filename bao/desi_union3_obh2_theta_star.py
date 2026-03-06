f045d42905cc39357d31c271e51b4138135593f7
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-03-06 17:57:45 +0100

    bao union3.1 obh2+theta* cmb

--- bao/desi_union3_obh2_theta_star.py
+++ bao/desi_union3_obh2_theta_star.py
@@ -2,34 +2,34 @@ from numba import njit
 import numpy as np
 import cmb.data_planck_act_compression as cmb
 from interpolator import interp_hermite
-from y2023union3.data import get_data as get_sn_data
+from y2026union3_1.data import get_data as get_sn_data
 from y2025BAO.data import get_data as get_bao_data
 
 c = cmb.c  # Speed of light in km/s
 Orh2 = cmb.Or_h2
 Omnuh2 = cmb.Omnu_h2
 
-sn_legend, z_sn_vals, mu_vals, cov_matrix_sn = get_sn_data()
+sn_legend, z_cmb, z_hel, mu_vals, cov_matrix_sn = get_sn_data()
 bao_legend, bao_data, cov_matrix_bao = get_bao_data()
 
 inv_cov_sn = np.linalg.inv(cov_matrix_sn)
 inv_cov_bao = np.linalg.inv(cov_matrix_bao)
 inv_cov_cmb = np.linalg.inv(cmb.covariance[1:, 1:])
 
-z_max = max(np.max(z_sn_vals), np.max(bao_data["z"])) + 0.1
+z_max = max(np.max(z_cmb), np.max(bao_data["z"])) + 0.1
 z_grid = np.linspace(0, z_max, num=4000, dtype=np.float64)
-dx = np.diff(z_grid)
+dz = np.diff(z_grid)
 
 
 @njit
-def Ode_z(z, w0, wa):
+def Ode_z(z, w0):
+    # Thawing quintessence
     cubed = (1.0 + z) ** 3
     return (2 * cubed / (1.0 + w0 + (1.0 - w0) * cubed)) ** 2
 
 
 @njit
-def Ez(z, H0, Obh2, Och2, w0=-1.0, wa=0.0):
-    h = H0 / 100
+def Ez(z, h, Obh2, Och2):
     Onu = Omnuh2 / h**2
     Or = Orh2 / h**2
     Obc = (Obh2 + Och2) / h**2
@@ -40,21 +40,28 @@ def Ez(z, H0, Obh2, Och2, w0=-1.0, wa=0.0):
     radiation_term = Or * zp1**4
     matter_term = Obc * zp1**3
     neutrino_term = Onu * cmb.Omnu_z(z)
-    dark_energy_term = Ode * Ode_z(z, w0, wa)
+    dark_energy_term = Ode
 
     return np.sqrt(radiation_term + matter_term + neutrino_term + dark_energy_term)
 
 
 @njit
-def theory_mu(theta):
-    dL = (1.0 + z_sn_vals) * DM_z(z_sn_vals, theta)
-    return theta[0] + 25.0 + 5 * np.log10(dL)
+def mu_corr(params, DM_ref):
+    v_km_s = 100 * params[4] * np.where(z_cmb <= 0.2, 1, -1)
+    z_pec = v_km_s / c
+    z_cosmo = -1.0 + (1.0 + z_cmb) / (1.0 + z_pec)
+    return 5.0 * np.log10(DM_z(z_cosmo, params) / DM_ref)
+
+
+@njit
+def theory_mu(theta, DM):
+    return theta[0] + 25.0 + 5 * np.log10((1.0 + z_hel) * DM)
 
 
 @njit
 def H_z(z, params):
-    H0, Obh2, Och2, w0 = params[1:]
-    return H0 * Ez(z, H0, Obh2, Och2, w0)
+    H0 = params[1]
+    return H0 * Ez(z, h=H0 / 100, Obh2=params[2], Och2=params[3])
 
 
 cmb.set_HZ(H_z)
@@ -68,9 +75,9 @@ def DH_z(z, params):
 @njit
 def DM_z(z, theta):
     dh_grid = DH_z(z_grid, theta)
-    dy = (dh_grid[:-1] + dh_grid[1:]) / 2
+    dh = (dh_grid[:-1] + dh_grid[1:]) / 2
     cum_dm = np.zeros(z_grid.size, dtype=np.float64)
-    cum_dm[1:] = np.cumsum(dx * dy)
+    cum_dm[1:] = np.cumsum(dh * dz)
     return interp_hermite(z, z_grid, cum_dm, dh_grid)
 
 
@@ -82,7 +89,7 @@ def DV_z(z, theta):
 
 
 qty_map = {"DV_over_rs": 0, "DM_over_rs": 1, "DH_over_rs": 2}
-quantities = np.array([qty_map[q] for q in bao_data["quantity"]], dtype=np.int64)
+desi_qty = np.array([qty_map[q] for q in bao_data["quantity"]], dtype=np.int64)
 
 
 @njit
@@ -102,13 +109,14 @@ def bao_theory(z, qty, theta):
 
 @njit
 def chi2_sn(theta):
-    delta_sn = mu_vals - theory_mu(theta)
+    DM = DM_z(z_cmb, theta)
+    delta_sn = mu_vals - theory_mu(theta, DM) - mu_corr(theta, DM)
     return delta_sn @ inv_cov_sn @ delta_sn
 
 
 @njit
 def chi2_bao(theta):
-    delta_bao = bao_data["value"] - bao_theory(bao_data["z"], quantities, theta)
+    delta_bao = bao_data["value"] - bao_theory(bao_data["z"], desi_qty, theta)
     return delta_bao @ inv_cov_bao @ delta_bao
 
 
@@ -133,11 +141,11 @@ def main():
     from bao.plot_predictions import plot_bao_predictions
 
     prior = Prior()
-    prior.add_parameter("ΔM", dist=(-1.0, +1.0))
-    prior.add_parameter("H0", dist=(50.0, 90.0))
+    prior.add_parameter("ΔM", dist=(-1, +1))
+    prior.add_parameter("H0", dist=(50, 90))
     prior.add_parameter("ωb", dist=(0.01, 0.03))
     prior.add_parameter("ωc", dist=(0.05, 0.3))
-    prior.add_parameter("w0", dist=(-1.0, -1 / 3))
+    prior.add_parameter("v100", dist=(-12, 5))
 
     with Pool(8) as pool:
         sampler = Sampler(
@@ -153,7 +161,7 @@ def main():
     H0_16, H0_50, H0_84 = quantile(samples[:, 1], one_sigma_ci, weights=w)
     Obh2_16, Obh2_50, Obh2_84 = quantile(samples[:, 2], one_sigma_ci, weights=w)
     Och2_16, Och2_50, Och2_84 = quantile(samples[:, 3], one_sigma_ci, weights=w)
-    w0_16, w0_50, w0_84 = quantile(samples[:, 4], one_sigma_ci, weights=w)
+    v_16, v_50, v_84 = quantile(samples[:, 4], one_sigma_ci, weights=w)
 
     Omh2_samples = samples[:, 2] + samples[:, 3] + Omnuh2
     Om_samples = Omh2_samples / (samples[:, 1] / 100) ** 2
@@ -165,8 +173,8 @@ def main():
     zd_16, zd_50, zd_84 = quantile(zd_samples, one_sigma_ci, weights=w)
     rd_16, rd_50, rd_84 = quantile(rd_samples, one_sigma_ci, weights=w)
 
-    best_fit = [dM_50, H0_50, Obh2_50, Och2_50, w0_50]
-    degrees_of_freedom = 2 + len(bao_data["z"]) + len(z_sn_vals) - len(best_fit)
+    best_fit = [dM_50, H0_50, Obh2_50, Och2_50, v_50]
+    degrees_of_freedom = 2 + len(bao_data) + len(z_cmb) - len(best_fit)
 
     print(f"ΔM: {dM_50:.3f} +{(dM_84 - dM_50):.3f} -{(dM_50 - dM_16):.3f} mag")
     print(f"H0: {H0_50:.2f} +{(H0_84 - H0_50):.2f} -{(H0_50 - H0_16):.2f} km/s/Mpc")
@@ -174,14 +182,14 @@ def main():
     print(f"ωc: {Och2_50:.4f} +{(Och2_84 - Och2_50):.4f} -{(Och2_50 - Och2_16):.4f}")
     print(f"ωm: {Omh2_50:.4f} +{(Omh2_84 - Omh2_50):.4f} -{(Omh2_50 - Omh2_16):.4f}")
     print(f"Ωm: {Om_50:.3f} +{(Om_84 - Om_50):.3f} -{(Om_50 - Om_16):.3f}")
-    print(f"w0: {w0_50:.3f} +{(w0_84 - w0_50):.3f} -{(w0_50 - w0_16):.3f}")
+    print(f"v: {v_50:.3f} +{(v_84 - v_50):.3f} -{(v_50 - v_16):.3f} x 100 km/s")
     print(f"z_d: {zd_50:.2f} +{(zd_84 - zd_50):.2f} -{(zd_50 - zd_16):.2f}")
     print(f"r_d: {rd_50:.2f} +{(rd_84 - rd_50):.2f} -{(rd_50 - rd_16):.2f} Mpc")
     print(f"Chi squared: {chi_squared(best_fit):.2f}")
     print(f"Log Evidence: {sampler.log_z:.2f}")
     print(f"Degs of freedom: {degrees_of_freedom}")
 
-    labels = ["$Δ_M$", "$H_0$", "$ω_b$", "$ω_c$", "$w_0$"]
+    labels = ["$Δ_M$", "$H_0$", "$ω_b$", "$ω_c$", "$v_{100}$"]
     corner(
         samples,
         weights=w,
@@ -207,10 +215,10 @@ def main():
     )
     plot_sn_predictions(
         legend=sn_legend,
-        x=z_sn_vals,
-        y=mu_vals,
+        x=z_cmb,
+        y=mu_vals - mu_corr(best_fit, DM_z(z_cmb, best_fit)),
         y_err=np.sqrt(np.diag(cov_matrix_sn)),
-        y_model=theory_mu(best_fit),
+        y_model=theory_mu(best_fit, DM_z(z_cmb, best_fit)),
         label=f"$Ω_m$={Om_50:.3f}",
         x_scale="log",
     )
@@ -222,7 +230,7 @@ if __name__ == "__main__":
 
 """
 DESI DR2
-SNIa Union3
+SNIa Union3.1
 (θ*, ωb) CMB Planck + ACT compression
 
 Priors:
@@ -230,6 +238,9 @@ Priors:
 H0 U(50.0, 90.0)
 ωb U(0.01, 0.03)
 ωc U(0.05, 0.3)
+
+flow correction:
+v U(-12, 5) x 100 km/s
 
 wzCDM:
 w0 U(-1.0, -1/3)
@@ -243,65 +254,76 @@ wa U(-3.0, 1.0)
 """
 
 """
-Flat ΛCDM  w(z) = -1
-H0: 68.59 +0.30 -0.30 km/s/Mpc
+Flat ΛCDM
+
+ΔM: -0.047 +0.007 -0.007 mag
+H0: 68.63 +0.30 -0.30 km/s/Mpc
 ωb: 0.02249 +0.00011 -0.00011
-ωc: 0.1166 +0.0008 -0.0008
-ωm: 0.1397 +0.0008 -0.0008
-Ωm: 0.297 +0.004 -0.004
-w0: -1
-wa: 0
-z_d: 1059.96 +0.27 -0.26
-r_d: 147.87 +0.26 -0.27 Mpc
-Chi squared: 39.69
-Log Evidence: -36.40
+ωc: 0.1165 +0.0008 -0.0008
+ωm: 0.1396 +0.0008 -0.0008
+Ωm: 0.296 +0.004 -0.004
+z_d: 1059.95 +0.27 -0.27
+r_d: 147.90 +0.27 -0.26 Mpc
+Chi squared: 41.66
+Log Evidence: -40.49
 Degs of freedom: 33
+"""
 
-===============================
+"""
+Flat ΛCDM
+Isotropic coherent flow (turning point z <= 0.2 inflow z > 0.2 outflow)
+z_cosmo = -1 + (1 + z) / (1 + v/c)
 
+v: -3.16 +1.05 -1.03 x 100 km/s
+ΔM: -0.047 +0.007 -0.007 mag
+H0: 68.71 +0.30 -0.30 km/s/Mpc
+ωb: 0.02249 +0.00011 -0.00011
+ωc: 0.1163 +0.0008 -0.0008
+ωm: 0.1394 +0.0008 -0.0008
+Ωm: 0.295 +0.004 -0.004
+z_d: 1059.94 +0.26 -0.26
+r_d: 147.96 +0.26 -0.26 Mpc
+Chi squared: 32.50 (3.03 sigma significance)
+Log Evidence: -37.78 (delta logZ = 2.71 in favour of v corrections)
+Degs of freedom: 32
+"""
+
+"""
 Flat wCDM w(z) = w0
-H0: 66.87 +0.78 -0.78 km/s/Mpc
+
+w0: -0.936 +0.033 -0.033
+ΔM: -0.064 +0.011 -0.012 mag
+H0: 67.28 +0.76 -0.76 km/s/Mpc
 ωb: 0.02250 +0.00011 -0.00011
-ωc: 0.1140 +0.0014 -0.0014
-ωm: 0.1372 +0.0014 -0.0014
-Ωm: 0.307 +0.006 -0.006
-w0: -0.919 +0.034 -0.034
-wa: 0
-z_d: 1059.78 +0.28 -0.28
-r_d: 148.56 +0.41 -0.41 Mpc
-Chi squared: 33.96
-Log Evidence: -35.71 (Δ logZ = 0.69 against ΛCDM)
+ωc: 0.1145 +0.0014 -0.0014
+ωm: 0.1376 +0.0014 -0.0014
+Ωm: 0.304 +0.006 -0.006
+z_d: 1059.81 +0.27 -0.28
+r_d: 148.43 +0.41 -0.39 Mpc
+Chi squared: 37.92 (1.93 sigma significance)
+Log Evidence: -40.79 (delta logZ = -0.30 in favour of ΛCDM)
 Degs of freedom: 32
+"""
 
-===============================
-
+"""
 Flat wzCDM: w(z) = -1 + 2 * (1 + w0) / (1 + w0 + (1 - w0) * (1 + z)^3)
-H0: 66.17 +0.85 -0.84 km/s/Mpc
+
+w0: -0.846 +0.062 -0.063
+wa: d w/dz at z=0 = -1.5 * (1 - w0^2) (derived)
+ΔM: -0.062 +0.010 -0.010 mag
+H0: 66.78 +0.81 -0.80 km/s/Mpc
 ωb: 0.02250 +0.00011 -0.00011
-ωc: 0.1150 +0.0010 -0.0010
-ωm: 0.1382 +0.0010 -0.0010
-Ωm: 0.316 +0.008 -0.008
-w0: -0.799 +0.063 -0.067
-wa: d w(z)/dz at z=0 = -1.5 * (1 - w0^2)
-z_d: 1059.85 +0.27 -0.27
-r_d: 148.29 +0.30 -0.30 Mpc
-Chi squared: 30.74
-Log Evidence: -33.42 (Δ logZ = 2.98 against ΛCDM)
+ωc: 0.1153 +0.0010 -0.0010
+ωm: 0.1384 +0.0010 -0.0010
+Ωm: 0.310 +0.007 -0.007
+z_d: 1059.87 +0.27 -0.27
+r_d: 148.22 +0.30 -0.30 Mpc
+Chi squared: 35.87 (2.41 sigma significance)
+Log Evidence: -39.10 (delta logZ = 1.39 against ΛCDM)
 Degs of freedom: 32
+"""
 
-===============================
-
+"""
 Flat w(z) = w0 + wa * z / (1 + z)
-H0: 66.10 +0.84 -0.83 km/s/Mpc
-ωb: 0.02250 +0.00011 -0.00011
-ωc: 0.1179 +0.0018 -0.0019
-ωm: 0.1411 +0.0018 -0.0019
-Ωm: 0.323 +0.010 -0.009
-w0: -0.720 +0.099 -0.095
-wa: -0.803 +0.361 -0.384
-z_d: 1060.06 +0.29 -0.30
-r_d: 147.51 +0.54 -0.48 Mpc
-Chi squared: 29.24
-Log Evidence: -35.36 (Δ logZ = 1.05 against ΛCDM)
-Degs of freedom: 31
+TODO
 """
