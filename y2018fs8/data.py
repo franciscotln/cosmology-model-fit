4eaabaa58e1085282bfe9665f3d319efdaa1b4bb
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-03-08 21:25:19 +0100

    fs8 data

--- y2018fs8/data.py
+++ y2018fs8/data.py
@@ -1,5 +1,24 @@
 import numpy as np
 
+"""
+
+Data:
+arXiv:1803.01337v4
+arXiv:2110.08498v2 (ALFALFA z=0.013)
+
+Covariances:
+arXiv:1806.10822v2
+
+Covariance for the data points z = 0.3, 0.4, 0.5, 0.6 respectively (cov_id = 2)
+arXiv:1203.6565v2, section 5.1
+estimated_corr = [
+    [1.00, 0.84, 0.50, 0.15],
+    [0.84, 1.00, 0.88, 0.65],
+    [0.50, 0.88, 1.00, 0.92],
+    [0.15, 0.65, 0.92, 1.00],
+]
+"""
+
 pathname = "y2018fs8/raw/"
 data = np.loadtxt(
     pathname + "fs8.csv",
@@ -13,35 +32,15 @@ data = np.loadtxt(
         ("s8_fid", np.float64),
         ("year", np.int16),
         ("cov_id", np.int16),
+        ("outlier", bool),
     ],
 )
 cov_mat = np.loadtxt(pathname + "fs8_cov.dat", dtype=np.float64)
 
-mask = (
-    ((data["omega_fid"] >= 0.28) & (data["s8_fid"] >= 0.8))
-    | (data["cov_id"] == 1)
-    | (data["cov_id"] == 2)
-    | (data["cov_id"] == 3)
-)
+mask = data["outlier"] == False  # 3 data points
 
 data = data[mask]
 cov_mat = cov_mat[mask, :][:, mask]
-
-# covariances from arXiv:1806.10822v2
-
-# data
-# arXiv:1803.01337v4
-# arXiv:2110.08498v2
-# arXiv:2007.08999v2
-
-# Covariance for the data points z = 0.3, 0.4, 0.5, 0.6 respectively (cov_id = 2)
-# taken from arXiv:1203.6565v2, section 5.1
-# estimated_corr = [
-#     [1.00, 0.84, 0.50, 0.15],
-#     [0.84, 1.00, 0.88, 0.65],
-#     [0.50, 0.88, 1.00, 0.92],
-#     [0.15, 0.65, 0.92, 1.00],
-# ]
 
 cov1 = 1e-3 * np.array(
     [
