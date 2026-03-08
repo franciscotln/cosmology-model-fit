98fcb9a81c9b4318e15f64438ee13aeb7577cc11
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-03-08 13:13:22 +0100

    add sig8 fiducial to data

--- y2018fs8/data.py
+++ y2018fs8/data.py
@@ -10,6 +10,7 @@ data = np.loadtxt(
         ("fs8", np.float64),
         ("fs8_err", np.float64),
         ("omega_fid", np.float64),
+        ("s8_fid", np.float64),
         ("year", np.int16),
         ("cov_id", np.int16),
     ],
