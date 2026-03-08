caebd5d8798c8acb59005361cab83cf5d95428dd
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-03-09 00:25:25 +0100

    bao+fs8+cc

--- bao/desi_cc_fs8.py
+++ bao/desi_cc_fs8.py
@@ -22,17 +22,24 @@ logdet_fs8 = np.linalg.slogdet(fs8.cov_mat)[1]
 inv_cov_cc = np.linalg.inv(cov_matrix)
 logdet_cc = np.linalg.slogdet(cov_matrix)[1]
 
+N_cc = z_cc.size
+N_fs8 = z_fs8.size
+
 z_max = max(np.max(z_fs8), np.max(z_cc), np.max(data["z"]))
 z_grid = np.linspace(0, z_max + 0.1, num=4000)
 dz = np.diff(z_grid)
 
 
 @njit
+def Ode_z(z, w0):
+    cubic = (1.0 + z) ** 3
+    return (2 * cubic / (1.0 + w0 + (1.0 - w0) * cubic)) ** 2
+
+
+@njit
 def H_z(z, params):
     H0, Om, w0 = params[0], params[1], params[-1]
-    cubic = (1 + z) ** 3
-    rho_de = (2 * cubic / (1 + w0 + (1 - w0) * cubic)) ** 2
-    return H0 * np.sqrt(Om * (1 + z) ** 3 + (1 - Om) * rho_de)
+    return H0 * np.sqrt(Om * (1.0 + z) ** 3 + (1.0 - Om) * Ode_z(z, w0))
 
 
 @njit
@@ -66,12 +73,14 @@ def dH_da(z, params):
     return -dH_dz / a**2
 
 
-denominator_fiducial = np.zeros(len(z_fs8), dtype=np.float64)
+denominator_fiducial = np.zeros(N_fs8, dtype=np.float64)
 
-for i in range(len(z_fs8)):
+for i in range(N_fs8):
     zi = z_fs8[i]
     Om_fid = fs8.data["omega_fid"][i]
-    params = [67.5, Om_fid, 0.8, 1.0, 1.0, 147.0, -1.0]
+    s8_fid = fs8.data["s8_fid"][i]
+    w0_fid = -1.0
+    params = [67.5, Om_fid, s8_fid, 1.0, 1.0, 147.0, w0_fid]
     denominator_fiducial[i] = H_z(zi, params) * DM_z(zi, params)
 
 
@@ -115,11 +124,11 @@ def fs8_theory(z, params):
 
 
 qty_map = {"DV_over_rs": 0, "DM_over_rs": 1, "DH_over_rs": 2}
-quantities = np.array([qty_map[q] for q in data["quantity"]], dtype=np.int32)
+desi_qty = np.array([qty_map[q] for q in data["quantity"]], dtype=np.int32)
 
 
 @njit
-def theory_bao(z, qty, params):
+def bao_theory(z, qty, params):
     results = np.empty(z.size, dtype=np.float64)
     DV_mask = qty == 0
     DM_mask = qty == 1
@@ -140,19 +149,17 @@ def chi_squared(params):
     delta_cc = H_values - H_z(z_cc, params)
     chi2_cc = f_cc**2 * np.dot(delta_cc, np.dot(inv_cov_cc, delta_cc))
 
-    delta_bao = data["value"] - theory_bao(data["z"], quantities, params)
+    delta_bao = data["value"] - bao_theory(data["z"], desi_qty, params)
     chi_bao = np.dot(delta_bao, np.dot(inv_cov_bao, delta_bao))
 
     return chi2_cc + chi2_fs8 + chi_bao
 
 
 def log_likelihood(params):
-    N_cc = z_cc.size
     normalization_cc = (
         N_cc * np.log(2 * np.pi) + logdet_cc - 2 * N_cc * np.log(params[3])
     )
 
-    N_fs8 = z_fs8.size
     normalization_fs8 = (
         N_fs8 * np.log(2 * np.pi) + logdet_fs8 - 2 * N_fs8 * np.log(params[4])
     )
@@ -166,16 +173,17 @@ def main():
     import matplotlib.pyplot as plt
     from multiprocessing import Pool
     from cosmic_chronometers.plot_predictions import plot_cc_predictions
+    from fs8.plot_predictions import plot_predictions as plot_fs8_predictions
     from bao.plot_predictions import plot_bao_predictions
 
     prior = Prior()
-    prior.add_parameter("H0", dist=(40.0, 100.0))
+    prior.add_parameter("H0", dist=(40, 100))
     prior.add_parameter("Om", dist=(0.1, 0.6))
-    prior.add_parameter("sig8", dist=(0.2, 1.2))
-    prior.add_parameter("f_cc", dist=(0.3, 2.6))
-    prior.add_parameter("f_fs8", dist=(0.5, 2.2))
-    prior.add_parameter("rd", dist=(110.0, 180.0))
-    prior.add_parameter("w0", dist=(-1.0, 0.0))
+    prior.add_parameter("sig8", dist=(0.1, 1.5))
+    prior.add_parameter("f_cc", dist=(0.03, 3.0))
+    prior.add_parameter("f_fs8", dist=(0.2, 3.0))
+    prior.add_parameter("rd", dist=(110, 180))
+    prior.add_parameter("w0", dist=(-1, 0))
 
     with Pool(6) as pool:
         sampler = Sampler(
@@ -188,10 +196,19 @@ def main():
     log_evd = sampler.log_z
     one_sigma_ci = [0.159, 0.5, 0.841]
 
+    labels = [
+        "$H_0$",
+        "$Ω_m$",
+        "$\sigma_8$",
+        "$f_{cc}$",
+        "$f_{fs8}$",
+        "$r_{drag}$",
+        "$w_0$",
+    ]
     corner(
         samples,
         weights=w,
-        labels=prior.keys,
+        labels=labels,
         quantiles=one_sigma_ci,
         show_titles=True,
         title_fmt=".4f",
@@ -201,7 +218,7 @@ def main():
         smooth=2.0,
         smooth1d=2.0,
         levels=(0.393, 0.864),
-        range=np.repeat(0.9999, len(prior.keys)),
+        range=np.repeat(0.9999, len(labels)),
     )
     plt.show()
 
@@ -216,7 +233,7 @@ def main():
     S8_samples = samples[:, 2] * np.sqrt(samples[:, 1] / 0.3)
     S8_16, S8_50, S8_84 = quantile(S8_samples, one_sigma_ci, weights=w)
 
-    best_fit = [H0_50, Om_50, sig8_50, fcc_50, fs_50, rd_50 , w0_50]
+    best_fit = [H0_50, Om_50, sig8_50, fcc_50, fs_50, rd_50, w0_50]
 
     print(f"H0: {H0_50:.1f} +{(H0_84 - H0_50):.1f} -{(H0_50 - H0_16):.1f} km/s/Mpc")
     print(f"Ωm: {Om_50:.3f} +{(Om_84 - Om_50):.3f} -{(Om_50 - Om_16):.3f}")
@@ -232,7 +249,7 @@ def main():
     print(f"Degs of freedom: {len(z_cc) + len(z_fs8) - len(best_fit)}")
 
     plot_bao_predictions(
-        theory_predictions=lambda z, qty: theory_bao(z, qty, best_fit),
+        theory_predictions=lambda z, qty: bao_theory(z, qty, best_fit),
         data=data,
         errors=np.sqrt(np.diag(bao_cov_matrix)),
         title=bao_legend,
@@ -244,22 +261,12 @@ def main():
         H_err=np.sqrt(np.diag(cov_matrix)) / fcc_50,
         label=f"{cc_legend} $H_0$: {H0_50:.1f} ± {(H0_84 - H0_50):.1f} km/s/Mpc",
     )
-
-    z_plot = np.linspace(0, np.max(z_fs8), 200)
-    fs8_plot = fs8_theory(z_plot, best_fit)
-    q = H_z(z_fs8, best_fit) * DM_z(z_fs8, best_fit) / denominator_fiducial
-    plt.errorbar(
-        z_fs8,
-        fs8_values * q,
-        yerr=fs8.data["fs8_err"] * q / fs_50,
-        fmt=".",
-        label="data",
+    plot_fs8_predictions(
+        fs8_theory=lambda z: fs8_theory(z, best_fit),
+        data=fs8.data,
+        q=H_z(z_fs8, best_fit) * DM_z(z_fs8, best_fit) / denominator_fiducial,
+        f_err=fs_50,
     )
-    plt.plot(z_plot, fs8_plot, label="best-fit", color="C1")
-    plt.xlabel("z")
-    plt.ylabel(r"$f\sigma_8(z)$")
-    plt.legend()
-    plt.show()
 
 
 if __name__ == "__main__":
@@ -268,50 +275,52 @@ if __name__ == "__main__":
 
 """
 Flat ΛCDM: w(z) = -1
-H0: 69.2 +2.3 -2.3 km/s/Mpc
-Ωm: 0.295 +0.008 -0.008
-σ8: 0.778 +0.011 -0.011
-S8: 0.772 +0.013 -0.013
+
+H0: 68.9 +2.2 -2.3 km/s/Mpc
+Ωm: 0.299 +0.008 -0.008
+σ8: 0.784 +0.009 -0.009
+S8: 0.783 +0.011 -0.011
+rd: 147.1 +4.9 -4.6 Mpc
 f_cc: 1.48 +0.18 -0.17
-f_fs8: 1.31 +0.12 -0.12
-rd: 146.9 +4.9 -4.6 Mpc
-Chi squared: 107.83
-Log likelihood: -46.24
-Log evidence: -63.1
-Degs of freedom: 93
+f_fs8: 1.71 +0.16 -0.16
+Chi squared: 102.70
+Log likelihood: -39.13
+Log evidence: -57.0
+Degs of freedom: 88
 """
 
 """
 Flat wCDM: w(z) = w0
-H0: 67.5 +2.5 -2.4 km/s/Mpc
-Ωm: 0.293 +0.008 -0.008
-σ8: 0.808 +0.022 -0.021
-S8: 0.798 +0.020 -0.019
+
+H0: 67.0 +2.4 -2.3 km/s/Mpc
+Ωm: 0.295 +0.008 -0.008
+σ8: 0.821 +0.022 -0.020
+S8: 0.815 +0.018 -0.018
+rd: 147.4 +4.9 -4.6 Mpc
+w0: -0.858 +0.060 -0.062 (prior -1.4 to -0.4)
 f_cc: 1.48 +0.18 -0.17
-f_fs8: 1.34 +0.12 -0.12
-rd: 147.3 +5.0 -4.6 Mpc
-w0: -0.880 +0.064 -0.066 (prior -1.4 to -0.4)
-Chi squared: 106.65
-Log likelihood: -44.62
-Log evidence: -63.3
-Degs of freedom: 92
+f_fs8: 1.78 +0.17 -0.16
+Chi squared: 101.73
+Log likelihood: -36.55
+Log evidence: -56.3 (Δ logZ = 0.7 aganst ΛCDM)
+Degs of freedom: 87
 """
 
 """
 Flat wzCDM: w(z) = -1 + 2 * (1 + w0) / (1 + w0 + (1 - w0) * (1 + z)^3)
-H0: 66.8 +2.5 -2.5 km/s/Mpc
-Ωm: 0.307 +0.010 -0.010
-σ8: 0.799 +0.016 -0.015
-S8: 0.807 +0.023 -0.022
+
+H0: 66.0 +2.5 -2.4 km/s/Mpc
+Ωm: 0.313 +0.009 -0.009
+σ8: 0.813 +0.015 -0.015
+S8: 0.831 +0.021 -0.021
+rd: 147.4 +4.9 -4.6 Mpc
+w0: -0.724 +0.094 -0.100 (prior -1.0 to 0.0)
 f_cc: 1.47 +0.18 -0.17
-f_fs8: 1.33 +0.12 -0.12
-rd: 147.2 +5.0 -4.7 Mpc
-w0: -0.782 +0.104 -0.107 (prior -1.0 to 0.0)
-wa: -1.5 * (1 - w0^2)
-Chi squared: 105.64
-Log likelihood: -44.60
-Log evidence: -62.8
-Degs of freedom: 92
+f_fs8: 1.78 +0.17 -0.17
+Chi squared: 100.31
+Log likelihood: -35.82
+Log evidence: -55.1 (Δ logZ = 1.9 aganst ΛCDM)
+Degs of freedom: 87
 """
 
 """
