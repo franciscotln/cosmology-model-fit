b80b4e8da4c456094edac29045abff8b4a008ffa
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-03-17 11:59:56 +0100

    desi+des dovekie+planck prior Omh2

--- bao/desi_des5y_omh2.py
+++ bao/desi_des5y_omh2.py
@@ -7,25 +7,30 @@ from y2025DESdovekie.data import get_data, effective_sample_size as sn_size
 from y2025BAO.data import get_data as get_bao_data
 
 sn_legend, z_cmb, z_hel, mu_values, cov_matrix_sn = get_data()
-bao_legend, bao_data, cov_matrix_bao = get_bao_data()
+bao_legend, bao, cov_matrix_bao = get_bao_data()
 
 cho_sn = cho_factor(cov_matrix_sn, lower=True)[0]
-cho_bao = cho_factor(cov_matrix_bao, lower=True)[0]
+inv_cov_bao = np.linalg.inv(cov_matrix_bao)
 
 c = c0 / 1000  # Speed of light in km/s
 
-z_max = max(np.max(z_cmb), np.max(bao_data["z"])) + 0.1
-z_grid = np.linspace(0, z_max, num=3000)
+z_max = max(np.max(z_cmb), np.max(bao["z"])) + 0.1
+z_grid = np.linspace(0, z_max, num=4000)
 dz = np.diff(z_grid)
 
 
 @njit
-def Ez(z, params):
-    h, Omh2, w0 = params[2] / 100, params[3], params[4]
-    Om = Omh2 / h**2
+def Ode_z(z, w0):
+    # Thawing quintessence (not used now)
     cubic = (1.0 + z) ** 3
-    rho_de = (2 * cubic / (1.0 + w0 + (1.0 - w0) * cubic)) ** 2
-    return np.sqrt(Om * cubic + (1.0 - Om) * rho_de)
+    return (2 * cubic / (1.0 + w0 + (1.0 - w0) * cubic)) ** 2
+
+
+@njit
+def Ez(z, params):
+    h, Omh2 = params[2] / 100, params[3]
+    Om = Omh2 / h**2
+    return np.sqrt(Om * (1.0 + z) ** 3 + (1.0 - Om))
 
 
 @njit
@@ -55,11 +60,12 @@ def DV_z(z, params):
 
 
 qty_map = {"DV_over_rs": 0, "DM_over_rs": 1, "DH_over_rs": 2}
-quantities = np.array([qty_map[q] for q in bao_data["quantity"]], dtype=np.int64)
+bao_qty = np.array([qty_map[q] for q in bao["quantity"]], dtype=np.int64)
 
 
 @njit
 def bao_theory(z, qty, params):
+    rdrag = params[1]
     DV_mask = qty == 0
     DM_mask = qty == 1
     DH_mask = qty == 2
@@ -67,13 +73,22 @@ def bao_theory(z, qty, params):
     results[DH_mask] = DH_z(z[DH_mask], params)
     results[DM_mask] = DM_z(z[DM_mask], params)
     results[DV_mask] = DV_z(z[DV_mask], params)
-    return results / params[1]
+    return results / rdrag
 
 
 @njit
-def theory_mu(params):
-    dL = (1.0 + z_hel) * DM_z(z_cmb, params)
-    return params[0] + 25.0 + 5 * np.log10(dL)
+def mu_corr(params, DM_obs):
+    # Heaviside step function
+    v_km_s = 100 * params[4] * np.where(z_cmb <= 0.10563, 1, -1)
+    z_pec = v_km_s / c
+    z_cosmo = -1.0 + (1.0 + z_cmb) / (1.0 + z_pec)
+
+    return 5.0 * np.log10(DM_z(z_cosmo, params) / DM_obs)
+
+
+@njit
+def theory_mu(offset, DM):
+    return offset + 25.0 + 5 * np.log10((1.0 + z_hel) * DM)
 
 
 def solve_triang(cho_L, delta):
@@ -81,13 +96,20 @@ def solve_triang(cho_L, delta):
     return np.dot(y, y)
 
 
-def chi_squared(params):
-    delta_sn = mu_values - theory_mu(params)
-    chi_sn = solve_triang(cho_sn, delta_sn)
+def chi2_sn(params):
+    DM = DM_z(z_cmb, params)
+    delta_sn = mu_values - theory_mu(params[0], DM) - mu_corr(params, DM)
+    return solve_triang(cho_sn, delta_sn)
 
-    delta_bao = bao_data["value"] - bao_theory(bao_data["z"], quantities, params)
-    chi_bao = solve_triang(cho_bao, delta_bao)
-    return chi_sn + chi_bao
+
+@njit
+def chi2_bao(params):
+    delta_bao = bao["value"] - bao_theory(bao["z"], bao_qty, params)
+    return delta_bao @ inv_cov_bao @ delta_bao
+
+
+def chi_squared(params):
+    return chi2_sn(params) + chi2_bao(params)
 
 
 def log_likelihood(params):
@@ -108,7 +130,7 @@ def main():
     prior.add_parameter("rd", dist=(120.0, 165.0))
     prior.add_parameter("H0", dist=(50.0, 90.0))
     prior.add_parameter("ωm", dist=norm(loc=0.1430, scale=0.0011))  # Planck prior
-    prior.add_parameter("w0", dist=(-1.0, -1 / 3))
+    prior.add_parameter("v", dist=(-5.5, 2.5))
 
     with Pool(6) as pool:
         sampler = Sampler(
@@ -126,33 +148,33 @@ def main():
     rd_16, rd_50, rd_84 = quantile(samples[:, 1], one_sigma_ci, weights=w)
     H0_16, H0_50, H0_84 = quantile(samples[:, 2], one_sigma_ci, weights=w)
     Omh2_16, Omh2_50, Omh2_84 = quantile(samples[:, 3], one_sigma_ci, weights=w)
-    w0_16, w0_50, w0_84 = quantile(samples[:, 4], one_sigma_ci, weights=w)
+    v_16, v_50, v_84 = quantile(samples[:, 4], one_sigma_ci, weights=w)
     Om_16, Om_50, Om_84 = quantile(Om_samples, one_sigma_ci, weights=w)
 
-    best_fit = [dM_50, rd_50, H0_50, Omh2_50, w0_50]
+    best_fit = [dM_50, rd_50, H0_50, Omh2_50, v_50]
 
     print(f"ΔM: {dM_50:.3f} +{(dM_84 - dM_50):.3f} -{(dM_50 - dM_16):.3f} mag")
     print(f"r_d: {rd_50:.2f} +{(rd_84 - rd_50):.2f} -{(rd_50 - rd_16):.2f} Mpc")
     print(f"H0: {H0_50:.2f} +{(H0_84 - H0_50):.2f} -{(H0_50 - H0_16):.2f} km/s/Mpc")
     print(f"ωm: {Omh2_50:.4f} +{(Omh2_84 - Omh2_50):.4f} -{(Omh2_50 - Omh2_16):.4f}")
     print(f"Ωm: {Om_50:.3f} +{(Om_84 - Om_50):.3f} -{(Om_50 - Om_16):.3f}")
-    print(f"w0: {w0_50:.3f} +{(w0_84 - w0_50):.3f} -{(w0_50 - w0_16):.3f}")
+    print(f"v: {v_50:.3f} +{(v_84 - v_50):.3f} -{(v_50 - v_16):.3f} x 100 km/s")
     print(f"Chi squared: {chi_squared(best_fit):.1f}")
     print(f"Log evidence: {sampler.log_z:.1f}")
-    print(f"Degrees of freedom: {len(bao_data['z']) + sn_size - len(best_fit)}")
+    print(f"Degrees of freedom: {len(bao) + sn_size - len(best_fit)}")
 
     plot_bao_predictions(
         theory_predictions=lambda z, qty: bao_theory(z, qty, best_fit),
-        data=bao_data,
+        data=bao,
         errors=np.sqrt(np.diag(cov_matrix_bao)),
         title=bao_legend,
     )
     plot_sn_predictions(
         legend=sn_legend,
         x=z_cmb,
-        y=mu_values,
+        y=mu_values - mu_corr(best_fit, DM_z(z_cmb, best_fit)),
         y_err=np.sqrt(np.diag(cov_matrix_sn)),
-        y_model=theory_mu(best_fit),
+        y_model=theory_mu(best_fit[0], DM_z(z_cmb, best_fit)),
         label=f"$Ω_m$={Om_50:.3f}",
         x_scale="log",
     )
@@ -182,32 +204,47 @@ if __name__ == "__main__":
 *******************************
 DESI BAO DR2 2025
 DES5Y Dovekie SNe
-Omh2 prior from Planck 2018
+Om x h^2 prior from Planck 2018
 
 Priors:
 ΔM U(-0.5, +0.5)
 rd U(120, 165)
 H0 U(50.0, 90.0)
 ωm N(0.1430, 0.0011)
+
 w0 U(-1.5, 0.0)
-wa U(-3.0, +2.0)
+wa U(-3.0, +2.0) (w0 + wa > 0 enforced)
+
+v  U(-5.5, +2.5) x 100 km/s
 *******************************
 """
 
 """
-Flat ΛCDM w(z) = -1
-r_d: 147.57 +1.21 -1.21 Mpc
-H0: 68.36 +0.90 -0.89 km/s/Mpc
+Flat ΛCDM
+ΔM: -0.059 +0.024 -0.024 mag
+r_d: 147.55 +1.22 -1.20 Mpc
+H0: 68.37 +0.90 -0.89 km/s/Mpc
 ωm: 0.1430 +0.0011 -0.0011
 Ωm: 0.306 +0.008 -0.008
-w0: -1
-wa: 0
 Chi squared: 1645.3
 Log evidence: -833.8
 Degrees of freedom: 1723
 
 ===============================
 
+Flat ΛCDM +  v corrections
+v: -1.59 +0.58 -0.57 x 100 km/s
+ΔM: -0.041 +0.025 -0.025 mag
+r_d: 146.71 +1.25 -1.25 Mpc
+H0: 69.11 +0.94 -0.93 km/s/Mpc
+ωm: 0.1430 +0.0011 -0.0011
+Ωm: 0.299 +0.008 -0.008
+Chi squared: 1637.4 (2.81 sigma significance)
+Log evidence: -831.6 (Δ logZ = 2.2 in favour of v corrections)
+Degrees of freedom: 1722
+"""
+
+"""
 Flat wCDM w(z) = w0
 r_d: 143.73 +2.07 -2.15 Mpc
 H0: 69.38 +1.06 -1.02 km/s/Mpc
@@ -227,7 +264,7 @@ H0: 68.52 +0.90 -0.89 km/s/Mpc
 ωm: 0.1430 +0.0011 -0.0011
 Ωm: 0.305 +0.008 -0.007
 w0: -0.860 +0.051 -0.052
-wa: d w(z)/dz at z=0 = -(3/2) * (1 - w0^2)
+wa: d w(z)/dz at z=0 = -1.5 * (1 - w0^2)
 Chi squared: 1638.5
 Log evidence: -832.0 (Δ logZ = 1.8 against ΛCDM)
 Degrees of freedom: 1722
