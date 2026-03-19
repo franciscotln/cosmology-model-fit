4b2e79152ca09a9080aa81684fb825a98773c36a
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-03-19 21:21:58 +0100

    cc+union3.1

--- cosmic_chronometers/cc_union3.py
+++ cosmic_chronometers/cc_union3.py
@@ -28,13 +28,14 @@ def Ode_z(z, w0):
 
 
 @njit
-def H_z(z, H0, Om):
+def H_z(z, params):
+    H0, Om = params[2], params[3]
     return H0 * np.sqrt(Om * (1.0 + z) ** 3 + (1.0 - Om))
 
 
 @njit
-def DM_z(z, H0, Om):
-    dh_grid = c / H_z(z_grid, H0, Om)
+def DM_z(z, params):
+    dh_grid = c / H_z(z_grid, params)
     dh = (dh_grid[:-1] + dh_grid[1:]) / 2
     cum_dm = np.zeros(z_grid.size, dtype=np.float64)
     cum_dm[1:] = np.cumsum(dh * dz)
@@ -42,40 +43,39 @@ def DM_z(z, H0, Om):
 
 
 @njit
-def mu_corr(H0, Om, v100):
+def mu_corr(params, DM_obs):
     # Heaviside step at z = 0.2
-    v_km_s = 100 * v100 * np.where(z_cmb <= 0.2, 1, -1)
+    v_km_s = 100 * params[4] * np.where(z_cmb <= 0.2, 1, -1)
     z_pec = v_km_s / c
     z_cosmo = -1.0 + (1.0 + z_cmb) / (1.0 + z_pec)
 
-    DM_obs = DM_z(z_cmb, H0, Om)
-    return 5 * np.log10(DM_z(z_cosmo, H0, Om) / DM_obs)
+    return 5 * np.log10(DM_z(z_cosmo, params) / DM_obs)
 
 
 @njit
-def mu_theory(offset, H0, Om):
-    dL = (1.0 + z_hel) * DM_z(z_cmb, H0, Om)
-    return offset + 25.0 + 5 * np.log10(dL)
+def mu_theory(offset, DM):
+    return offset + 25.0 + 5 * np.log10((1.0 + z_hel) * DM)
 
 
-def chi_squared(pars):
-    H0, Om, v100 = pars["H0"], pars["Om"], pars["v"]
-    f_cc = pars["f_cc"]
-    offset = pars["dM"]
+@njit
+def chi_squared(params):
+    f_cc, offset = params[0], params[1]
 
-    delta_sn = mu_vals - mu_theory(offset, H0, Om) - mu_corr(H0, Om, v100)
+    DM_obs = DM_z(z_cmb, params)
+    delta_sn = mu_vals - mu_theory(offset, DM_obs) - mu_corr(params, DM_obs)
     chi_sn = delta_sn @ inv_cov_sn @ delta_sn
 
-    cc_delta = H_cc_vals - H_z(z_cc_vals, H0, Om)
-    chi_cc = cc_delta @ (f_cc**2 * inv_cov_cc) @ cc_delta
+    cc_delta = H_cc_vals - H_z(z_cc_vals, params)
+    chi_cc = f_cc**2 * cc_delta @ inv_cov_cc @ cc_delta
 
     return chi_sn + chi_cc
 
 
-def log_likelihood(pars):
-    f_cc = pars["f_cc"]
+@njit
+def log_likelihood(params):
+    f_cc = params[0]
     normalization_cc = N_cc * np.log(2 * np.pi) + logdet_cc - 2 * N_cc * np.log(f_cc)
-    return -0.5 * chi_squared(pars) - 0.5 * normalization_cc
+    return -0.5 * chi_squared(params) - 0.5 * normalization_cc
 
 
 def main():
@@ -87,14 +87,16 @@ def main():
     from .plot_predictions import plot_cc_predictions
 
     prior = Prior()
-    prior.add_parameter("f_cc", dist=(0.1, 3.0))
+    prior.add_parameter("f_cc", dist=(0.05, 3.35))
     prior.add_parameter("dM", dist=(-1.0, 1.0))
-    prior.add_parameter("H0", dist=(55.0, 85.0))
+    prior.add_parameter("H0", dist=(40.0, 95.0))
     prior.add_parameter("Om", dist=(0.1, 0.7))
-    prior.add_parameter("v", dist=(-10.5, 4.0))
+    prior.add_parameter("v", dist=(-11.0, 5.5))
 
     with Pool(6) as pool:
-        sampler = Sampler(prior, log_likelihood, n_live=8_000, pool=pool, seed=42)
+        sampler = Sampler(
+            prior, log_likelihood, n_live=6_000, pool=pool, seed=42, pass_dict=False
+        )
         sampler.run(verbose=True)
 
     samples, log_w, log_l = sampler.posterior()
@@ -129,7 +131,7 @@ def main():
     Omh2_samples = samples[:, 3] * (samples[:, 2] / 100) ** 2
     Omh2_16, Omh2_50, Omh2_84 = quantile(Omh2_samples, one_sigma_ci, weights=w)
 
-    best_fit = {"f_cc": fcc_50, "dM": dM_50, "H0": h0_50, "Om": Om_50, "v": v_50}
+    best_fit = [fcc_50, dM_50, h0_50, Om_50, v_50]
     deg_of_freedom = len(z_cmb) + N_cc - len(labels)
 
     print(f"f_cc: {fcc_50:.2f} +{(fcc_84 - fcc_50):.2f} -{(fcc_50 - fcc_16):.2f}")
@@ -143,7 +145,7 @@ def main():
     print(f"Degrees of freedom: {deg_of_freedom}")
 
     plot_cc_predictions(
-        H_z=lambda z: H_z(z, best_fit["H0"], best_fit["Om"]),
+        H_z=lambda z: H_z(z, best_fit),
         z=z_cc_vals,
         H=H_cc_vals,
         H_err=np.sqrt(np.diag(cov_matrix_cc)) / fcc_50,
@@ -152,9 +154,9 @@ def main():
     plot_sn_predictions(
         legend=legend_sn,
         x=z_cmb,
-        y=mu_vals,
+        y=mu_vals - mu_corr(best_fit, DM_z(z_cmb, best_fit)),
         y_err=np.sqrt(np.diag(cov_matrix_sn)),
-        y_model=mu_theory(dM_50, h0_50, Om_50),
+        y_model=mu_theory(dM_50, DM_z(z_cmb, best_fit)),
         label=f"$Ω_m$={Om_50:.4f}",
         x_scale="log",
     )
@@ -166,13 +168,13 @@ if __name__ == "__main__":
 
 """
 Flat ΛCDM
-ΔM: -0.077 +0.074 -0.076 mag
-H0: 66.7 +2.6 -2.5 km/s/Mpc
+ΔM: -0.076 +0.073 -0.076 mag
+H0: 66.8 +2.5 -2.5 km/s/Mpc
 Ωm: 0.333 +0.022 -0.021
-ωm: 0.1486 +0.0103 -0.0100
-f_cc: 1.49 +0.18 -0.17
-Chi squared: 63.94
-Log evidence: -167.3
+ωm: 0.149 +0.010 -0.010
+f_cc: 1.48 +0.18 -0.17
+Chi squared: 63.80
+Log evidence: -168.0
 Degrees of freedom: 54
 """
 
@@ -181,41 +183,27 @@ Flat ΛCDM
 Isotropic velocity SNe observed redshifts (turning point z <= 0.2 inflow z > 0.2 outflow)
 z_cosmo = -1 + (1 + z) / (1 + v/c)
 
-v: -298 +117 -116 km/s (prior U(-10.5, 4.0) x 100 km/s)
-ΔM: -0.044 +0.075 -0.077 mag
-H0: 68.5 +2.7 -2.7 km/s/Mpc
+v: -290 +117 -116 km/s (prior U(-11.0, 5.5) x 100 km/s)
+ΔM: -0.044 +0.075 -0.076 mag
+H0: 68.5 +2.7 -2.6 km/s/Mpc
 Ωm: 0.307 +0.023 -0.022
 ωm: 0.144 +0.010 -0.010
 f_cc: 1.48 +0.18 -0.17
-Chi squared: 57.35 (2.57 sigma significance)
-Log evidence: -165.8 (ΔlogZ = 1.5 in favour of in/outflow)
+Chi squared: 57.36 (2.54 sigma significance)
+Log evidence: -166.7 (ΔlogZ = 1.3 in favour of v corrections)
 Degrees of freedom: 53
 """
 
 """
 Flat wCDM: w(z) = w0
-ΔM: -0.059 +0.081 -0.081 mag
-H0: 67.1 +2.7 -2.6 km/s/Mpc
+ΔM: -0.057 +0.080 -0.082 mag
+H0: 67.1 +2.6 -2.6 km/s/Mpc
 Ωm: 0.307 +0.042 -0.048
-ωm: 0.1377 +0.0172 -0.0197
+ωm: 0.1379 +0.0172 -0.0198
 w0: -0.91 +0.12 -0.13 (prior U(-1.5, -0.5))
 f_cc: 1.47 +0.18 -0.17
-Chi squared: 62.44 (1.24 sigma significance)
-Log evidence: -168.1
-Degrees of freedom: 53
-
-==============================
-
-Flat wzCDM: w(z) = -1 + 2 * (1 + w0) / (1 + w0 + (1 - w0) * (1 + z)^3)
-ΔM: -0.049 +0.077 -0.078 mag
-H0: 67.1 +2.6 -2.6 km/s/Mpc
-Ωm: 0.310 +0.026 -0.027
-ωm: 0.1393 +0.0117 -0.0118
-w0: -0.84 +0.11 -0.10 (prior U(-1, 0) - truncated at -1, 1.45 sigma to the left of the mean)
-wa: d w(z)/dz at z=0 = -1.5 * (1 - w0^2) = -0.442
-f_cc: 1.47 +0.18 -0.17
-Chi squared: 62.13 (1.35 sigma significance)
-Log evidence: -168.0
+Chi squared: 62.49 (1.14 sigma significance)
+Log evidence: -168.9 (ΔlogZ = -0.9 in favour of ΛCDM)
 Degrees of freedom: 53
 
 ==============================
