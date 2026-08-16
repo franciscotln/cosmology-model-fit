1094946a915028f9a450f9aa6afa07d470aab445
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-08-16 20:27:31 +0200

    DESI BAO + FS Lya (nautilus + getdist)

--- bao/desi_fs_lya.py
+++ bao/desi_fs_lya.py
@@ -61,102 +61,77 @@ def chi_squared(theta):
     return delta_bao @ inv_cov_bao @ delta_bao
 
 
-bounds = np.array(
-    [
-        (0.50, 0.80),  # h
-        (0.1, 0.5),  # Ωm
-        (-1.0, 0.0),  # w0
-    ]
-)
-
-normalization = -np.sum(np.log(bounds[:, 1] - bounds[:, 0]))
-
-
-@njit
-def log_prior(params):
-    if not np.all((bounds[:, 0] < params) & (params < bounds[:, 1])):
-        return -np.inf
-    return normalization
-
-
 @njit
 def log_likelihood(params):
     return -0.5 * chi_squared(params)
 
 
-def log_probability(params):
-    lp = log_prior(params)
-    if np.isinf(lp):
-        return -np.inf
-    return lp + log_likelihood(params)
-
-
 def main():
     from multiprocessing import Pool
-    import emcee
+    from corner import quantile
+    from getdist import plots, MCSamples
+    from nautilus import Sampler, Prior
+    import matplotlib.pyplot as plt
     from bao.plot_predictions import plot_bao_predictions, plot_bao_residuals
-    from gelman_rubin import gelman_rubin
-    from log_evidence import log_evidence
-    from corner_plot import plot_corner_and_chains
 
-    np.random.seed(42)
-    ndim = len(bounds)
-    nwalkers = 100
-    burn_in = 1000
-    nsteps = 9000 + burn_in
-    initial_pos = np.random.uniform(bounds[:, 0], bounds[:, 1], (nwalkers, ndim))
-    moves = [
-        (emcee.moves.KDEMove(bw_method="silverman"), 0.15),
-        (emcee.moves.DEMove(), 0.85),
-    ]
+    prior = Prior()
+    prior.add_parameter("h", dist=(0.5, 0.8))
+    prior.add_parameter("om", dist=(0.1, 0.8))
+    prior.add_parameter("w0", dist=(-2.5, 2.5))
 
     with Pool(6) as pool:
-        sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, pool, moves)
-        sampler.run_mcmc(
-            initial_pos, nsteps, progress=True, progress_kwargs={"colour": "#ff5a00"}
+        sampler = Sampler(
+            prior, log_likelihood, n_live=6_000, pool=pool, seed=42, pass_dict=False
         )
+        sampler.run(verbose=True)
 
-    try:
-        tau = sampler.get_autocorr_time()
-        print("Auto-correlation time:", tau)
-        print(
-            "Effective samples:", ndim * nwalkers * (nsteps - burn_in) / np.max(tau)
-        )
-        print("Acceptance fraction:", np.mean(sampler.acceptance_fraction))
-    except emcee.autocorr.AutocorrError as e:
-        print("Autocorrelation time could not be computed", e)
+    samples, log_w, log_l = sampler.posterior()
+    weights = np.exp(log_w)
+    one_sigma_ci = [0.159, 0.5, 0.841]
 
-    samples = sampler.get_chain(discard=burn_in, flat=True)
-    chain_samples = sampler.get_chain(discard=burn_in, flat=False)
-    log_probs = sampler.get_log_prob(discard=burn_in, flat=True)
-    log_evd = log_evidence(samples, log_probs, log_probability, bounds)
-    print("Gelman-Rubin:", gelman_rubin(chain_samples))
+    h_16, h_50, h_84 = quantile(samples[:, 0], one_sigma_ci, weights)
+    om_16, om_50, om_84 = quantile(samples[:, 1], one_sigma_ci, weights)
+    w0_16, w0_50, w0_84 = quantile(samples[:, 2], one_sigma_ci, weights)
 
-    [
-        (h_16, h_50, h_84),
-        (om_16, om_50, om_84),
-        (w0_16, w0_50, w0_84),
-    ] = np.percentile(samples, [15.9, 50, 84.1], axis=0).T
-
-    best_fit = np.percentile(samples, 50, axis=0)
-    dof = len(data["value"]) - len(best_fit)
+    best_fit = [h_50, om_50, w0_50]
+    dof = len(data["z"]) - len(best_fit)
 
     residuals = data["value"] - bao_theory(data["z"], bao_qty, best_fit)
     ss_res = np.sum(residuals**2)
     ss_tot = np.sum((data["value"] - np.mean(data["value"])) ** 2)
     r2 = 1 - ss_res / ss_tot
+    chi2 = chi_squared(best_fit)
 
     print(f"h * rd: {RD * h_50:.2f} +{RD * (h_84 - h_50):.2f} -{RD * (h_50 - h_16):.2f}")
     print(f"Ωm: {om_50:.4f} +{om_84-om_50:.4f} -{om_50-om_16:.4f}")
     print(f"w0: {w0_50:.3f} +{(w0_84 - w0_50):.3f} -{(w0_50 - w0_16):.3f}")
-    print(f"χ2: {chi_squared(best_fit):.2f}")
-    print(f"Log evidence: {log_evd:.2f}")
+    print(f"χ2: {chi2:.2f}")
     print(f"DOF: {dof}")
+    print(f"χ2/dof: {chi2 / dof:.2f}")
+    print(f"Log evidence: {sampler.log_z:.1f}")
     print(f"R^2: {r2:.4f}")
     print(f"RMSD: {np.sqrt(np.mean(residuals**2)):.3f}")
 
-    labels = ["$h$", "$Ω_m$", "$w_0$"]
-    plot_corner_and_chains(labels=labels, flat_samples=samples, samples=chain_samples)
+    gd_samples = MCSamples(
+        samples=samples,
+        weights=weights,
+        loglikes=log_l,
+        names=prior.keys,
+        labels=["h", "Ω_m", "w_0"],
+        label="BAO + FS Lyman-alpha",
+    )
+    gd_samples.addDerived(gd_samples["h"] * RD, name="hrd", label="h * r_{drag}")
+    gd_samples.updateBaseStatistics()
+    plots.getSubplotPlotter().triangle_plot(
+        gd_samples,
+        params=['hrd', 'om', 'w0'],
+        filled=True,
+        title_limit=1,
+        contour_colors=["C0"],
+        color=["C0"],
+    )
+    plt.show()
+
     plot_bao_predictions(
         theory_predictions=lambda z, qty: bao_theory(z, qty, best_fit),
         data=data,
@@ -175,41 +150,45 @@ if __name__ == "__main__":
 # *******************************
 
 # Flat ΛCDM:
-# h * rd: 101.18 +0.67 -0.67
-# Ωm: 0.3016 +0.0078 -0.0076
+# h * rd: 101.17 +0.67 -0.67
+# Ωm: 0.3015 +0.0078 -0.0075
 # χ2: 12.81
 # DOF: 12
 # χ2/dof: 1.07
+# Log evidence: -14.2
 # R^2: 0.9987
 # RMSD: 0.298
 
 # Flat wCDM:
-# h * rd: 100.42 +1.78 -1.67
-# Ωm: 0.3021 +0.0082 -0.0080
-# w0: -0.966 +0.072 -0.075 (prior ~U(-1.4, -0.4))
-# χ2: 12.61
+# h * rd: 100.5 +1.7 -1.7
+# Ωm: 0.3022 +0.0081 -0.0081
+# w0: -0.967 +0.074 -0.074 (prior ~U(-1.4, -0.4))
+# χ2: 12.60
 # DOF: 11
 # χ2/dof: 1.15
+# Log evidence: -15.7
 # R^2: 0.9988
 # RMSD: 0.287
 
 # Flat wzCDM: w(z) = -1 + 2 * (1 + w0) / (1 + w0 + (1 - w0) * (1 + z)**3)
-# h * rd: 98.49 +1.77 -2.09
-# Ωm: 0.3141 +0.0120 -0.0108
-# w0: -0.833 +0.124 -0.105 (prior ~U(-1, 0)) - left side truncated
+# h * rd: 98.3 +2.2 -1.5
+# Ωm: 0.315 +0.010 -0.012
+# w0: -0.824 +0.068 -0.15 (prior ~U(-1, 0)) - left side truncated
 # χ2: 12.08
 # DOF: 11
 # χ2/dof: 1.10
+# Log evidence: -15.0
 # R^2: 0.9990
-# RMSD: 0.267
+# RMSD: 0.268
 
 # Flat w0waCDM:
-# h * rd: 90.02 +4.63 -4.16
-# Ωm: 0.403 +0.044 -0.044 (prior ~U(0.1, 0.8))
-# w0: -0.042 +0.441 -0.429 (prior ~U(-2.5, 2.5))
-# wa: -3.30 +1.45 -1.48 (prior ~U(-10, 4))
-# χ2: 7.21
+# h * rd: 90.3 +3.9 -4.8
+# Ωm: 0.403 +0.045 -0.045
+# w0: -0.04 +0.43 -0.43 (prior ~U(-2.5, 2.5))
+# wa: -3.3 +1.5 -1.5 (prior ~U(-10, 4))
+# χ2: 7.22
 # DOF: 10
 # χ2/dof: 0.72
+# Log evidence: -16.1
 # R^2: 0.9995
 # RMSD: 0.195
