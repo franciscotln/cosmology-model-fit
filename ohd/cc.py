14c511d1f52378ddc2983803dff64a018e370689
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-08-29 10:05:36 +0200

    cc

--- ohd/cc.py
+++ ohd/cc.py
@@ -39,11 +39,11 @@ def log_likelihood(params):
 
 
 def main():
-    from nautilus import Sampler, Prior
-    from corner import corner, quantile
-    import matplotlib.pyplot as plt
     from multiprocessing import Pool
-    from .plot_predictions import plot_cc_predictions
+    from nautilus import Sampler, Prior
+    from corner import quantile
+    from corner_plot import plot_corner_and_chains
+    from ohd.plot_predictions import plot_cc_predictions
 
     prior = Prior()
     prior.add_parameter("H0", dist=(30, 100))
@@ -52,7 +52,7 @@ def main():
 
     with Pool(5) as pool:
         sampler = Sampler(
-            prior, log_likelihood, n_live=10_000, pool=pool, seed=42, pass_dict=False
+            prior, log_likelihood, n_live=6_000, pool=pool, seed=42, pass_dict=False
         )
         sampler.run(verbose=True)
 
@@ -61,28 +61,11 @@ def main():
     log_evd = sampler.log_z
     one_sigma_ci = [0.159, 0.5, 0.841]
 
-    corner(
-        samples,
-        weights=w,
-        labels=prior.keys,
-        quantiles=one_sigma_ci,
-        show_titles=True,
-        title_fmt=".4f",
-        bins=100,
-        fill_contours=False,
-        plot_datapoints=False,
-        smooth=2.0,
-        smooth1d=2.0,
-        levels=(0.393, 0.864),
-        range=np.repeat(0.9999, len(prior.keys)),
-    )
-    plt.show()
-
     H0_16, H0_50, H0_84 = quantile(samples[:, 0], one_sigma_ci, weights=w)
     Om_16, Om_50, Om_84 = quantile(samples[:, 1], one_sigma_ci, weights=w)
     f_16, f_50, f_84 = quantile(samples[:, 2], one_sigma_ci, weights=w)
 
-    best_fit = [H0_50, Om_50, f_50]
+    best_fit = samples[np.argmax(log_l)]
 
     print(f"H0: {H0_50:.1f} +{(H0_84 - H0_50):.1f} -{(H0_50 - H0_16):.1f}")
     print(f"Ωm: {Om_50:.3f} +{(Om_84 - Om_50):.3f} -{(Om_50 - Om_16):.3f}")
@@ -92,6 +75,7 @@ def main():
     print(f"Log evidence: {log_evd:.2f}")
     print(f"Degs of freedom: {len(z_values) - len(best_fit)}")
 
+    plot_corner_and_chains(prior.keys, samples, weights=w)
     plot_cc_predictions(
         H_z=lambda z: H_z(z, best_fit),
         z=z_values,
@@ -104,56 +88,52 @@ def main():
 if __name__ == "__main__":
     main()
 
-"""
-*******************************
-Results for data from
-https://arxiv.org/pdf/2307.09501
 
-4 data points from
-https://arxiv.org/pdf/2506.03836
-https://arxiv.org/pdf/2511.02730v1
+# *******************************
+# Results for data from
+# https://arxiv.org/pdf/2307.09501
+#
+# 4 data points from
+# https://arxiv.org/pdf/2506.03836
+# https://arxiv.org/pdf/2511.02730v1
+#
+# 1 data point from
+# https://arxiv.org/pdf/2606.07298v1
+#
+# 1 data point from
+# https://arxiv.org/pdf/2608.13178
+# *******************************
 
-1 data point from
-https://arxiv.org/pdf/2606.07298v1
 
-1 data point from
-https://arxiv.org/pdf/2608.13178
-*******************************
-"""
+# Model: Flat ΛCDM
 
-"""
-Flat ΛCDM
+# Varying f ~U[0.1, 3.3]:
+# H0: 66.9 +3.6 -3.6 km/s/Mpc
+# Ωm: 0.33 +0.05 -0.04
+# f: 1.49 +0.18 -0.17
+# Chi squared: 38.03
+# Log likelihood: -149.14
+# Log evidence: -155.91 (diff: 2.35 in evidence favouring the model with f)
+# Degs of freedom: 35
 
--------------------------------
+# -------------------------------
 
-Varying f in U(0.1, 3.3):
-H0: 66.9 +3.6 -3.6 km/s/Mpc
-Ωm: 0.33 +0.05 -0.04
-f: 1.49 +0.18 -0.17
-Chi squared: 36.34
-Log likelihood: -149.17
-Log evidence: -155.91 (diff: 2.35 in evidence favouring the model with f)
-Degs of freedom: 35
+# With fixed f = 1:
+# H0: 66.4 +5.3 -5.5 km/s/Mpc
+# Ωm: 0.34 +0.08 -0.06
+# Chi squared: 16.39
+# Log likelihood: -154.31
+# Log evidence: -158.26
+# Degs of freedom: 36
 
--------------------------------
+# -------------------------------
 
-With fixed f = 1:
-H0: 66.3 +5.4 -5.4 km/s/Mpc
-Ωm: 0.34 +0.08 -0.06
-Chi squared: 16.42
-Log likelihood: -154.33
-Log evidence: -158.26
-Degs of freedom: 36
-"""
-
-"""
-Log likelihood ratio test:
--2 * log(L0/L1) = -2 * log(L0) + 2 * log(L1)
--2 * (-154.33) + 2 * (-149.17) = 10.32
-
-Degrees of freedom = 1
-p-value = 0.0013
-We are 99.87% confident that the model with f is better than the one without f.
-So the uncertainties in the H(z) dataset are overestimated and should be scaled down.
-3.21 sigma significance.
-"""
+# Log likelihood ratio test:
+# -2 * log(L0/L1) = -2 * log(L0) + 2 * log(L1)
+# -2 * (-154.33) + 2 * (-149.17) = 10.34
+#
+# Degrees of freedom = 1
+# p-value = 0.0013
+# We are 99.87% confident that the model with f is better than the one without f.
+# So the uncertainties in the H(z) dataset are overestimated and should be scaled down.
+# 3.22 sigma significance.
