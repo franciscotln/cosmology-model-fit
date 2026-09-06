7795a0d4ba86a9744ac695e2462597f178644d11
Francisco Tavares de Lima Neto <francisco.tln@outlook.com>
2026-09-06 16:29:03 +0200

    fix one data point for z=0.09 and add reference

--- ohd/cc.py
+++ ohd/cc.py
@@ -12,8 +12,8 @@ def H_z(z, params):
     return H0 * np.sqrt(Om * (1.0 + z) ** 3 + (1.0 - Om))
 
 
-z_pivot = 0.6137
-# corr(ln(fp), n) = 8.179e-05
+z_pivot = 0.6142
+# corr(ln(fp), n) = 4.6240e-03
 
 
 @njit
@@ -71,6 +71,7 @@ def main():
     )
     gd_samples.addDerived(np.exp(gd_samples["ln_fp"]), name="fp", label="f_{pivot}")
     gd_samples.updateBaseStatistics()
+    print("Correlation matrix:\n", gd_samples.corr(prior.keys))
 
     for name in gd_samples.getParamNames().names:
         print(gd_samples.getInlineLatex(name, limit=1))
@@ -123,48 +124,48 @@ if __name__ == "__main__":
 # with z_piv = 0.6137, corr(ln(fp), n) = 0:
 # cov[i, j] = cov_sys[i, j] + cov_diag[i, j] * f(z_i) * f(z_j)
 #
-# H0 = 67.5 +- 3.9 km/s/Mpc
-# Ωm = 0.313 +0.038 -0.049
-# ln(fp) = -0.48 +0.11 -0.13 (prior ~U[ln(0.3), ln(1.2)])
-# n = 1.33 +- 0.48 (prior ~U[-4, 4])
-# Ωm h^2 = 0.141 +- 0.015
-# fp = 0.623 +0.058 -0.086
-# Log likelihood (MAP): -150.11
-# Log evidence: -158.14 (diff: 5.18 strong evidence favouring the model with fp, n)
-# χ2 (MAP): 37.97
+# H0 = 67.3 +- 3.8 km/s/Mpc
+# Ωm = 0.310 +0.038 -0.048
+# ln(fp) = -0.49 +0.11 -0.13 (prior ~U[ln(0.3), ln(1.2)])
+# n = 1.38 +- 0.49 (prior ~U[-4, 4])
+# Ωm h^2 = 0.139 +- 0.015
+# fp = 0.619 +0.058 -0.085
+# Log likelihood (MAP): -149.86
+# Log evidence: -157.90 (diff: 5.42 strong evidence favouring the model with fp, n)
+# χ2 (MAP): 38.01
 # DOF: 35
-# χ2/DOF: 1.08
+# χ2/DOF: 1.09
 # ---------------------------------
 
 # Constant covariance diagonal scaling f(z) = f0:
 # cov[i, j] = cov_sys[i, j] + cov_diag[i, j] * f0^2
 #
 # H0 = 65.9 ± 4.6 km/s/Mpc
-# Ωm = 0.340 +0.042 -0.058
+# Ωm = 0.340 +0.041 -0.059
 # ln(f0) = -0.39 +0.11 -0.13
 # Ωm h^2 = 0.146 ± 0.013
-# f0 = 0.685 +0.063 -0.093
-# Log likelihood (MAP): -154.26
-# Log evidence: -160.30 (diff: 3.02 moderate evidence favouring the model with f0)
-# χ2 (MAP): 38.27
+# f0 = 0.683 +0.064 -0.092
+# Log likelihood (MAP): -154.21
+# Log evidence: -160.25 (diff: 3.07 moderate evidence favouring the model with f0)
+# χ2 (MAP): 38.19
 # DOF: 36
 # χ2/DOF: 1.06
 # ---------------------------------
 
 # Without error scaling (fixed f0 = 1, n = 0):
 # H0: 66.3 ± 5.4 km/s/Mpc
-# Om: 0.344 +0.053 -0.083
+# Om: 0.343 +0.053 -0.082
 # Ωm h^2: 0.148 ± 0.018
-# Log likelihood (MAP): -159.38
+# Log likelihood (MAP): -159.36
 # Log evidence: -163.32
-# χ2 (MAP): 16.61
+# χ2 (MAP): 16.57
 # DOF: 37
 # χ2/DOF: 0.45
 # ---------------------------------
 
 # Log likelihood ratio test f(z) vs no scaling:
 # -2 * log(L0/L1) = -2 * log(L0) + 2 * log(L1)
-# -2 * (-159.38) + 2 * (-150.11) = 18.54
+# -2 * (-159.36) + 2 * (-149.86) = 19.00
 # corresponding to a p-value of approximately 9.42x10^-5,
 # indicating strong evidence in favor of the model with f0 and n.
 
@@ -172,41 +173,43 @@ if __name__ == "__main__":
 # ------ without systematics ------
 
 # scaling diagonal elements f(z) = f0 * [(1+z) / (1+z_piv)]^n
-# H0 = 68.6 ± 1.5 km/s/Mpc
-# Ωm = 0.306 +0.035 -0.041
-# ln(fp) = -0.48 +0.11 -0.13 (prior ~U[ln(0.3), ln(1.2)])
-# n = 1.36 ± 0.48 (prior ~U[-4, 4])
-# Ωm h^2 = 0.143 ± 0.013
-# f0 = 0.621 +0.058 -0.085
-# Log likelihood (MAP): -148.47
-# Log evidence: -157.94 (diff: 5.35 strong evidence favouring the model with f0, n)
-# χ2 (MAP): 38.70
+# cov[i, j] = cov_diag[i, j] * f(z_i) * f(z_j)
+
+# H0 = 68.8 +1.5 -1.4 km/s/Mpc
+# Ωm = 0.302 +0.035 -0.041
+# ln(fp) = -0.49 +0.11 -0.13 (prior ~U[ln(0.3), ln(1.2)])
+# n = 1.41 ± 0.49 (prior ~U[-4, 4])
+# Ωm h^2 = 0.142 ± 0.014
+# f0 = 0.617 +0.058 -0.085
+# Log likelihood (MAP): -148.25
+# Log evidence: -157.73 (diff: 5.54 strong evidence favouring the model with f0, n)
+# χ2 (MAP): 39.09
 # DOF: 35
-# χ2/DOF: 1.11
+# χ2/DOF: 1.12
 # ---------------------------------
 
 # Constant covariance diagonal scaling f(z) = f0:
-# cov[i, j] = cov_sys[i, j] + cov_diag[i, j] * f0^2
+# cov[i, j] = cov_diag[i, j] * f0^2
 
-# H0 = 67.9 ± 2.1 km/s/Mpc
-# Ωm = 0.325 +0.036 -0.044
-# ln(f0) = -0.39 +0.11 -0.13
+# H0 = 68.0 ± 2.1 km/s/Mpc
+# Ωm = 0.323 +0.035 -0.044
+# ln(f0) = -0.39 +0.11 -0.12
 # Ωm h^2 = 0.149 ± 0.012
-# f0 = 0.685 +0.064 -0.093
-# Log likelihood (MAP): -153.03
-# Log evidence: -160.25 (diff: 3.04 moderate evidence favouring the model with f0)
-# χ2 (MAP): 38.90
+# f0 = 0.684 +0.063 -0.092
+# Log likelihood (MAP): -152.98
+# Log evidence: -160.22 (diff: 3.05 moderate evidence favouring the model with f0)
+# χ2 (MAP): 38.87
 # DOF: 36
 # χ2/DOF: 1.08
 # ---------------------------------
 
 # No scaling (f0 = 1, n = 0)
-# H0 = 67.6 ± 3.1 km/s/Mpc
-# Ωm = 0.332 +0.050 -0.068
+# H0 = 67.8 ± 3.1 km/s/Mpc
+# Ωm = 0.330 +0.049 -0.068
 # Ωm h^2 = 0.150 ± 0.017
-# Log likelihood (MAP): -158.45
-# Log evidence: -163.29
-# χ2 (MAP): 16.65
+# Log likelihood (MAP): -158.43
+# Log evidence: -163.27
+# χ2 (MAP): 16.62
 # DOF: 37
 # χ2/DOF: 0.45
 # ---------------------------------
@@ -214,6 +217,6 @@ if __name__ == "__main__":
 
 # Log likelihood ratio test f(z) vs no scaling:
 # -2 * log(L0/L1) = -2 * log(L0) + 2 * log(L1)
-# -2 * (-158.45) + 2 * (-148.47) = 19.96
+# -2 * (-158.43) + 2 * (-148.25) = 20.36
 # corresponding to a p-value of approximately 4.57x10^-5,
 # indicating strong evidence in favor of the model with f0 and n.
